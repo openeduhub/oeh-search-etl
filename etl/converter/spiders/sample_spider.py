ff5bf30da0fe9f5e0fa9ba4849fe39cb434242eb
Torsten Simon <simon@edu-sharing.net>
2020-04-06 18:33:10 +0200

    add sanmple spider

---
+++ etl/converter/spiders/sample_spider.py
@@ -0,0 +1,37 @@
+from scrapy.spiders import CrawlSpider
+from converter.items import *
+import time
+from w3lib.html import remove_tags, replace_escape_chars
+from converter.spiders.lom_base import LomBase;
+import json
+
+# Sample Spider, using a SitemapSpider to crawl your web page
+# Can be used as a template for your custom spider
+class SampleSpider(CrawlSpider, LomBase):
+  name='sample_spider'
+  start_urls = ['https://edu-sharing.com']
+
+  def parse(self, response):
+    return LomBase.parse(self, response)
+
+  def getBase(self, response):
+    base = BaseItemLoader()
+    base.add_value('sourceId', response.xpath('//title//text()').get())
+    # use a stable hash to detect content changes
+    # if there is no hash available, may use the current time as "always changing" info
+    base.add_value('hash', time.time())
+    return base
+
+  def getLOMGeneral(self, response):
+    general = LomBase.getLOMGeneral(response)
+    general.add_value('title', response.xpath('//title//text()').get())
+    general.add_value('language', response.xpath('//meta[@property="og:locale"]/@content').get())
+    return general
+
+  def getLOMTechnical(self, response):
+    technical = LomBase.getLOMTechnical(response)
+    technical.add_value('format', 'text/html')
+    technical.add_value('size', len(response.body))
+    return technical
+
+  # You may override more lom container here
