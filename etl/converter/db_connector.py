cb6b48ecf2c7d98353b590afddaf5429a2a6f7d6
Torsten Simon <simon@edu-sharing.net>
2020-04-17 16:03:01 +0200

    fix db connector do not reopen new connections

--- etl/converter/db_connector.py
+++ etl/converter/db_connector.py
@@ -1,32 +1,35 @@
 import psycopg2
 
 class Database:
+    conn = None
+    curr = None
     def __init__(self):
         self.create_connection()
 
     def create_connection(self):
-        self.conn = psycopg2.connect(
-            host = 'localhost',
-            user = 'search',
-            password = 'admin',
-            database = 'search'
-        )
-        self.curr = self.conn.cursor()
+        if Database.conn == None:
+            Database.conn = psycopg2.connect(
+                host = 'localhost',
+                user = 'search',
+                password = 'admin',
+                database = 'search'
+            )
+            Database.curr = self.conn.cursor()
     def findItem(self, id, spider):
-        self.curr.execute("""SELECT uuid, hash FROM "references" WHERE source = %s AND source_id = %s""", (
+        Database.curr.execute("""SELECT uuid, hash FROM "references" WHERE source = %s AND source_id = %s""", (
             spider.name,
             str(id)
         ))
-        data = self.curr.fetchall()
+        data = Database.curr.fetchall()
         if(len(data)):
             return data[0]
         else:
             return None
     def findSource(self, spider):
-        self.curr.execute("""SELECT * FROM "sources" WHERE id = %s""", (
+        Database.curr.execute("""SELECT * FROM "sources" WHERE id = %s""", (
             spider.name,
         ))
-        data = self.curr.fetchall()
+        data = Database.curr.fetchall()
         if(len(data)):
             return data[0]
         else:
