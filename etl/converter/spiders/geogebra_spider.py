326a3d2eabcc18cccf9ac0bf410ef3093fca1f44
Torsten Simon <simon@edu-sharing.net>
2020-04-22 17:00:18 +0200

    add geogebra spider

---
+++ etl/converter/spiders/geogebra_spider.py
@@ -0,0 +1,97 @@
+from scrapy.spiders import CrawlSpider
+from converter.items import *
+from converter.constants import Constants
+from datetime import datetime
+from w3lib.html import remove_tags, replace_escape_chars
+from converter.spiders.lom_base import LomBase;
+from converter.spiders.json_base import JSONBase;
+import json
+import time
+
+# spider for GeoGebra
+class GeoGebraSpider(CrawlSpider, LomBase, JSONBase):
+  name='geogebra_spider'
+  friendlyName = 'GeoGebra'
+  url = 'https://www.geogebra.org'
+  version = '0.1'
+  start_urls = [
+    'https://www.geogebra.org/m-sitemap-1.xml'
+  ]
+
+  apiUrl = 'https://api.geogebra.org/v1.0/materials/%id?scope=extended&embed=creator,tags,topics'
+
+  def get(self, *params, response):
+    data = json.loads(response.body_as_unicode())
+    return JSONBase.get(self, *params, json = data)
+
+  def parse(self, response):
+    i = 0
+    for url in response.xpath('//*[name()="url"]/*[name()="loc"]//text()').getall():
+      split = url.split('/')
+      id = split[len(split)-1]
+      apiCall = self.apiUrl.replace('%id', id)
+      yield scrapy.Request(url = apiCall, callback = self.parseEntry, meta = {'url': url})
+      i += 1
+      if i>50:
+        return
+
+
+  def parseEntry(self, response):
+    if self.get('language', response = response) == 'de':
+      return LomBase.parse(self, response)
+    return None
+  
+  def getId(self, response):
+    return self.get('id', response = response)
+
+  def getHash(self, response):
+    return self.version + str(self.get('date_modified', response = response))
+
+  def getBase(self, response):
+    base = LomBase.getBase(self, response)
+    #print(response.url)
+    #print(self.get('thumbUrl', response = response))
+    #print(self.get('thumbUrl', response = response).replace('$1', '@l'))
+    base.add_value('thumbnail', self.get('thumbUrl', response = response).replace('$1', '@l'))
+    base.add_value('lastModified', self.get('date_modified', response = response))
+    return base
+
+  def getLOMGeneral(self, response):
+    general = LomBase.getLOMGeneral(self, response)
+    general.add_value('identifier', self.get('id', response = response))
+    general.add_value('title', self.get('title', response = response))
+    general.add_value('keyword', self.get('keywords', response = response))
+    general.add_value('language', self.get('language', response = response))
+    return general
+  def getValuespaces(self, response):
+    valuespaces = LomBase.getValuespaces(self, response)
+    valuespaces.add_value('discipline', self.get('topics',  response = response))
+    t = self.get('type', response = response)
+    if t == 'ws':
+      valuespaces.add_value('learningResourceType', 'worksheet')
+    if t == 'book':
+      valuespaces.add_value('learningResourceType', 'text')
+    c = self.get('categories', response = response)
+    if 'game' in c:
+      valuespaces.add_value('learningResourceType', 'educational_game')
+    if 'practice' in c:
+      valuespaces.add_value('learningResourceType', 'drill_and_practice')
+    return valuespaces
+
+  def getLOMEducational(self, response):
+    educational = LomBase.getLOMEducational(self, response)
+    educational.add_value('description', self.get('description', response = response))
+    #educational.add_value('typicalLearningTime', self.get('timeRequired'))
+    return educational
+
+  def getLOMRights(self, response):
+    rights = LomBase.getLOMRights(self, response)
+    rights.add_value('description', Constants.LICENSE_CC_BY_SA_30)
+    return rights
+
+  def getLOMTechnical(self, response):
+    technical = LomBase.getLOMTechnical(self, response)
+    technical.add_value('format', self.get('fileFormat', response = response))
+    technical.add_value('size', self.get('ContentSize', response = response))
+    technical.add_value('location', response.meta['url'])
+    return technical
