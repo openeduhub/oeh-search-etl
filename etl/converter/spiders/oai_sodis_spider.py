32e5460037e0889ce6942be867618d88aef7b1ce
Daniel Rudolph <rudolph@metaventis.com>
2020-04-29 18:24:39 +0200

    oai sodis spider

---
+++ etl/converter/spiders/oai_sodis_spider.py
@@ -0,0 +1,12 @@
+from converter.spiders.oai_base import OAIBase
+
+class OAISodis(OAIBase):
+    verb="listIdentifiers"
+    baseUrl = "https://sodis.de/cp/oai_pmh/oai.php"
+    metadataPrefix = "oai_lom-de"
+    set = "oer_mebis_activated"
+
+    name="OAISodis"
+    friendlyName='OAI Sodis'
+    url = baseUrl
+    version = '0.1'
