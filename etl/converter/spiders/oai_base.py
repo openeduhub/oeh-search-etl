863d21eaa49b680bdd304384b89b01222396723a
Daniel Rudolph <rudolph@metaventis.com>
2020-04-29 18:22:00 +0200

    oai base more metadata

--- etl/converter/spiders/oai_base.py
+++ etl/converter/spiders/oai_base.py
@@ -1,6 +1,8 @@
 from converter.items import *
 from converter.spiders.lom_base import LomBase;
 import logging
+import vobject
+
 class OAIBase(scrapy.Spider, LomBase):
     verb="ListIdentifiers"
     baseUrl = None
@@ -18,6 +20,7 @@ class OAIBase(scrapy.Spider, LomBase):
 
             identifier = header.xpath('identifier//text()').extract_first()
             getrecordUrl = self.baseUrl +"?verb=GetRecord&identifier=" +identifier+"&metadataPrefix="+self.metadataPrefix
+            self.logger.info('getrecordUrl: %s', getrecordUrl)
             yield scrapy.Request(url=getrecordUrl, callback=self.parseRecord)
 
         resumptionToken = response.xpath('//OAI-PMH/ListIdentifiers/resumptionToken//text()').extract_first()
@@ -49,4 +52,65 @@ class OAIBase(scrapy.Spider, LomBase):
         keywords = record.xpath('metadata/lom/general/keyword/string//text()').getall()
         general.add_value('keyword', keywords )
         return general
+
+    def getLOMEducational(self, response):
+        response.selector.remove_namespaces()
+        record = response.xpath('//OAI-PMH/GetRecord/record')
+
+        educational = LomBase.getLOMEducational(response)
+        #TODO put in general description
+        educational.add_value('description', record.xpath('metadata/lom/general/description/string//text()').extract_first())
+        tarString = record.xpath('metadata/lom/educational/typicalAgeRange/string//text()').extract_first()
+        if tarString:
+            tar = LomAgeRangeItemLoader()
+            tarSplitted = tarString.split('-')
+            if len(tarSplitted) > 1:
+                tar.add_value('fromRange',tarSplitted[0])
+                tar.add_value('toRange',tarSplitted[1])
+                educational.add_value('typicalAgeRange',tar.load_item())
+            else:
+                self.logger.info('unknown agerange %s',tarString) 
+        educational.add_value('language',record.xpath('metadata/lom/educational/language//text()').extract_first())
+        return educational
+    
+    def getLOMTechnical(self, response):
+        response.selector.remove_namespaces()
+        record = response.xpath('//OAI-PMH/GetRecord/record')
+
+        technical = LomBase.getLOMTechnical(response)
+        technical.add_value('format', record.xpath('metadata/lom/technical/format//text()').extract_first())
+        technical.add_value('size', record.xpath('metadata/lom/technical/size//text()').extract_first())
+        technical.add_value('size', record.xpath('metadata/lom/technical/location//text()').extract_first())
+        return technical
+
+    def getLOMLifecycle(self, response):
+        response.selector.remove_namespaces()
+        record = response.xpath('//OAI-PMH/GetRecord/record')
+
+        role = record.xpath('metadata/lom/lifeCycle/contribute/role/value//text()').extract_first()
+        lifecycle = LomBase.getLOMLifecycle(response)
+        lifecycle.add_value('role',role)
+        entity = record.xpath('metadata/lom/lifeCycle/contribute/entity//text()').extract_first()
+        if entity:
+            vcard = vobject.readOne(entity)
+            given = vcard.n.value.given
+            family = vcard.n.value.family
+            lifecycle.add_value('firstName',given)
+            lifecycle.add_value('lastName',family)
+        return lifecycle
+        
+
+
+    def getValuespaces(self, response):
+        valuespaces = LomBase.getValuespaces(self, response)
+        record = response.xpath('//OAI-PMH/GetRecord/record')
+
+        lrts = record.xpath('metadata/lom/educational/learningResourceType/value//text()').getall()
+        valuespaces.add_value('learningResourceType', lrts)
+        ier = record.xpath('metadata/lom/educational/intendedEndUserRole/value//text()').getall()
+        valuespaces.add_value('intendedEndUserRole', ier)
+        context = record.xpath('metadata/lom/educational/context/value//text()').getall()
+        valuespaces.add_value('educationalContext',context)
+
+        return valuespaces
 
