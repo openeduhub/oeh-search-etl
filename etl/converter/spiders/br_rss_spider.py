27c5e522fc272e274e3a0aec36b697bd154ffb5d
Torsten Simon <simon@edu-sharing.net>
2020-05-07 15:50:18 +0200

    add gsheet reader / converter for contents revamp csv reading

--- etl/converter/spiders/br_rss_spider.py
+++ etl/converter/spiders/br_rss_spider.py
@@ -17,7 +17,7 @@ class BRRSSSpider(RSSListBase):
   name='br_rss_spider'
   friendlyName='Bayerischer Rundfunk'
   url = 'https://www.br.de/'
-  version = '0.1'
+  version = '0.1.0'
 
-  def __init__(self):
-    RSSListBase.__init__(self, 'csv/br_rss.csv')
+  def __init__(self, **kwargs):
+    RSSListBase.__init__(self, 'csv/br_rss.csv', **kwargs)
--- etl/converter/spiders/rss_list_base.py
+++ etl/converter/spiders/rss_list_base.py
@@ -5,25 +5,24 @@ import logging
 from w3lib.html import remove_tags, replace_escape_chars
 from converter.spiders.lom_base import LomBase;
 from converter.spiders.rss_base import RSSBase;
+from converter.spiders.csv_base import CSVBase;
 from converter.valuespace_helper import ValuespaceHelper;
 import csv
 import os
 # rss crawler with a list of entries to crawl and map
+# for supported columns, please check the CSVbase class
 class RSSListBase(RSSBase, LomBase):
-    # column names supported:
-    COLUMN_URL = 'url'
-    COLUMN_TYPICAL_AGE_RANGE_FROM = 'typicalAgeRangeFrom'
-    COLUMN_TYPICAL_AGE_RANGE_TO = 'typicalAgeRangeTo'
-    COLUMN_TYPICAL_DISCIPLINE = 'discipline'
-    COLUMN_LEARNING_RESOURCE_TYPE = 'learningResourceType'
-    COLUMN_LANGUAGE = 'language'
-    COLUMN_LICENSE = 'license'
     mappings = {}
     start_urls = []
     rows = {}
     def getCSVValue(self, response, fieldName):
-        return list(map(lambda x: x.strip(),response.meta['row'][self.mappings[fieldName]].split(";")))
-    def __init__(self, file, delimiter = ','):
+        data = list(map(lambda x: x.strip(),response.meta['row'][self.mappings[fieldName]].split(";")))
+        if len(list(filter(lambda x: x != '', data))) > 0:
+            return data
+        return None
+
+    def __init__(self, file, delimiter = ',', **kwargs):
+        LomBase.__init__(self, **kwargs)
         dir = os.path.dirname(os.path.realpath(__file__))
         with open(dir + '/../' + file) as csvFile:
             csvReader = csv.reader(csvFile, delimiter=delimiter)
@@ -36,7 +35,7 @@ class RSSListBase(RSSBase, LomBase):
                         j += 1
                     i += 1
                     continue
-                url = row[self.mappings[RSSListBase.COLUMN_URL]]
+                url = row[self.mappings[CSVBase.COLUMN_URL]]
                 self.rows[url] = row
                 #self.start_urls.append(url)
                 i += 1
@@ -48,21 +47,22 @@ class RSSListBase(RSSBase, LomBase):
 
     def getLOMGeneral(self, response):
         general = RSSBase.getLOMGeneral(self, response)
-        general.replace_value('language', self.getCSVValue(response, RSSListBase.COLUMN_LANGUAGE))
+        general.replace_value('language', self.getCSVValue(response, CSVBase.COLUMN_LANGUAGE))
+        general.replace_value('keyword', self.getCSVValue(response, CSVBase.COLUMN_KEYWORD))
         return general
   
     def getLicense(self, response):
         license = LomBase.getLicense(self, response)
-        license.add_value('internal', self.getCSVValue(response, RSSListBase.COLUMN_LICENSE))
+        license.add_value('internal', self.getCSVValue(response, CSVBase.COLUMN_LICENSE))
         return license
 
     def getValuespaces(self, response):
         valuespaces = RSSBase.getValuespaces(self, response)
         valuespaces.add_value('educationalContext', ValuespaceHelper.educationalContextByAgeRange([
-            self.getCSVValue(response, RSSListBase.COLUMN_TYPICAL_AGE_RANGE_FROM)[0], 
-            self.getCSVValue(response, RSSListBase.COLUMN_TYPICAL_AGE_RANGE_TO)[0]
+            self.getCSVValue(response, CSVBase.COLUMN_TYPICAL_AGE_RANGE_FROM)[0], 
+            self.getCSVValue(response, CSVBase.COLUMN_TYPICAL_AGE_RANGE_TO)[0]
             ]))
         
-        valuespaces.add_value('discipline', self.getCSVValue(response, RSSListBase.COLUMN_TYPICAL_DISCIPLINE))
-        valuespaces.add_value('learningResourceType', self.getCSVValue(response, RSSListBase.COLUMN_LEARNING_RESOURCE_TYPE))
+        valuespaces.add_value('discipline', self.getCSVValue(response, CSVBase.COLUMN_TYPICAL_DISCIPLINE))
+        valuespaces.add_value('learningResourceType', self.getCSVValue(response, CSVBase.COLUMN_LEARNING_RESOURCE_TYPE))
         return valuespaces
