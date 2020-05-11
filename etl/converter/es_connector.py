6333c7956311d6ba54cad5ecbe021cbc0f7b10d0
Torsten Simon <simon@edu-sharing.net>
2020-05-11 19:58:55 +0200

    add es-connector

---
+++ etl/converter/es_connector.py
@@ -0,0 +1,124 @@
+import psycopg2
+import uuid
+import requests
+import json
+import base64
+from scrapy.utils.project import get_project_settings
+from requests.auth import HTTPBasicAuth
+from io import BytesIO
+
+class EduSharing:
+    spiderNodes = {}
+    cookie = None
+    etlFolder = None
+    def __init__(self):
+        self.loadSession()
+    def getHeaders(self, contentType = 'application/json'):
+        return { 'COOKIE' : EduSharing.cookie, 'Accept' : 'application/json', 'Content-Type' : contentType}
+    def createNode(self, parentUUID, type, properties):
+        response = requests.post(get_project_settings().get('EDU_SHARING_BASE_URL') + 'rest/node/v1/nodes/-home-/' + parentUUID + '/children?type=' + type + '&renameIfExists=true', 
+                    headers = self.getHeaders(),
+                    data = json.dumps(properties))
+        return json.loads(response.text)['node']
+    def setNodeText(self, uuid, item):
+        if 'fulltext' in item:
+            response = requests.post(get_project_settings().get('EDU_SHARING_BASE_URL') + 'rest/node/v1/nodes/-home-/' + uuid + '/textContent?mimetype = text/plain', 
+                        headers = self.getHeaders(None),
+                        data = item['fulltext'].encode('utf-8'))
+            return response.status_code == 200
+
+    def setNodePreview(self, uuid, item):
+        key = 'large' if 'large' in item['thumbnail'] else 'small'
+        files = {'image': base64.b64decode(item['thumbnail'][key])}
+        response = requests.post(get_project_settings().get('EDU_SHARING_BASE_URL') + 'rest/node/v1/nodes/-home-/' + uuid + '/preview?mimetype=' + item['thumbnail']['mimetype'], 
+                    headers = self.getHeaders(None),
+                    files = files)
+        return response.status_code == 200
+    
+    def findNodeByName(self, parentUUID, name):
+        response = requests.get(get_project_settings().get('EDU_SHARING_BASE_URL') + 'rest/node/v1/nodes/-home-/' + parentUUID + '/children?maxItems=10000', 
+                    headers = self.getHeaders())
+        result = list(filter(lambda x: x['name'] == name, json.loads(response.text)['nodes']))
+        if len(result) == 1:
+            return result[0]
+        return None
+
+    def transformItem(self, uuid, item):
+        spaces = {
+            'cm:name' : item['lom']['general']['title'] + '_' + uuid,
+            'ccm:wwwurl' : item['lom']['technical']['location'],
+            'cclom:location' : item['lom']['technical']['location'],
+            'cclom:general_title' : item['lom']['general']['title'],
+            'cclom:general_description' : item['lom']['general']['description'],
+        }
+        if 'language' in item['lom']['general']:
+            spaces['cclom:general_language'] = item['lom']['general']['language']
+
+        if 'keyword' in item['lom']['general']:
+            spaces['cclom:general_keyword'] = item['lom']['general']['keyword'],
+
+        valuespaceMapping = {
+            'discipline' : 'ccm:taxonid',
+            'intendedEndUserRole' : 'ccm:educationalintendedenduserrole',
+            'educationalContext' : 'ccm:educationalcontext',
+            'learningResourceType' : 'ccm:learningResourceType',
+            'sourceContentType' : 'ccm:sourceContentType', # @TODO find suited data field
+        }
+        for key in item['valuespaces']:
+            spaces[valuespaceMapping[key]] = item['valuespaces'][key]
+            
+        if 'typicalagerange' in item['lom']['educational']:
+            spaces['ccm:educationaltypicalagerange_from'] = item['lom']['educational']['typicalagerange']['from']
+            spaces['ccm:educationaltypicalagerange_to'] = item['lom']['educational']['typicalagerange']['to']
+        # intendedEndUserRole = Field(output_processor=JoinMultivalues())
+        # discipline = Field(output_processor=JoinMultivalues())
+        # educationalContext = Field(output_processor=JoinMultivalues())
+        # learningResourceType = Field(output_processor=JoinMultivalues())
+        # sourceContentType = Field(output_processor=JoinMultivalues())
+        for key in spaces:
+            if type(spaces[key]) is tuple:
+                spaces[key] = list([x for y in spaces[key] for x in y])
+            if not type(spaces[key]) is list:
+                spaces[key] = [spaces[key]]
+        return spaces
+
+    def insertItem(self, spider, uuid, item):
+        node = self.createNode(EduSharing.spiderNodes[spider.name]['ref']['id'], 'ccm:io' ,self.transformItem(uuid, item))
+        self.setNodePreview(node['ref']['id'], item)
+        self.setNodeText(node['ref']['id'], item)
+
+
+    def loadSession(self):
+        if EduSharing.cookie == None:
+            settings = get_project_settings()
+            auth = requests.get(settings.get('EDU_SHARING_BASE_URL') + 'rest/authentication/v1/validateSession', 
+                    auth = HTTPBasicAuth(settings.get('EDU_SHARING_USERNAME'), settings.get('EDU_SHARING_PASSWORD')),
+                    headers = { 'Accept' : 'application/json'}
+            )
+            isAdmin = json.loads(auth.text)['isAdmin']
+            if isAdmin:
+                EduSharing.cookie = auth.headers['SET-COOKIE'].split(';')[0]
+                sys = self.findNodeByName('-userhome-', 'Edu_Sharing_System')
+                EduSharing.etlFolder = self.findNodeByName(sys['ref']['id'], 'ETL')
+                if EduSharing.etlFolder == None:
+                    EduSharing.etlFolder = self.createNode(sys['ref']['id'], 'ccm:map' ,{ 'cm:name' : ['ETL']})
+                return
+            raise Exception('Could not authentify as admin at edu-sharing. Please check your settings for repository ' + settings.get('EDU_SHARING_BASE_URL'))
+            
+    def buildUUID(self, url):
+        return str(uuid.uuid5(uuid.NAMESPACE_URL, url))
+
+    def uuidExists(self, uuid):
+        return False
+
+    def findItem(self, id, spider):
+        return None
+    def findSource(self, spider):
+        src = self.findNodeByName(EduSharing.etlFolder['ref']['id'], spider.name)
+        if src != None:
+            EduSharing.spiderNodes[spider.name] = src
+        return src
+    def createSource(self, spider):
+        src = self.createNode(EduSharing.etlFolder['ref']['id'], 'ccm:map', {'cm:name' : [spider.name]})
+        EduSharing.spiderNodes[spider.name] = src
+        return src
