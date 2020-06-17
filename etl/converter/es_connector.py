88144c54fdc1668e103c1f2bd769cabef1581246
Torsten Simon <simon@edu-sharing.net>
2020-06-17 16:15:53 +0200

    es map internal copyright license

--- etl/converter/es_connector.py
+++ etl/converter/es_connector.py
@@ -48,7 +48,7 @@ class EduSharing:
                 spaces['ccm:commonlicense_key'] = 'CC_0'
                 spaces['ccm:commonlicense_cc_version'] = '1.0'
         if 'internal' in license:
-            if license['internal'] )) Constants.LICENSE_COPYRIGHT_LAW:
+            if license['internal'] == Constants.LICENSE_COPYRIGHT_LAW:
                 spaces['ccm:commonlicense_key'] = 'COPYRIGHT_FREE'
 
     def transformItem(self, uuid, spider, item):
