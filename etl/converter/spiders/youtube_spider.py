665f1a2c3f996ad501f7e8f780d681347795b6a4
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-08-12 11:47:40 +0200

    refine youtube spider
    - don't inherit LomBase
    - add some more information

--- etl/converter/spiders/youtube_spider.py
+++ etl/converter/spiders/youtube_spider.py
@@ -15,6 +15,14 @@ import converter.env as env
 import converter.items as items
 from converter.spiders.lom_base import LomBase
 
+# TODO: Find suitable target field for channel/playlist information:
+#   - Title
+#   - Description
+#   - URL
+#
+# TODO: Find out whether `publishedAt` reflects modification
+#   - Find another way to set `hash` if not
+#
 # Unhandled csv columns:
 #   - sourceTitle
 #     TODO: Find suitable target field
@@ -22,10 +30,6 @@ from converter.spiders.lom_base import LomBase
 #   - typicalAgeRangeFrom
 #   - typicalAgeRangeTo
 #     TODO: Replace with educationalContext
-#
-# TODO: Find suitable target field for channel/playlist URL
-#
-# We could also consider adding information about the channel to videos.
 #
 # Example item:
 # ```
@@ -60,86 +64,57 @@ from converter.spiders.lom_base import LomBase
 # ```
 
 
-class YoutubeSpider(Spider, LomBase):
+class YoutubeSpider(Spider):
     name = "youtube_spider"
     friendlyName = "Youtube"
     url = "https://www.youtube.com/"
     version = "0.1.0"
 
+    @staticmethod
+    def get_video_url(item: dict) -> str:
+        assert item["snippet"]["resourceId"]["kind"] == "youtube#video"
+        return "https://www.youtube.com/watch?v={}".format(
+            item["snippet"]["resourceId"]["videoId"]
+        )
+
+    @staticmethod
+    def update_url_query(url: str, params: dict) -> str:
+        """Take a url and update selected query parameters."""
+        url_parts = list(urlparse(url))
+        query = dict(parse_qsl(url_parts[4]))
+        query.update(params)
+        url_parts[4] = urlencode(query)
+        return urlunparse(url_parts)
+
+    @staticmethod
+    def get_csv_rows(filename: str) -> Generator[dict, None, None]:
+        csv_file_path = os.path.realpath(
+            os.path.join(os.path.dirname(__file__), "..", "..", "csv", filename)
+        )
+        with open(csv_file_path, newline="") as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                yield row
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.lomLoader = YoutubeLomLoader(self.name, self.version, **kwargs)
+
     @overrides  # Spider
     def start_requests(self):
-        for row in get_csv_rows("youtube.csv"):
-            if (request := self._request_row(row)) is not None:
+        for row in YoutubeSpider.get_csv_rows("youtube.csv"):
+            if (request := self.request_row(row)) is not None:
                 yield request
 
-    @overrides  # LomBase
-    def getId(self, response: Response) -> str:
-        return self._get_video_url(response.meta["item"])
-
-    @overrides  # LomBase
-    def getHash(self, response: Response) -> str:
-        return self.version + response.meta["item"]["snippet"]["publishedAt"]
-
-    @overrides  # LomBase
-    def getBase(self, response: Response) -> items.BaseItemLoader:
-        base = LomBase.getBase(self, response)
-        thumbnails = response.meta["item"]["snippet"]["thumbnails"]
-        thumbnail = (
-            thumbnails["maxres"]
-            if "maxres" in thumbnails
-            else thumbnails["standard"]
-            if "standard" in thumbnails
-            else thumbnails["high"]
-        )
-        base.add_value("thumbnail", thumbnail["url"])
-        return base
-
-    @overrides  # LomBase
-    def getLOMGeneral(self, response: Response) -> items.LomGeneralItemloader:
-        general = LomBase.getLOMGeneral(self, response)
-        general.add_value("title", response.meta["item"]["snippet"]["title"])
-        general.add_value(
-            "description", response.meta["item"]["snippet"]["description"]
-        )
-        general.add_value("keyword", parse_csv_field(response.meta["row"]["keyword"]))
-        general.add_value("language", parse_csv_field(response.meta["row"]["language"]))
-        return general
-
-    @overrides  # LomBase
-    def getLOMTechnical(self, response: Response) -> items.LomTechnicalItemLoader:
-        technical = LomBase.getLOMTechnical(self, response)
-        technical.add_value("format", "text/html")
-        technical.add_value("location", self._get_video_url(response.meta["item"]))
-        return technical
-
-    @overrides  # LomBase
-    def getLicense(self, response: Response) -> items.LicenseItemLoader:
-        license = LomBase.getLicense(self, response)
-        license.add_value("internal", parse_csv_field(response.meta["row"]["license"]))
-        return license
-
-    @overrides  # LomBase
-    def getValuespaces(self, response: Response) -> items.ValuespaceItemLoader:
-        valuespaces = LomBase.getValuespaces(self, response)
-        row = response.meta["row"]
-        valuespaces.add_value(
-            "learningResourceType", parse_csv_field(row["learningResourceType"])
-        )
-        valuespaces.add_value("discipline", parse_csv_field(row["discipline"]))
-        valuespaces.add_value(
-            "intendedEndUserRole", parse_csv_field(row["intendedEndUserRole"])
-        )
-        return valuespaces
-
-    def _request_row(self, row: dict) -> Request:
+    def request_row(self, row: dict) -> Request:
         if row["url"].startswith("https://www.youtube.com"):
             url = urlparse(row["url"])
             if url.path == "/playlist":
                 playlist_id = dict(parse_qsl(url.query))["list"]
-                return self._request_playlist(playlist_id, meta={"row": row})
+                return self.request_playlist(playlist_id, meta={"row": row})
             elif url.path.startswith("/channel/"):
                 channel_id = url.path.split("/")[2]
-                return self._request_channel(channel_id, meta={"row": row})
+                return self.request_channel(channel_id, meta={"row": row})
             else:
                 # Youtube offers custom URLs to popular channels of the form
                 #   - https://www.youtube.com/c/<custom channel name>
@@ -150,10 +125,10 @@ class YoutubeSpider(Spider, LomBase):
                 # All of these lead to an ordinary channel, but we need to read its ID from the page
                 # body.
                 return Request(
-                    row["url"], meta={"row": row}, callback=self._parse_custom_url,
+                    row["url"], meta={"row": row}, callback=self.parse_custom_url,
                 )
 
-    def _request_channel(self, channel_id: str, meta: dict) -> Request:
+    def request_channel(self, channel_id: str, meta: dict) -> Request:
         part = ["snippet", "contentDetails", "statistics"]
         request_url = (
             "https://www.googleapis.com/youtube/v3/channels"
@@ -161,16 +136,34 @@ class YoutubeSpider(Spider, LomBase):
                 "%2C".join(part), channel_id, env.get("YOUTUBE_API_KEY")
             )
         )
-        return Request(url=request_url, meta=meta, callback=self._parse_channel)
+        return Request(url=request_url, meta=meta, callback=self.parse_channel)
 
-    def _parse_channel(self, response: Response) -> Request:
+    def parse_channel(self, response: Response) -> Request:
         body = json.loads(response.body)
         assert body["kind"] == "youtube#channelListResponse"
         assert body["pageInfo"]["totalResults"] == 1
         playlist_id = body["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
-        return self._request_playlist(playlist_id, response.meta)
+        response.meta["channel"] = body["items"][0]
+        return self.request_playlist(playlist_id, response.meta)
 
-    def _request_playlist(self, playlist_id: str, meta: dict) -> Request:
+    def request_playlist(self, playlist_id: str, meta: dict) -> Request:
+        part = ["snippet"]
+        request_url = (
+            "https://www.googleapis.com/youtube/v3/playlists"
+            + "?part={}&id={}&key={}".format(
+                "%2C".join(part), playlist_id, env.get("YOUTUBE_API_KEY"),
+            )
+        )
+        return Request(request_url, meta=meta, callback=self.parse_playlist)
+
+    def parse_playlist(self, response: Response):
+        body = json.loads(response.body)
+        assert body["kind"] == "youtube#playlistListResponse"
+        assert body["pageInfo"]["totalResults"] == 1
+        response.meta["playlist"] = body["items"][0]
+        return self.request_playlist_items(body["items"][0]["id"], response.meta)
+
+    def request_playlist_items(self, playlist_id: str, meta: dict) -> Request:
         part = ["snippet"]
         request_url = (
             "https://www.googleapis.com/youtube/v3/playlistItems"
@@ -178,60 +171,143 @@ class YoutubeSpider(Spider, LomBase):
                 "%2C".join(part), playlist_id, env.get("YOUTUBE_API_KEY"),
             )
         )
-        return Request(request_url, meta=meta, callback=self._parse_playlist_items)
+        return Request(request_url, meta=meta, callback=self.parse_playlist_items)
 
-    def _parse_playlist_items(self, response: Response):
+    def parse_playlist_items(self, response: Response):
         body = json.loads(response.body)
         assert body["kind"] == "youtube#playlistItemListResponse"
-        body = json.loads(response.body)
+        for item in body["items"]:
+            response_copy = response.replace(url=self.get_video_url(item))
+            response_copy.meta["item"] = item
+            yield self.lomLoader.parse(response_copy)
         if "nextPageToken" in body:
-            request_url = update_url_query(
+            request_url = YoutubeSpider.update_url_query(
                 response.url, {"pageToken": body["nextPageToken"]}
             )
             yield response.follow(
-                request_url, meta=response.meta, callback=self._parse_playlist_items
+                request_url, meta=response.meta, callback=self.parse_playlist_items
             )
-        for item in body["items"]:
-            response_copy = response.replace(url=self._get_video_url(item))
-            response_copy.meta["item"] = item
-            yield LomBase.parse(self, response_copy)
 
-    def _parse_custom_url(self, response: Response) -> Request:
+    def parse_custom_url(self, response: Response) -> Request:
         if (
             match := re.search('"externalChannelId":"(.+?)"', response.text)
         ) is not None:
             channel_id = match.group(1)
-            return self._request_channel(channel_id, meta=response.meta)
+            return self.request_channel(channel_id, meta=response.meta)
         logging.warn("Could not extract channel id for {}".format(response.url))
 
-    def _get_video_url(self, item: dict) -> str:
-        assert item["snippet"]["resourceId"]["kind"] == "youtube#video"
-        return "https://www.youtube.com/watch?v={}".format(
-            item["snippet"]["resourceId"]["videoId"]
+
+class YoutubeLomLoader(LomBase):
+    # The `response.meta` field is populated as follows:
+    #   - `row`: The row of the CSV file containing the channel or playlist to be scraped with some
+    #     additional information regarding all found videos.
+    #   - `item`: Information about the video, obtained from the Youtube API.
+    #   - `channel`: Information about the Youtube channel, obtained from the Youtuber API. Only
+    #     populated if an entire channel was given in the CSV row.
+    #   - `playlist`: Information about the Youtube playlist, obtained from the Youtuber API. These
+    #     information are more relevant than the channel information when a specific playlist was
+    #     given in the CSV row. However, when an entire channel was requested, we fall back to the
+    #     `uploads` playlist, which provides only a generated title.
+
+    @staticmethod
+    def parse_csv_field(field: str) -> List[str]:
+        """Parse semicolon-separated string."""
+        values = [value.strip() for value in field.split(";") if value.strip()]
+        if len(values):
+            return values
+
+    def __init__(self, name, version, **kwargs):
+        self.name = name
+        self.version = version
+        super().__init__(**kwargs)
+
+    @overrides  # LomBase
+    def getId(self, response: Response) -> str:
+        return YoutubeSpider.get_video_url(response.meta["item"])
+
+    @overrides  # LomBase
+    def getHash(self, response: Response) -> str:
+        return self.version + response.meta["item"]["snippet"]["publishedAt"]
+
+    @overrides  # LomBase
+    def getBase(self, response: Response) -> items.BaseItemLoader:
+        base = LomBase.getBase(self, response)
+        base.add_value("lastModified", response.meta["item"]["snippet"]["publishedAt"])
+        base.add_value("thumbnail", self.getThumbnailUrl(response))
+        base.add_value("fulltext", self.getFulltext(response))
+        return base
+
+    def getThumbnailUrl(self, response: Response) -> str:
+        thumbnails = response.meta["item"]["snippet"]["thumbnails"]
+        thumbnail = (
+            thumbnails["maxres"]
+            if "maxres" in thumbnails
+            else thumbnails["standard"]
+            if "standard" in thumbnails
+            else thumbnails["high"]
         )
+        return thumbnail["url"]
 
+    def getFulltext(self, response: Response) -> str:
+        # If `channel` is populated, it has more relevant information than `playlist` (see comments
+        # to `meta` field above).
+        if "channel" in response.meta:
+            channel = response.meta["channel"]["snippet"]
+            fulltext = "\n\n".join([channel["title"], channel["description"]],)
+        else:
+            playlist = response.meta["playlist"]["snippet"]
+            fulltext = "\n\n".join(
+                [playlist["channelTitle"], playlist["title"], playlist["description"],],
+            )
+        return fulltext
 
-def update_url_query(url: str, params: dict) -> str:
-    """Take a url and update selected query parameters."""
-    url_parts = list(urlparse(url))
-    query = dict(parse_qsl(url_parts[4]))
-    query.update(params)
-    url_parts[4] = urlencode(query)
-    return urlunparse(url_parts)
+    @overrides  # LomBase
+    def getLOMGeneral(self, response: Response) -> items.LomGeneralItemloader:
+        general = LomBase.getLOMGeneral(self, response)
+        general.add_value("title", response.meta["item"]["snippet"]["title"])
+        general.add_value("description", self.getDescription(response))
+        general.add_value(
+            "keyword", self.parse_csv_field(response.meta["row"]["keyword"])
+        )
+        general.add_value(
+            "language", self.parse_csv_field(response.meta["row"]["language"])
+        )
+        return general
 
+    def getDescription(self, response: Response) -> str:
+        if (description := response.meta["item"]["snippet"]["description"]) :
+            return description
+        else:
+            # Fall back to playlist title when no description was given.
+            return response.meta["playlist"]["snippet"]["title"]
 
-def get_csv_rows(filename: str) -> Generator[dict, None, None]:
-    csv_file_path = os.path.realpath(
-        os.path.join(os.path.dirname(__file__), "..", "..", "csv", filename)
-    )
-    with open(csv_file_path, newline="") as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            yield row
+    @overrides  # LomBase
+    def getLOMTechnical(self, response: Response) -> items.LomTechnicalItemLoader:
+        technical = LomBase.getLOMTechnical(self, response)
+        technical.add_value("format", "text/html")
+        technical.add_value(
+            "location", YoutubeSpider.get_video_url(response.meta["item"])
+        )
+        return technical
 
+    @overrides  # LomBase
+    def getLicense(self, response: Response) -> items.LicenseItemLoader:
+        license = LomBase.getLicense(self, response)
+        license.add_value(
+            "internal", self.parse_csv_field(response.meta["row"]["license"])
+        )
+        return license
 
-def parse_csv_field(field: str) -> List[str]:
-    """Parse semicolon-separated string."""
-    values = [value.strip() for value in field.split(";") if value.strip()]
-    if len(values):
-        return values
+    @overrides  # LomBase
+    def getValuespaces(self, response: Response) -> items.ValuespaceItemLoader:
+        valuespaces = LomBase.getValuespaces(self, response)
+        row = response.meta["row"]
+        valuespaces.add_value(
+            "learningResourceType", self.parse_csv_field(row["learningResourceType"]),
+        )
+        valuespaces.add_value("discipline", self.parse_csv_field(row["discipline"]))
+        valuespaces.add_value(
+            "intendedEndUserRole", self.parse_csv_field(row["intendedEndUserRole"]),
+        )
+        return valuespaces
+
