dd427e8135434893ee7e16acd5778cbb9bce7c4a
Torsten Simon <simon@edu-sharing.net>
2020-08-27 13:04:17 +0200

    fix:oeh-spider prefer real wwwurl instead of open edu hub

--- etl/converter/spiders/oeh_spider.py
+++ etl/converter/spiders/oeh_spider.py
@@ -8,7 +8,7 @@ class OEHSpider(EduSharingBase):
     friendlyName = "Open Edu Hub"
     url = "https://redaktion.openeduhub.net/edu-sharing/"
     apiUrl = "https://redaktion.openeduhub.net/edu-sharing/rest/"
-    version = "0.1.0"
+    version = "0.1.1"
     mdsId = "mds_oeh"
 
     def __init__(self, **kwargs):
@@ -18,6 +18,15 @@ class OEHSpider(EduSharingBase):
         base = EduSharingBase.getBase(self, response)
         base.replace_value("type", self.getProperty("ccm:objecttype", response))
         return base
+
+
+    def getLOMTechnical(self, response):
+        technical = EduSharingBase.getLOMTechnical(self, response)
+        if "ccm:wwwurl" in response.meta["item"]["properties"]:
+            technical.replace_value("format", "text/html")
+            technical.replace_value("location", response.meta["item"]["properties"]["ccm:wwwurl"][0])
+        return technical
+
 
     def shouldImport(self, response=None):
         if "ccm:collection_io_reference" in response.meta["item"]["aspects"]:
