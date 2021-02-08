cb7f718b9b66847264f08cd5060fb62a945e81e9
David '-1' Schmid <info@david-schmid.de>
2021-02-08 17:02:30 +0100

    clean up imports

--- converter/items.py
+++ converter/items.py
@@ -5,12 +5,10 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
-from scrapy.item import Item, BaseItem, Field
+from scrapy.item import Item, Field
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import Join, MapCompose, TakeFirst
+from itemloaders.processors import TakeFirst
 from w3lib.html import remove_tags, replace_escape_chars
-import logging
 
 
 def replace_processor(value):
