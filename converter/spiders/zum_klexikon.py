8ed8fb9afc195c499b8e50c1e834e118c37412d4
David '-1' Schmid <info@david-schmid.de>
2021-02-09 10:45:40 +0100

    klexikon does not return the pageid on parse, fix tests

--- converter/spiders/zum_klexikon.py
+++ converter/spiders/zum_klexikon.py
@@ -1,8 +1,11 @@
 from converter.items import LomTechnicalItem, LicenseItem, LomGeneralItem, ValuespaceItem
-from .base_classes import MediaWikiBase
+from .base_classes.mediawiki_base import MediaWikiBase, jmes_pageids
 import scrapy
-
+import json
+import jmespath
 from ..constants import Constants
+
+jmes_continue = jmespath.compile('"query-continue".allpages')
 
 
 class ZUMSpider(MediaWikiBase, scrapy.Spider):
@@ -12,11 +15,33 @@ class ZUMSpider(MediaWikiBase, scrapy.Spider):
     version = "0.1.0"
     license = Constants.LICENSE_CC_BY_SA_30
 
-    def technical_item(self, response=None) -> LomTechnicalItem:
+    def parse_page_query(self, response: scrapy.http.Response):
+        """
+        @url https://klexikon.zum.de/api.php?format=json&action=query&list=allpages&aplimit=100&apfilterredir=nonredirects
+        @returns requests 101 101
+        """
+        data = json.loads(response.body)
+        pageids = jmes_pageids.search(data)
+        for pageid in pageids:
+            yield scrapy.FormRequest(
+                url=self.api_url,
+                formdata=self._parse_params | {'pageid': str(pageid)},
+                callback=self.parse_page_data,
+                cb_kwargs={"extra": {'pageid': str(pageid)}}
+            )
+        if 'query-continue' not in data:
+            return
+        yield self.query_for_pages(jmes_continue.search(data))
+
+    def getId(self, response=None):
+        return response.meta['item_extra']['pageid']
+
+    def technical_item(self, response) -> LomTechnicalItem:
         """
         @url https://klexikon.zum.de/api.php?format=json&action=parse&pageid=10031&prop=text|langlinks|categories|links|templates|images|externallinks|sections|revid|displaytitle|iwlinks|properties
         @scrapes format location
         """
+        response.meta['item'] = json.loads(response.body)
         return self.getLOMTechnical(response).load_item()
 
     def license_item(self, response) -> LicenseItem:
@@ -24,13 +49,15 @@ class ZUMSpider(MediaWikiBase, scrapy.Spider):
         @url https://klexikon.zum.de/api.php?format=json&action=parse&pageid=10031&prop=text|langlinks|categories|links|templates|images|externallinks|sections|revid|displaytitle|iwlinks|properties
         @scrapes url
         """
+        response.meta['item'] = json.loads(response.body)
         return self.getLicense(response).load_item()
 
-    def general_item(self, response=None) -> LomGeneralItem:
+    def general_item(self, response) -> LomGeneralItem:
         """
         @url https://klexikon.zum.de/api.php?format=json&action=parse&pageid=4937&prop=text|langlinks|categories|links|templates|images|externallinks|sections|revid|displaytitle|iwlinks|properties
         @scrapes title keyword description
         """
+        response.meta['item'] = json.loads(response.body)
         return self.getLOMGeneral(response).load_item()
 
     def valuespace_item(self, response) -> ValuespaceItem:
@@ -38,4 +65,5 @@ class ZUMSpider(MediaWikiBase, scrapy.Spider):
         @url https://klexikon.zum.de/api.php?format=json&action=parse&pageid=10031&prop=text|langlinks|categories|links|templates|images|externallinks|sections|revid|displaytitle|iwlinks|properties
         @scrapes discipline educationalContext intendedEndUserRole
         """
+        response.meta['item'] = json.loads(response.body)
         return self.getValuespaces(response).load_item()
