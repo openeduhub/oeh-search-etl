8cfe96ba9b508a8a234c894233a845c929596656
Ioannis Koumarelas <ioannis.koumarelas@hpi.de>
2021-02-12 10:45:02 +0100

    SC-8573 cron execution spiders (#23)
    * SC-8573 - Cron execution of spiders:
    
    Modified crawl_schulcloud.sh to suppress output.
    Replacing .env.X (for a specific X) with .env
    Passing environment (dev, prod) as an argument
    Mailing reports spider executions
    Using env var $mailx_recipients to send reports.
    Adding documentation at the top of the script.
    Fixed hardcoded spider's name
    Also added some variables to control whether we want to suppress the output or not, execute using nohup (as a background task), etc.
    
    Co-authored-by: Ioannis Koumarelas <ioannis.koumarelas@gmail.com>

--- crawl_schulcloud.sh
+++ crawl_schulcloud.sh
@@ -1,36 +1,48 @@
 #!/bin/bash
+# Please execute this script in the following way:
+# env mailx_recipients="e-mail1 e-mail2 ... e-mailN" bash crawl_schulcloud.sh
+# TIP: This is how you could include it in a cronjob as well.
 
-# This script is used to execute the spiders, while storing their output to log files.
+working_dir=/root/oeh-search-etl-branches/master_cron/oeh-search-etl
+cd $working_dir
+source .venv/bin/activate
 
-# First we store all spiders in an array variable.
 spiders=(
-	"br_rss"
-	"digitallearninglab"
-	"geogebra"
-	"irights"
-	"leifi"
-	"mediothek_pixiothek"
-	"memucho"
-	"merlin"
-	"oai_sodis"
-	"planet_schule"
-	"rlp"
-	"serlo"
-	"wirlernenonline"
-	"wirlernenonline_gsheet"
-	"zdf_rss"
-	"zoerr"
-	"zum"
+        "mediothek_pixiothek_spider"
+        "merlin_spider"
+        "oeh_spider"
 )
 
-# Print the spiders that wil be executed (for debugging purposes).
-#echo ${spiders[@]}
+print_logo=false
+show_spider_output=false
+
+# dev, prod | WARNING: It assumes the existence of .env.dev and .env.prod in the converter/ directory. Please refer to
+# .env.example for reference environmental variables.
+environment="dev"
+if [[ $# -eq 0 ]] ; then
+  echo 'No environment specified as an argument, defaulting to dev.'
+else
+  environment=$1
+  echo "The environment ${environment} was specified."
+fi
+if ! test -f "converter/.env.$environment"; then
+  echo "converter/.env.$environment does not exist. Exiting..."
+  exit 2
+else
+  echo "Copying converter/.env.$environment to converter/.env"
+  cp "converter/.env.$environment" "converter/.env"
+fi
+
+# Set to true only when $show_spider_output = false. Please prefer to keep to false, at least for crawlings against the
+# production machine. (It causes the execution to run in the background and, thus, multiple spiders will run.)
+use_nohup=false
 
 # Make the directory "nohups" if it does not already exist.
 mkdir -p nohups
 
-echo 
-'
+###################################
+if [ "$print_logo" = true ] ; then
+    echo '
                           (
                            )
                           (
@@ -45,25 +57,48 @@ echo
                  (\ |)   '---'   (| /)
                   ` (|           |) `
                     \)           (/
-   ____  ________  __              _     __              
+   ____  ________  __              _     __
   / __ \/ ____/ / / /  _________  (_)___/ /__  __________
  / / / / __/ / /_/ /  / ___/ __ \/ / __  / _ \/ ___/ ___/
-/ /_/ / /___/ __  /  (__  ) /_/ / / /_/ /  __/ /  (__  ) 
-\____/_____/_/ /_/  /____/ .___/_/\__,_/\___/_/  /____/  
-                        /_/                              		
-'
+/ /_/ / /___/ __  /  (__  ) /_/ / / /_/ /  __/ /  (__  )
+\____/_____/_/ /_/  /____/ .___/_/\__,_/\___/_/  /____/
+                        /_/'
+fi
+
 
 # Execute the spiders.
 for spider in ${spiders[@]}
 do
-	echo "Executing $spider spider."
-	
-	# Execute the spider and save its output to two files: "nohup_SPIDER.out" (individual log) and "nohup.out" (collective logs).
-	#nohup scrapy crawl ${spider}_spider -a resetVersion=true | tee -a nohups/nohup_${spider}.out nohups/nohup.out >/dev/null 2>&1 &
-	#nohup scrapy crawl ${spider}_spider -a cleanrun=true | tee -a nohups/nohup_${spider}.out nohups/nohup.out >/dev/null 2>&1 &
-	nohup scrapy crawl ${spider}_spider | tee -a nohups/nohup_${spider}.out nohups/nohup.out >/dev/null & 2>&1
-	
-	# Execute the spider in the background. 
-	#scrapy crawl ${spider}_spider &
+        echo "Executing $spider spider."
+
+        # Execute the spider
+        if [ "$show_spider_output" = true ] ; then
+          # ... , save its output to "nohup_SPIDER.out", AND print stdout and stderr.
+          scrapy crawl ${spider} -a resetVersion=true | tee -a nohups/nohup_${spider}.out
+        elif [ "$show_spider_output" = false ] && [ "$use_nohup" = true ]; then
+          # Execute the spider and save its output to two files: "nohup_SPIDER.out" (individual log) and "nohup.out"
+          # (collective logs).
+          nohup scrapy crawl ${spider} -a resetVersion=true | tee -a nohups/nohup_${spider}.out \
+                nohups/nohup.out >/dev/null 2>&1 &
+        else # elif [ "$show_spider_output" = false ] && [ "use_nohup" = false ]; then
+          # ... and save its output to "nohup_SPIDER.out".
+          scrapy crawl ${spider} -a resetVersion=true &> nohups/nohup_${spider}.out
+        fi
+
+        echo "Finished execution of $spider spider"
+
+        # If the env var $mailx_recipients is set, please send the report to it. (Could be multiple addresses separated
+        # via a white spaces). e.g., export mailx_recipients="mail1@hpi.de mail2@hpi.de ... mailN@hpi.de"
+        if [ ! -z ${mailx_recipients+x} ]; then
+          echo "Gathering report for $spider spider"
+
+          spider_output=$(tail -n 40 nohups/nohup_${spider}.out)
+          # Remove everything before and including the string 'INFO: Closing spider (finished)'
+          spider_output_statistics="*** Report for ${spider} crawling ***"${spider_output#*"INFO: Closing spider (finished)"}
+          echo "$spider_output_statistics" | mailx -s "${spider} has just finished crawling." ${mailx_recipients}
+
+          echo "Report sent for $spider spider"
+        fi
 done
-echo "Happy crawling! :-)"
+
+echo "Finished with all spiders! :-)"
