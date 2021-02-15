29fb995da72be1086b2e790794c20f5ded010772
einsweniger <info@david-schmid.de>
2021-02-15 19:16:38 +0100

    generated async client library

---
+++ edu_sharing_async/api/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from edu_sharing_async.api.about_api import ABOUTApi
+from edu_sharing_async.api.admin_v1_api import ADMINV1Api
+from edu_sharing_async.api.archive_v1_api import ARCHIVEV1Api
+from edu_sharing_async.api.authentication_v1_api import AUTHENTICATIONV1Api
+from edu_sharing_async.api.bulk_v1_api import BULKV1Api
+from edu_sharing_async.api.clientutils_v1_api import CLIENTUTILSV1Api
+from edu_sharing_async.api.collection_v1_api import COLLECTIONV1Api
+from edu_sharing_async.api.comment_v1_api import COMMENTV1Api
+from edu_sharing_async.api.config_v1_api import CONFIGV1Api
+from edu_sharing_async.api.connector_v1_api import CONNECTORV1Api
+from edu_sharing_async.api.iam_v1_api import IAMV1Api
+from edu_sharing_async.api.mds_v1_api import MDSV1Api
+from edu_sharing_async.api.mediacenter_v1_api import MEDIACENTERV1Api
+from edu_sharing_async.api.network_v1_api import NETWORKV1Api
+from edu_sharing_async.api.node_v1_api import NODEV1Api
+from edu_sharing_async.api.organization_v1_api import ORGANIZATIONV1Api
+from edu_sharing_async.api.rating_v1_api import RATINGV1Api
+from edu_sharing_async.api.register_v1_api import REGISTERV1Api
+from edu_sharing_async.api.rendering_v1_api import RENDERINGV1Api
+from edu_sharing_async.api.search_v1_api import SEARCHV1Api
+from edu_sharing_async.api.sharing_v1_api import SHARINGV1Api
+from edu_sharing_async.api.statistic_v1_api import STATISTICV1Api
+from edu_sharing_async.api.stream_v1_api import STREAMV1Api
+from edu_sharing_async.api.tool_v1_api import TOOLV1Api
+from edu_sharing_async.api.tracking_v1_api import TRACKINGV1Api
+from edu_sharing_async.api.usage_v1_api import USAGEV1Api
