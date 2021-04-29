db09182212045d2068616869f0623790b3c08dca
Criamos <Criamos@googlemail.com>
2021-04-29 17:09:57 +0200

    schule_im_aufbruch_single_video_spider.py: - added lifecycle metadata - first pass on code cleanup and refactoring, more to come

--- converter/spiders/schule_im_aufbruch_single_video_spider.py
+++ converter/spiders/schule_im_aufbruch_single_video_spider.py
@@ -4,7 +4,7 @@ import logging
 
 import scrapy
 
-from converter.items import BaseItemLoader, LomBaseItemloader, LomGeneralItemloader, LomTechnicalItemLoader, \
+from converter.items import BaseItemLoader, LomGeneralItemloader, LomTechnicalItemLoader, \
     LomEducationalItemLoader, ValuespaceItemLoader, LicenseItemLoader, PermissionItemLoader, ResponseItemLoader, \
     LomLifecycleItemloader
 from converter.spiders.base_classes import LomBase
@@ -14,7 +14,7 @@ class SchuleImAufbruchVideoSpider(scrapy.Spider, LomBase):
     name = "sia_single"
     friendlyName = "Schule im Aufbruch (Single Run Spider)"
     url = "https://vimeo.com/412230600"
-    version = "0.0.1"
+    version = "0.1.1"
 
     def __init__(self, **kwargs):
         LomBase.__init__(self, **kwargs)
@@ -38,24 +38,17 @@ class SchuleImAufbruchVideoSpider(scrapy.Spider, LomBase):
         # XPath to description of a video looks like this:
         # //*[@id="main"]/div/main/div/div/div/div[2]/div[3]/div
 
-        # base = BaseItemLoader(response=response)
-        lom = LomBaseItemloader()
         lifecycle = LomLifecycleItemloader()
-        # general = LomGeneralItemloader(response=response)
-        # technical = LomTechnicalItemLoader()
+
         edu = LomEducationalItemLoader()
-        # vs = ValuespaceItemLoader()
-        lic = LicenseItemLoader()
+
         permissions = PermissionItemLoader(response=response)
         response_loader = ResponseItemLoader()
 
         # if ld+json script-container doesn't exist, at least log the error
         if (response.xpath('/html/body/script[1]/text()').get().strip()) is not None:
             # using ld+json to fetch metadata:
-            current_page = response.xpath('/html/body/script[1]/text()').get().strip()
-            current_page_json = json.loads(current_page)
-            # author information is inside a dictionary with schema.org type Person
-            author_dict = current_page_json[1]["itemListElement"][0]["item"]
+            ld_json = self.get_ld_json(response)
 
             current_url = str(response.url)  # making double-sure that we're using a string for sourceID
 
@@ -67,13 +60,6 @@ class SchuleImAufbruchVideoSpider(scrapy.Spider, LomBase):
             #       - third_party_ads_enabled
             # response.xpath('//*[@id="wrap"]/div[2]/script[1]/text()').get()
             # might have to access it by regEx
-
-            # # TODO: LomLifeCycleItemLoader
-            # lifecycle.add_value('organization', author_dict["name"])
-            # lifecycle.add_value('url', author_dict["@id"])
-            # lom.add_value('lifecycle', lifecycle.load_item())
-
-
 
             # TODO: which category does "schule im Aufbruch" fit into? double-check!
             # edu.add_value('language', 'de')  # okay to hardcode this? (some videos are sometimes bilingual, but meta
@@ -94,6 +80,12 @@ class SchuleImAufbruchVideoSpider(scrapy.Spider, LomBase):
             logging.debug("Could not find ld+json script, skipping entry: " + response.url)
 
     @staticmethod
+    def get_ld_json(response: scrapy.http.Response):
+        ld_json_string = response.xpath('/html/body/script[1]/text()').get().strip()
+        ld_json = json.loads(ld_json_string)
+        return ld_json
+
+    @staticmethod
     def get_license(response: scrapy.http.Response = None) -> str:
         """
         grabs the license information from the "about"-button (pop-in)
@@ -109,24 +101,22 @@ class SchuleImAufbruchVideoSpider(scrapy.Spider, LomBase):
 
     def getBase(self, response=None) -> BaseItemLoader:
         base = LomBase.getBase(self, response)
-        current_page = response.xpath('/html/body/script[1]/text()').get().strip()
-        current_page_json = json.loads(current_page)
+        ld_json = self.get_ld_json(response)
         current_url = str(response.url)  # making double-sure that we're using a string for sourceID
         base.add_value('sourceId', current_url)  # this needs to be fixed
-        base.add_value("hash", current_page_json[0]["dateModified"])
+        base.add_value("hash", ld_json[0]["dateModified"])
         # TODO: datetime-conversion necessary?
-        base.add_value("lastModified", current_page_json[0]["dateModified"])
+        base.add_value("lastModified", ld_json[0]["dateModified"])
         # TODO: store thumbnail or thumbnailUrl?
         # url can be acquired from current_page_json[0]["thumbnailUrl"]
-        base.add_value('thumbnail', current_page_json[0]["thumbnailUrl"])
+        base.add_value('thumbnail', ld_json[0]["thumbnailUrl"])
         return base
 
     def getLOMGeneral(self, response=None) -> LomGeneralItemloader:
         general = LomBase.getLOMGeneral(self, response)
-        current_page = response.xpath('/html/body/script[1]/text()').get().strip()
-        current_page_json = json.loads(current_page)
-        general.add_value('title', html.unescape(current_page_json[0]["name"]))
-        general.add_value('description', html.unescape(current_page_json[0]["description"]))
+        ld_json = self.get_ld_json(response)
+        general.add_value('title', html.unescape(ld_json[0]["name"]))
+        general.add_value('description', html.unescape(ld_json[0]["description"]))
         # TODO: general.add_value('keyword', '') set manually if there are no keywords given?
         return general
 
@@ -139,11 +129,10 @@ class SchuleImAufbruchVideoSpider(scrapy.Spider, LomBase):
         # 'stock' (this video is a Vimeo Stock video)
         # 'video' (this video is a standard Vimeo video)
         technical = LomBase.getLOMTechnical(self, response)
-        current_page = response.xpath('/html/body/script[1]/text()').get().strip()
-        current_page_json = json.loads(current_page)
+        ld_json = self.get_ld_json(response)
         # TODO: Make sure that we're grabbing the right type for 'format'
         technical.add_value('format', response.xpath('/html/head/meta[18]/@content').get())
-        technical.add_value('location', current_page_json[0]["url"])
+        technical.add_value('location', ld_json[0]["url"])
         return technical
 
     def getValuespaces(self, response) -> ValuespaceItemLoader:
@@ -162,6 +151,16 @@ class SchuleImAufbruchVideoSpider(scrapy.Spider, LomBase):
         vs.add_value('discipline', '720')  # is this the correct category? (allgemein)
         vs.add_value('learningResourceType', 'video')
         return vs
+
+    def getLOMLifecycle(self, response=None) -> LomLifecycleItemloader:
+        lifecycle = LomBase.getLOMLifecycle(self, response)
+        ld_json = self.get_ld_json(response)
+        # author information is inside a dictionary with schema.org type Person
+        author_dict = ld_json[1]["itemListElement"][0]["item"]
+        # TODO: LomLifeCycleItemLoader
+        lifecycle.add_value('organization', author_dict["name"])
+        lifecycle.add_value('url', author_dict["@id"])
+        return lifecycle
 
     def getLicense(self, response=None) -> LicenseItemLoader:
         lic = LomBase.getLicense(self, response)
