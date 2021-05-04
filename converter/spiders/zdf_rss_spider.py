a262bbf20c531d0f5cd9f28ad4febf8fbc8aeb6d
Torsten Simon <simon@edu-sharing.net>
2021-05-04 10:45:40 +0200

    feat:zdf rss spider expiration dates

--- converter/spiders/zdf_rss_spider.py
+++ converter/spiders/zdf_rss_spider.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
 import logging
 import re
+import requests
 
 import scrapy
 
@@ -21,29 +24,27 @@ class ZDFRSSSpider(RSSListBase):
         license_info.add_value(
             "internal", self.getCSVValue(response, CSVBase.COLUMN_LICENSE)
         )
-        expiration_date = self.get_expiration_date(response.url)
-
-        license_info.add_value('expirationDate', expiration_date)
+        page_content = scrapy.Selector(requests.get(response.url))
+        date = self.get_expiration_date(page_content)
+        if date:
+            license_info.add_value('expirationDate', date)
         return license_info
 
     @staticmethod
-    def get_expiration_date(response) -> str:
+    def get_expiration_date(response) -> Optional[str]:
         # grabbing the expiration date from the "other-infos"-container:
         # XPath: /html/body/div[1]/div/main/article/article[1]/div/div/div[2]/div/div[1]/div/dl[2]/dd
         # using the container-name instead to grab the 'verfügbar bis <datum>'-string:
         logging.debug("current URL inside the get_expiration_date method: ", response)
-        request = scrapy.Request(response,)
-
-        if request is not None:
-            temp_string = request.xpath(
-                '//div[contains(@class,"other-infos")]/dl[2]/dd[contains(@class,"desc-text")]/text()').get().strip()
-            # using RegEx to grab the date only, by using this pattern:
-            # (1-2 digits for the day).(1-2 digits for the month).(4 digits for the year), e.g. 13.04.2026
-            date_reg_ex = re.compile(r'(\d{1,2}).(\d{1,2}).(\d{4})')
-            date_temp = date_reg_ex.search(temp_string)
-            date_only = date_temp.group()
-            return date_only
-        elif request is None:
-            return "could not grab expirationDate, response was None"
-        else:
-            return "expirationDate not found"
+        if response is not None:
+            temp_string = response.xpath(
+                '//div[contains(@class,"other-infos")]/dl[2]/dd[contains(@class,"desc-text")]/text()'
+            ).get()
+            if temp_string is not None:
+                temp_string = temp_string.strip()
+                # using RegEx to grab the date only, by using this pattern:
+                # (1-2 digits for the day).(1-2 digits for the month).(4 digits for the year), e.g. 13.04.2026
+                date_reg_ex = re.compile(r'(\d{1,2}).(\d{1,2}).(\d{4})')
+                date_temp = date_reg_ex.search(temp_string)
+                return date_temp.group()
+        return None
