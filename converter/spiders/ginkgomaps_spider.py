abed4a163f2cf81a27f5c538b3c1d22f44c5a16e
Andreas Schnäpp <schnaepp@edu-sharing.net>
2021-06-15 14:43:24 +0200

    add: remaining metadata - general (thumbnail, language) - lifecycle (date, role, firstName, lastName, organization, url) - educational (interactivityType) - valuespaces (intendedEndUserRole, conditionsOfAccess) - base (response)
    code cleanup (removed debug prints)
    - removed (most) debug prints
    - first set of docs and TypeHints

--- converter/spiders/ginkgomaps_spider.py
+++ converter/spiders/ginkgomaps_spider.py
@@ -5,7 +5,7 @@ import w3lib.html
 
 from converter.constants import Constants
 from converter.items import LomBaseItemloader, LomGeneralItemloader, LomTechnicalItemLoader, LomLifecycleItemloader, \
-    LomEducationalItemLoader, ValuespaceItemLoader, LicenseItemLoader, ResponseItemLoader
+    LomEducationalItemLoader, ValuespaceItemLoader, LicenseItemLoader
 from converter.spiders.base_classes import LomBase
 
 
@@ -68,12 +68,12 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
                         yield scrapy.Request(url=temp_url, callback=self.get_navigation_urls_third_level)
                     if url_depth == 3:
                         yield scrapy.Request(url=temp_url, callback=self.get_navigation_urls_fourth_level)
-                print("The amount of URLs gathered so far: " + str(len(self.navigation_urls)))
+                # print("The amount of URLs gathered so far: " + str(len(self.navigation_urls)))
 
     def check_for_dead_ends_before_parsing(self, response: scrapy.http.Response):
         """
         Checks if the current response.url has already been parsed or is on the "skip_these_urls"-list.
-        If the current url hasn't been parsed already, copies the response and calls parse_page to gather metadata
+        If the current url hasn't been parsed already, copies the response and calls parse to gather metadata
         from the current .html
         :param response:
         :return:
@@ -87,7 +87,9 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
                     if (no_entry_regex.search(table_item.get())) is not None:
                         self.debug_dead_end_counter += 1
                         print("The URL", response.url, "is a 'Bisher kein Eintrag'-dead-end.")
-                        print("Total amount of dead-ends:", self.debug_dead_end_counter)
+                        print("check_for_dead_ends... Method: already parsed URLs =", len(self.debug_parsed_urls),
+                              "| gathered urls =", len(self.navigation_urls), "| skip_these_urls =",
+                              len(self.skip_these_urls), "| Total amount of dead-ends:", self.debug_dead_end_counter)
 
                     # check if the current url has already been parsed:
                     elif (response.url not in self.debug_parsed_urls) and (response is not None):
@@ -97,13 +99,13 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
                             current_regex = re.compile(url_pattern)
                             if current_regex.search(response.url) is not None:
                                 skip_check = True
-                        # if the current url is a "fresh" one, call the parse_page method to extract metadata
+                        # if the current url is a "fresh" one, call the parse method to extract metadata
                         if skip_check is False:
-                            print("URL TO BE PARSED: ", response.url)
+                            # print("URL TO BE PARSED: ", response.url)
                             self.debug_parsed_urls.add(response.url)
                             response_copy = response.copy()
-                            yield from self.parse_page(response_copy)
-                            # yield scrapy.Request(url=response_copy.url, callback=self.parse_page(response_copy))
+                            yield from self.parse(response_copy)
+                            # yield scrapy.Request(url=response_copy.url, callback=self.parse(response_copy))
 
     def getHash(self, response=None) -> str:
         pass
@@ -111,7 +113,7 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
     def getId(self, response=None) -> str:
         pass
 
-    def get_navigation_urls_first_level(self, response, **kwargs):
+    def get_navigation_urls_first_level(self, response: scrapy.http.Response):
         url_depth = 1
 
         yield from self.check_for_dead_ends_before_parsing(response)
@@ -120,7 +122,7 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
         nav_sidebar = response.xpath('/html/body/center/table[1]/tr[2]/td[1]/table')
 
         for item in nav_sidebar.xpath('tr/td[@class="NavCell"]'):
-            nav_name = item.xpath('a[@class="NavLink"]/text()').get()
+            # nav_name = item.xpath('a[@class="NavLink"]/text()').get()
             nav_url = item.xpath('a[@class="NavLink"]/@href').get()
             if nav_url is not None:
                 temp_set.add(nav_url)
@@ -130,7 +132,7 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
 
         yield from self.crawl_next_url_level(diff_set, response, url_depth)
 
-    def get_navigation_urls_second_level(self, response, **kwargs):
+    def get_navigation_urls_second_level(self, response: scrapy.http.Response):
         url_depth = 2
         yield from self.check_for_dead_ends_before_parsing(response)
         temp_set = set()
@@ -138,7 +140,7 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
         nav_sidebar = response.xpath('/html/body/center/table[1]/tr[2]/td[1]/table')
 
         for sub_item in nav_sidebar.xpath('tr/td[@class="SubNavCell"]'):
-            sub_nav_name = sub_item.xpath('a[@class="SubNavLink"]/text()').get()
+            # sub_nav_name = sub_item.xpath('a[@class="SubNavLink"]/text()').get()
             sub_nav_url = sub_item.xpath('a[@class="SubNavLink"]/@href').get()
             if sub_nav_url is not None:
                 temp_set.add(sub_nav_url)
@@ -148,14 +150,14 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
 
         yield from self.crawl_next_url_level(diff_set, response, url_depth)
 
-    def get_navigation_urls_third_level(self, response, **kwargs):
+    def get_navigation_urls_third_level(self, response: scrapy.http.Response):
         url_depth = 3
         yield from self.check_for_dead_ends_before_parsing(response)
         temp_set = set()
 
         nav_sidebar = response.xpath('/html/body/center/table[1]/tr[2]/td[1]/table')
         for sub_sub_item in nav_sidebar.xpath('//td[@class="SubSubNavCell"]'):
-            sub_sub_name = sub_sub_item.xpath('a[@class="SubSubNavLink"]/text()').get()
+            # sub_sub_name = sub_sub_item.xpath('a[@class="SubSubNavLink"]/text()').get()
             sub_sub_url = sub_sub_item.xpath('a[@class="SubSubNavLink"]/@href').get()
             if sub_sub_url is not None:
                 temp_set.add(sub_sub_url)
@@ -165,31 +167,24 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
 
         yield from self.crawl_next_url_level(diff_set, response, url_depth)
 
-    def get_navigation_urls_fourth_level(self, response, **kwargs):
-        url_depth = 4
-        yield from self.check_for_dead_ends_before_parsing(response)
+    def get_navigation_urls_fourth_level(self, response: scrapy.http.Response):
+        # url_depth = 4
         # as of 2021-06-14 there's no urls that go deeper than 4 levels in the nav-sidebar
+        yield from self.check_for_dead_ends_before_parsing(response)
 
         # for debugging purposes:
         # print("fourth level Method: current url = ", str(response.url), " amount of URLs in total: ",
         #       len(self.navigation_urls))
-        #
-        # print("AMOUNT OF ALREADY PARSED URLs: ", len(self.debug_parsed_urls))
-        # print(self.debug_crawled_urls)
-        # pass
 
-    def parse(self, response, **kwargs):
-        pass
-
-    def parse_page(self, response, **kwargs):
+    def parse(self, response: scrapy.http.Response, **kwargs):
         # page_content = scrapy.Selector(requests.get(response.url))
         # making sure that the current url is marked as parsed:
         self.debug_parsed_urls.add(response.url)
 
         # for debugging:
-        print("PARSE_PAGE METHOD: current url = ", response.url)
-        print("PARSE_PAGE Method: crawled URLs = ", len(self.debug_parsed_urls), " gathered urls = ",
-              len(self.navigation_urls))
+        # print("parse METHOD: current url = ", response.url)
+        # print("parse Method: crawled URLs = ", len(self.debug_parsed_urls), " gathered urls = ",
+        #       len(self.navigation_urls))
 
         # IMPORTANT: modern browsers add "tbody"-elements into tables, scrapy doesn't see those tags!
         #   Remember: whatever request you see with the developer tools in your browser, you need to manually remove
@@ -201,6 +196,8 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
 
         table_body = response.xpath('//table[@class="smalltable"]')
         description_temp = str()
+        first_thumbnail = str()
+        # first_thumbnail = str()
         if table_body is not None:
             for table_item in table_body:
                 # print(table_item.get())
@@ -209,39 +206,41 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
                 map_design = table_item.xpath('tr/td[2]/p[1]/text()').get()
                 map_content_heading = table_item.xpath('tr/td[2]/u[2]/text()').get()
                 map_content = table_item.xpath('tr/td[2]/p[2]/text()').get()
-                map_thumbnail = response.urljoin(table_item.xpath('tr/td[1]/a[1]/img/@src').get())
-                map_thumbnail_description = table_item.xpath('tr/td[1]/a[1]/img/@alt').get()
+                # map_thumbnail = response.urljoin(table_item.xpath('tr/td[1]/a[1]/img/@src').get())
+                # map_thumbnail_description = table_item.xpath('tr/td[1]/a[1]/img/@alt').get()
 
-                pdf_download_url = response.urljoin(table_item.xpath('tr/td[2]/p[3]/a[1]/@href').get())
-                pdf_download_title = table_item.xpath('tr/td[2]/p[3]/a[2]/text()').get()
-                jpeg_download_medium_url = response.urljoin(table_item.xpath('tr/td[2]/p[4]/a[2]/@href').get())
-                jpeg_download_medium_description = table_item.xpath('tr/td[2]/p[4]/a[2]/text()').get()
-                jpeg_download_high_url = response.urljoin(table_item.xpath('tr/td[2]/p[5]/a[2]/@href').get())
-                jpeg_download_high_description = table_item.xpath('tr/td[2]/p[5]/a[2]/text()').get()
+                # pdf_download_url = response.urljoin(table_item.xpath('tr/td[2]/p[3]/a[1]/@href').get())
+                # pdf_download_title = table_item.xpath('tr/td[2]/p[3]/a[2]/text()').get()
+                # jpeg_download_medium_url = response.urljoin(table_item.xpath('tr/td[2]/p[4]/a[2]/@href').get())
+                # jpeg_download_medium_description = table_item.xpath('tr/td[2]/p[4]/a[2]/text()').get()
+                # jpeg_download_high_url = response.urljoin(table_item.xpath('tr/td[2]/p[5]/a[2]/@href').get())
+                # jpeg_download_high_description = table_item.xpath('tr/td[2]/p[5]/a[2]/text()').get()
 
                 description_temp += map_title + "\n" \
-                    + map_content_heading + map_design \
+                    + map_design_heading + map_design \
                     + map_content_heading + map_content
+            # while we could theoretically grab all thumbnails during the above loop,
+            # the first one is enough for a preview-image in edu-sharing
+            first_thumbnail = response.urljoin(table_body[0].xpath('tr/td[1]/a[1]/img/@src').get())
 
         description_temp = w3lib.html.strip_html5_whitespace(description_temp)
 
         base = super().getBase(response=response)
         base.add_value('sourceId', response.url)
-        # TODO: base
-        #   - thumbnail
+
         last_modified = response.xpath('/html/head/meta[6]/@content').get()
         hash_temp = last_modified + self.version
         base.add_value('hash', hash_temp)
         base.add_value('type', Constants.TYPE_MATERIAL)
-        # base.add_value('thumbnail', )
-        # base.add_value('response', super().mapResponse(response).load_item())
+        if first_thumbnail is not None:
+            base.add_value('thumbnail', first_thumbnail)
         base.add_value('lastModified', last_modified)
+
         lom = LomBaseItemloader()
         general = LomGeneralItemloader(response=response)
-        # TODO: general
-        #   - description (WIP)
-        #   - identifier
-        #   - language
+        general.add_value('language', 'de')
+        general.add_value('identifier', response.url)
+        # the description could be extended with infos about the map-formats and their resolutions, if necessary
         general.add_value('description', description_temp)
         general.add_value('title', response.xpath('/html/head/title/text()').get())
         # keywords are stored inside a String, separated by commas with (sometimes multiple) whitespaces,
@@ -257,28 +256,36 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
         lom.add_value('technical', technical.load_item())
 
         lifecycle = LomLifecycleItemloader()
-        # TODO: lifecycle
-        #   - organization
-        #   - url
-        #   - date (pubDate?)
+        lifecycle.add_value('date', last_modified)
+        lifecycle.add_value('role', 'author')
+        lifecycle.add_value('firstName', 'Dirk')
+        lifecycle.add_value('lastName', 'Benkert')
+        lifecycle.add_value('organization', 'Ginkgomaps')
+        lifecycle.add_value('url', 'https://dirkbenkert.com/')
         lom.add_value('lifecycle', lifecycle.load_item())
 
         educational = LomEducationalItemLoader()
-        # TODO: educational
+        # since the learning objects are maps, expositive seems to be the best fit for interactivityType:
+        educational.add_value('interactivityType', 'expositive')
         lom.add_value('educational', educational.load_item())
         base.add_value('lom', lom.load_item())
 
         vs = ValuespaceItemLoader()
-        # TODO: Valuespaces
-        #   - educationalContext
-        #   - intendedEndUserRole
+        # since no educationalContext is given, either hardcode these values or don't use them at all
+        # vs.add_value('educationalContext', ["http://w3id.org/openeduhub/vocabs/educationalContext/sekundarstufe_1",
+        #                                     "http://w3id.org/openeduhub/vocabs/educationalContext/sekundarstufe_2",
+        #                                     "http://w3id.org/openeduhub/vocabs/educationalContext/berufliche_bildung",
+        #                                     "http://w3id.org/openeduhub/vocabs/educationalContext/erwachsenenbildung"])
+        vs.add_value('intendedEndUserRole', ["http://w3id.org/openeduhub/vocabs/intendedEndUserRole/learner",
+                                             "http://w3id.org/openeduhub/vocabs/intendedEndUserRole/teacher",
+                                             "http://w3id.org/openeduhub/vocabs/intendedEndUserRole/parent"])
         vs.add_value('discipline', 'http://w3id.org/openeduhub/vocabs/discipline/220')  # Geografie
         vs.add_value('learningResourceType', 'http://w3id.org/openeduhub/vocabs/learningResourceType/map')  # Karte
+        vs.add_value('conditionsOfAccess', 'http://w3id.org/openeduhub/vocabs/conditionsOfAccess/no_login')
 
         lic = LicenseItemLoader()
-        # TODO: license
-        #   - description?
-        #   - conditionsOfAccess
+        # if needed, the license description could also be gathered and constructed from multiple tags within a
+        # container: /html/body/center/table[1]/tbody/tr[5]/td[2]/p
         license_url: str = response.xpath('/html/body/center/table[1]/tr[5]/td[2]/p/a/@href').get()
         if (license_url is not None) and (license_url.endswith("deed.de")):
             license_url = license_url[:-len("deed.de")]
@@ -292,9 +299,10 @@ class GinkgoMapsSpider(scrapy.Spider, LomBase):
         permissions = super().getPermissions(response)
         base.add_value('permissions', permissions.load_item())
 
-        # TODO: either fill ResponseItemLoader() manually or by calling super()
-        response_loader = ResponseItemLoader()
-        response_loader.add_value('url', response.url)
-        base.add_value('response', response_loader.load_item())
+        # either fill ResponseItemLoader() manually or by calling super()
+        base.add_value('response', super().mapResponse(response).load_item())
+        # response_loader = ResponseItemLoader()
+        # response_loader.add_value('url', response.url)
+        # base.add_value('response', response_loader.load_item())
 
         yield base.load_item()
