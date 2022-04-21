4f030d956caf2ad3fa7724c46f21bd08c211be9f
Ttring <lei-cheng.yap@capgemini.com>
2022-04-21 14:14:35 +0200

    implemented LomBase

--- converter/spiders/sodix_spider.py
+++ converter/spiders/sodix_spider.py
@@ -1,146 +1,160 @@
-from scrapy.spiders import CrawlSpider
+import ast
+from email.quoprimime import body_check
+import json
 import time
-from .base_classes import LomBase
-import converter.env as env
+from types import SimpleNamespace
+import xmltodict as xmltodict
+from lxml import etree
+from scrapy.spiders import CrawlSpider
 import scrapy as scrapy
-from scrapy.spidermiddlewares.httperror import HttpError
-from twisted.internet.error import DNSLookupError
-from twisted.internet.error import TimeoutError, TCPTimedOutError
+from converter.constants import Constants
+from converter.items import *
+from converter.spiders.base_classes.lom_base import LomBase
 
 
-# Sample Spider, using a SitemapSpider to crawl your web page
-# Can be used as a template for your custom spider
 class SodixSpider(CrawlSpider, LomBase):
-    name = "sodix_spider"
-    url = "https://edu-sharing.com"  # the url which will be linked as the primary link to your source (should be the main url of your site)
-    friendlyName = "Sodix"  # name as shown in the search ui
-    apiUrl = "https://api.sodix.de/gql/auth/login"
-    version = "0.2"  # the version of your crawler, used to identify if a reimport is necessary
+    """
+    This crawler fetches data from the Merlin content source, which provides us paginated XML data. For every element
+    in the returned XML array we call LomBase.parse(), which in return calls methods, such as getId(), getBase() etc.
 
-    access_token="null"
-    user=env.get("SODIX_USER")
-    password=env.get("SODIX_PASSWORD")
+    Author: BRB team
+    """
+
+    name            = "sodix_spider"
+    url             = "https://www.sodix.de/"  # the url which will be linked as the primary link to your source (should be the main url of your site)
+    friendlyName    = "Sodix"  # name as shown in the search ui
+    version         = "0.1"  # the version of your crawler, used to identify if a reimport is necessary
+    apiUrl          = "https://api.sodix.de/gql/auth/login"  # * regular expression, to represent all possible values.
+    apiUrl2         = "https://api.sodix.de/gql/graphql"
 
     def __init__(self, **kwargs):
         LomBase.__init__(self, **kwargs)
+        #OAIBase.__init__(self, **kwargs)
+        
+    def open_json(self) :
+        with open('/home/leicheng/schulcloud/oeh-search-etl/credentials.json') as f:
+            data = json.load(f)
+        return data
 
-    def start_requests(self):
-        yield scrapy.http.Request(
-            url=self.apiUrl,
-            callback=self.parse_sodix,
-            method="POST",
-            errback=self.errback_sodix,
-            headers={
-                "Content-Type": "application/json",
-            },
-            body="{\"login\": \""+self.user+"\",\"password\": \""+self.password+"\"}"
-        )
+    # login into sodix page, then call parse function. 
+    def start_requests(self):    
+        yield scrapy.Request(
+                                url      = self.apiUrl,
+                                callback = self.parse,
+                                method   = 'POST',
+                                headers  = { 'Content-Type': 'application/json' },
+                                body     = json.dumps(self.open_json())
+                            )
 
-    def parse_sodix(self, response):
-        full_response=response.body
-        print(full_response)
-        self.access_token=str(full_response).split("\"")[3]
-        self.access_token="Bearer "+self.access_token
-        print(self.access_token)
-            #  Timer 5m
-        yield scrapy.http.Request(
-            url="https://api.sodix.de/gql/graphql",
-            callback=self.parse_sodix_end,
-            method="POST",
-            errback=self.errback_sodix,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": self.access_token,
-                },
-            body="{\"query\": \"{sources{id,name}}\"}"
-        )
+    # access sodix database by using access_token only since it has 8 hours of TTL    
+    def parse(self, response: scrapy.http.Response):
+        # print(  "Parsing URL  :" + response.url,
+        #       "\nStatus       :" ,response.status,
+        #       "\nresponse.body:" ,response.body ) 
+        json_response_body = json.loads(response.body) if response.status==200 else print("LOGIN ERROR")
 
-    def parse_sodix_end(self, response):
-        full_response=response.body
-        print(full_response)
-        print(self.access_token)
-        self.logger.error('>>>>>>>>>>>>Ende<<<<<<<<<<<<<<')
+        # check tokens
+        # for x in json_response_body:
+        #     print("%s: %s" % (x, json_response_body[x]))
+        
+        access_token = str(json_response_body['access_token'])
+        headers      = { 
+                         'Authorization':'Bearer ' + access_token,
+                         'Content-Type' : 'application/json'
+                       } # dict
+        body         = json.dumps({"query":
+                                        "{\n sources { id\n name\n created\n metadata { description\n keywords\n language\n learnResourceType\n media { thumbPreview\n } publishers { linkToGeneralUseRights\n } title\n } }\n}"})
+        yield scrapy.Request(
+                                url      = self.apiUrl2,
+                                callback = self.parse_sodix,
+                                method   = 'POST',
+                                headers  = headers,
+                                body     = body   
+                            )   
 
-    def get_new_token(self, response):
-        yield scrapy.http.Request(
-            url=self.apiUrl,
-            callback=self.parse_sodix,
-            method="POST",
-            errback=self.errback_sodix,
-            headers={
-                "Content-Type": "application/json",
-            },
-            body="{\"login\": \""+self.user+"\",\"password\": \""+self.password+"\"}"
-        )
+    def getId(self, response) :
+        return response.url
 
-    def errback_sodix(self, failure):
-        # log all failures
-        self.logger.error(repr(failure))
-
-        # in case you want to do something special for some errors,
-        # you may need the failure's type:
-
-        if failure.check(HttpError):
-            # these exceptions come from HttpError spider middleware
-            # you can get the non-200 response
-            response = failure.value.response
-            self.logger.error('HttpError on %s', response.url)
-
-        elif failure.check(DNSLookupError):
-            # this is the original request
-            request = failure.request
-            self.logger.error('DNSLookupError on %s', request.url)
-
-        elif failure.check(TimeoutError, TCPTimedOutError):
-            request = failure.request
-            self.logger.error('TimeoutError on %s', request.url)
-
-    def parse(self, response):
-        return LomBase.parse(self, response)
-
-    # return a (stable) id of the source
-    def getId(self, response):
-        return response.xpath("//title//text()").get()
-
-    # return a stable hash to detect content changes
-    # if there is no hash available, may use the current time as "always changing" info
-    # Please include your crawler version as well
-    def getHash(self, response):
-        return self.version + time.time()
+    def getHash(self, response): 
+        return str(self.version)+str(time.time())
 
     def getBase(self, response):
+        print("sodix_spider : getBase\n")
         base = LomBase.getBase(self, response)
-        # optionally provide thumbnail. If empty, it will tried to be generated from the getLOMTechnical 'location' (if format is 'text/html')
-        # base.add_value('thumbnail', 'https://url/to/thumbnail')
-        return base
-
+        x    = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
+        for i in range(len(x.data.sources)):
+            for j in range(len(x.data.sources[i].metadata)):
+                base.add_value("thumbnail", x.data.sources[i].metadata[j].media.thumbPreview)
+        return base   
+    
+    # to access sodix with access_token
+    def parse_sodix(self, response):
+        #json_response_body = json.loads(response.body)
+        #data = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
+        print("sodix_spider : parse_sodix\n")
+        lom = LomBase.parse(self,response)
+        return lom
+       
+ 
     def getLOMGeneral(self, response):
+        print("sodix_spider : getLOMGeneral\n")
         general = LomBase.getLOMGeneral(self, response)
-        general.add_value("title", response.xpath("//title//text()").get())
-        general.add_value(
-            "language", response.xpath('//meta[@property="og:locale"]/@content').get()
-        )
-        return general
+        x       = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
 
-    def getLOMTechnical(self, response):
-        technical = LomBase.getLOMTechnical(self, response)
-        technical.add_value("location", response.url)
-        technical.add_value("format", "text/html")
-        technical.add_value("size", len(response.body))
-        return technical
+        for i in range(len(x.data.sources)):
+            for j in range(len(x.data.sources[i].metadata)):
+           
+                general.add_value("identifier", x.data.sources[i].id)
+                general.add_value("title", x.data.sources[i].metadata[j].title)
+                general.add_value("keyword", x.data.sources[i].metadata[j].keywords)
+                general.add_value("language", x.data.sources[i].metadata[j].language)
+                general.add_value("description",x.data.sources[i].metadata[j].description)
+
+        return general
+    
+    # def getLOMTechnical(self, response):
+    #     print("sodix_spider : getLOMTechnical\n")
+    #     technical = LomBase.getLOMTechnical(response)
+       
+    #     x = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
+    #     for i in range(len(x.data.sources)):
+    #         for j in range(len(x.data.sources[i].metadata)):
+    #             #size is in int
+    #             #technical.add_value("size", x.data.sources[i].metadata[j].size)
+    #             #duration is null
+    #             #technical.add_value( "duration",x.data.sources[i].metadata[j].duration)
+
+    #     return technical
+
+    def getLOMEducational(self, response=None):
+        print("sodix_spider : getLomEducation\n")
+        educational = LomBase.getLOMEducational(response)
+        x           = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
+        
+        for i in range(len(x.data.sources)):
+            for j in range(len(x.data.sources[i].metadata)):
+                educational.add_value("description", x.data.sources[i].metadata[j].description)
+                educational.add_value("language", x.data.sources[i].metadata[j].language)
+        return educational
+    
+    def getLicense(self, response=None):
+        print("sodix_spider : getLicense\n")
+        license = LomBase.getLicense(self, response)
+        x       = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
+        
+        for i in range(len(x.data.sources)):
+            for j in range(len(x.data.sources[i].metadata)):
+                license.add_value("description", x.data.sources[i].metadata[j].publishers.linkToGeneralUseRights)
+        return license
 
     def getValuespaces(self, response):
+        print("sodix_spider : getValuespaces\n")
         valuespaces = LomBase.getValuespaces(self, response)
-        # Provide valuespace data. This data will later get automatically mapped
-        # Please take a look at the valuespaces here:
-        # https://vocabs.openeduhub.de/
-        # You can either use full identifiers or also labels. The system will auto-map them accordingly
+        x           = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
+        
+        for i in range(len(x.data.sources)):
+            for j in range(len(x.data.sources[i].metadata)):
+                valuespaces.add_value("learningResourceType", x.data.sources[i].metadata[j].learnResourceType)
 
-        # Please also checkout the ValuespaceHelper class which provides usefull mappers for common data
-
-        # valuespaces.add_value('educationalContext', context)
-        # valuespaces.add_value('discipline',discipline)
-        # valuespaces.add_value('learningResourceType', lrt)
         return valuespaces
-
-    # You may override more functions here, please checkout LomBase class
+    
