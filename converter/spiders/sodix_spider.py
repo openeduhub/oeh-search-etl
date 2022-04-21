39d3ca3584e91d5623781568ea1df7b83b1ecfa7
Krause <stephan.krause.ext@capgemini.com>
2022-04-21 14:25:32 +0200

    move login and password to env

--- converter/spiders/sodix_spider.py
+++ converter/spiders/sodix_spider.py
@@ -10,6 +10,7 @@ import scrapy as scrapy
 from converter.constants import Constants
 from converter.items import *
 from converter.spiders.base_classes.lom_base import LomBase
+import converter.env as env
 
 
 class SodixSpider(CrawlSpider, LomBase):
@@ -26,39 +27,36 @@ class SodixSpider(CrawlSpider, LomBase):
     version         = "0.1"  # the version of your crawler, used to identify if a reimport is necessary
     apiUrl          = "https://api.sodix.de/gql/auth/login"  # * regular expression, to represent all possible values.
     apiUrl2         = "https://api.sodix.de/gql/graphql"
+    user=env.get("SODIX_USER")
+    password=env.get("SODIX_PASSWORD")
 
     def __init__(self, **kwargs):
         LomBase.__init__(self, **kwargs)
         #OAIBase.__init__(self, **kwargs)
-        
-    def open_json(self) :
-        with open('/home/leicheng/schulcloud/oeh-search-etl/credentials.json') as f:
-            data = json.load(f)
-        return data
 
-    # login into sodix page, then call parse function. 
-    def start_requests(self):    
+    # login into sodix page, then call parse function.
+    def start_requests(self):
         yield scrapy.Request(
                                 url      = self.apiUrl,
                                 callback = self.parse,
                                 method   = 'POST',
                                 headers  = { 'Content-Type': 'application/json' },
-                                body     = json.dumps(self.open_json())
+                                body     = "{\"login\": \""+self.user+"\",\"password\": \""+self.password+"\"}"
                             )
 
-    # access sodix database by using access_token only since it has 8 hours of TTL    
+    # access sodix database by using access_token only since it has 8 hours of TTL
     def parse(self, response: scrapy.http.Response):
         # print(  "Parsing URL  :" + response.url,
         #       "\nStatus       :" ,response.status,
-        #       "\nresponse.body:" ,response.body ) 
+        #       "\nresponse.body:" ,response.body )
         json_response_body = json.loads(response.body) if response.status==200 else print("LOGIN ERROR")
 
         # check tokens
         # for x in json_response_body:
         #     print("%s: %s" % (x, json_response_body[x]))
-        
+
         access_token = str(json_response_body['access_token'])
-        headers      = { 
+        headers      = {
                          'Authorization':'Bearer ' + access_token,
                          'Content-Type' : 'application/json'
                        } # dict
@@ -69,13 +67,13 @@ class SodixSpider(CrawlSpider, LomBase):
                                 callback = self.parse_sodix,
                                 method   = 'POST',
                                 headers  = headers,
-                                body     = body   
-                            )   
+                                body     = body
+                            )
 
     def getId(self, response) :
         return response.url
 
-    def getHash(self, response): 
+    def getHash(self, response):
         return str(self.version)+str(time.time())
 
     def getBase(self, response):
@@ -85,8 +83,8 @@ class SodixSpider(CrawlSpider, LomBase):
         for i in range(len(x.data.sources)):
             for j in range(len(x.data.sources[i].metadata)):
                 base.add_value("thumbnail", x.data.sources[i].metadata[j].media.thumbPreview)
-        return base   
-    
+        return base
+
     # to access sodix with access_token
     def parse_sodix(self, response):
         #json_response_body = json.loads(response.body)
@@ -94,8 +92,8 @@ class SodixSpider(CrawlSpider, LomBase):
         print("sodix_spider : parse_sodix\n")
         lom = LomBase.parse(self,response)
         return lom
-       
- 
+
+
     def getLOMGeneral(self, response):
         print("sodix_spider : getLOMGeneral\n")
         general = LomBase.getLOMGeneral(self, response)
@@ -103,7 +101,7 @@ class SodixSpider(CrawlSpider, LomBase):
 
         for i in range(len(x.data.sources)):
             for j in range(len(x.data.sources[i].metadata)):
-           
+
                 general.add_value("identifier", x.data.sources[i].id)
                 general.add_value("title", x.data.sources[i].metadata[j].title)
                 general.add_value("keyword", x.data.sources[i].metadata[j].keywords)
@@ -111,11 +109,11 @@ class SodixSpider(CrawlSpider, LomBase):
                 general.add_value("description",x.data.sources[i].metadata[j].description)
 
         return general
-    
+
     # def getLOMTechnical(self, response):
     #     print("sodix_spider : getLOMTechnical\n")
     #     technical = LomBase.getLOMTechnical(response)
-       
+
     #     x = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
     #     for i in range(len(x.data.sources)):
     #         for j in range(len(x.data.sources[i].metadata)):
@@ -130,18 +128,18 @@ class SodixSpider(CrawlSpider, LomBase):
         print("sodix_spider : getLomEducation\n")
         educational = LomBase.getLOMEducational(response)
         x           = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
-        
+
         for i in range(len(x.data.sources)):
             for j in range(len(x.data.sources[i].metadata)):
                 educational.add_value("description", x.data.sources[i].metadata[j].description)
                 educational.add_value("language", x.data.sources[i].metadata[j].language)
         return educational
-    
+
     def getLicense(self, response=None):
         print("sodix_spider : getLicense\n")
         license = LomBase.getLicense(self, response)
         x       = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
-        
+
         for i in range(len(x.data.sources)):
             for j in range(len(x.data.sources[i].metadata)):
                 license.add_value("description", x.data.sources[i].metadata[j].publishers.linkToGeneralUseRights)
@@ -151,10 +149,10 @@ class SodixSpider(CrawlSpider, LomBase):
         print("sodix_spider : getValuespaces\n")
         valuespaces = LomBase.getValuespaces(self, response)
         x           = json.loads(response.body, object_hook=lambda d: SimpleNamespace(**d))
-        
+
         for i in range(len(x.data.sources)):
             for j in range(len(x.data.sources[i].metadata)):
                 valuespaces.add_value("learningResourceType", x.data.sources[i].metadata[j].learnResourceType)
 
         return valuespaces
-    
+
