f6abb9d8d97adb27602057f02298f53d89eeff71
Ttring <lei-cheng.yap@capgemini.com>
2022-04-26 13:59:11 +0200

    change Lom functions

--- converter/spiders/sodix_spider.py
+++ converter/spiders/sodix_spider.py
@@ -1,4 +1,5 @@
 import json
+import requests
 import time
 import scrapy as scrapy
 import converter.env as env
@@ -30,30 +31,33 @@ class SodixSpider(CrawlSpider, LomBase):
 
     def __init__(self, **kwargs):
         LomBase.__init__(self, **kwargs)
+        self.access_token = None
 
-    # login into sodix page, then call parse function. 
-    def start_requests(self):    
-        yield scrapy.Request(
-            url      = self.apiUrl,
-            callback = self.parse,
-            method   = 'POST',
-            headers  = { 'Content-Type': 'application/json' },
-            body     ="{\"login\": \""+self.user+"\",\"password\": \""+self.password+"\"}"
+    def start_requests(self):
+        self.login()
+        yield self.make_requests()
+
+    def login(self):
+        response = requests.post(
+            self.apiUrl,
+            headers =   {'Content-Type': 'application/json'},
+            data    =   f'{{"login": "{self.user}", "password": "{self.password}"}}'
         )
-                 
-    def parse(self, response: scrapy.http.Response):
-        json_response_body  = json.loads(response.body) if response.status==200 else print("LOGIN ERROR")
+        if not response.status_code == 200:
+            raise RuntimeError(f'Unexpected response status: {response.status_code}')
+        self.access_token = response.json()['access_token']
         
-        access_token        = str(json_response_body['access_token'])
+                 
+    def make_requests(self):
         headers             = { 
-                                'Authorization':'Bearer ' + access_token,
+                                'Authorization':'Bearer ' + self.access_token,
                                 'Content-Type' : 'application/json'
                               } 
 
         # add the metadata that you want to extract here
         body         = json.dumps({"query":
-                                        "{\n sources { id\n name\n created\n metadata { description\n keywords\n language\n learnResourceType\n media { dataType\n originalUrl\n size\n thumbPreview\n } publishers { linkToGeneralUseRights\n } title\n } }\n}"})
-        yield scrapy.Request(
+                                        "{\n sources { metadata { description\n id\n keywords\n language\n learnResourceType\n media { dataType\n originalUrl\n size\n thumbPreview\n } publishers { linkToGeneralUseRights\n } title\n } }\n}"})
+        return scrapy.Request(
                                 url      = self.apiUrl2,
                                 callback = self.parse_sodix,
                                 method   = 'POST',
@@ -61,45 +65,47 @@ class SodixSpider(CrawlSpider, LomBase):
                                 body     = body   
                             ) 
 
-    # to access sodix with access_token
     def parse_sodix(self, response):
-        elements     = json.loads(response.body.decode('utf-8'))
-        requestCount = len(elements['data']['sources'])
-
+        elements       = json.loads(response.body.decode('utf-8'))
+        requestCount   = len(elements['data']['sources'])
+        
         for i in range(requestCount):
-        # For debugging
-            # if i == 10:
-            #     print('dev-mode : 10 requests done, exiting...')
-            #     break
+            #for debugging
+            if i == 1:
+                    print('dev-mode : 1 requests done, exiting...')
+                    break
+            for j in range(len(elements['data']['sources'][i]['metadata'])):
 
-            copyResponse              = response.copy()
-            copyResponse.meta["item"] = elements['data']['sources'][i] 
+                copyResponse              = response.copy()
+                copyResponse.meta["item"] = elements['data']['sources'][i]['metadata'][j] 
 
-            json_str = json.dumps(elements['data']['sources'][i], indent=4, sort_keys=True, ensure_ascii=False)
+                json_str = json.dumps(elements['data']['sources'][i]['metadata'][j], indent=4, sort_keys=True, ensure_ascii=False)
 
-            copyResponse._set_body(json_str)
+                copyResponse._set_body(json_str)
 
-            # In order to transfer data to CSV/JSON, implement these 2 lines. 
-            if self.hasChanged(copyResponse):
-               yield LomBase.parse(self, copyResponse)
-            
-            # to call LomBase functions 
-            LomBase.parse(self, copyResponse)
+                # In order to transfer data to CSV/JSON, implement these 2 lines. 
+                if self.hasChanged(copyResponse):
+                    yield LomBase.parse(self, copyResponse)
+                
+                # to call LomBase functions 
+                LomBase.parse(self, copyResponse)
             print('Finish parsing: ' + str(i+1) + '/' + str(requestCount))
     
     def getBase(self, response):
         base         = LomBase.getBase(self, response)
-        source       = response.meta["item"]
+        metadata     = response.meta["item"]
 
-        for i in range(len(source['metadata'])):
-            base.add_value("thumbnail"  , source['metadata'][i]['media']['thumbPreview'])        
-        return base  
+        base.add_value("thumbnail"  , metadata['media']['thumbPreview'])
+
+        return base            
 
     def getId(self, response) :
-        source = response.meta["item"]
-        return source['id']
+        metadata = response.meta["item"]
 
+        return metadata['id']
+        
     def getHash(self, response):
+
         return hash(str(self.version)+str(time.time()))
 
     def mapResponse(self, response):
@@ -107,69 +113,75 @@ class SodixSpider(CrawlSpider, LomBase):
         r.add_value("status"    , response.status)
         r.add_value("headers"   , response.headers)
         r.add_value("url"       , response.url)
+
         return r     
 
     def getLOMEducational(self, response=None):
         educational = LomBase.getLOMEducational(self, response)
-        source      = response.meta["item"]
+        metadata    = response.meta["item"]
 
-        for i in range(len(source['metadata'])):
-            educational.add_value("description" , source['metadata'][i]['description'])
-            educational.add_value("language"    , source['metadata'][i]['language'])
+        educational.add_value("language"    , metadata['language'])
+
         return educational
     
     def getLOMGeneral(self, response):
-        general = LomBase.getLOMGeneral(self, response)
-        source  = response.meta["item"]
+        general   = LomBase.getLOMGeneral(self, response)
+        metadata  = response.meta["item"]
 
-        general.add_value("identifier"  , source['id'])
+        general.add_value("aggregationLevel", "1")
+        general.add_value("identifier"  , metadata['id'])
+        general.add_value("title"       , metadata['title'])                     
+        general.add_value("keyword"     , metadata['keywords'])
+        general.add_value("language"    , metadata['language'])
+        general.add_value("description" , metadata['description'])
 
-        for i in range(len(source['metadata'])):
-            general.add_value("title"       , source['metadata'][i]['title'])        
-            general.add_value("keyword"     , source['metadata'][i]['keywords'])
-            general.add_value("language"    , source['metadata'][i]['language'])
-            general.add_value("description" , source['metadata'][i]['description'])
-            return general
+        return general
+
     
     def getLicense(self, response=None):
-        license = LomBase.getLicense(self, response)
-        source  = response.meta["item"]
+        license     = LomBase.getLicense(self, response)
+        metadata    = response.meta["item"]
  
-        for i in range(len(source['metadata'])):
-            for j in range(len(source['metadata'][i]['publishers'])):
-                license.add_value("description",source['metadata'][i]['publishers'][j]['linkToGeneralUseRights'])
+        for i in range(len(metadata['publishers'])):
+            license.add_value("description", metadata['publishers'][i]['linkToGeneralUseRights'])
+
         return license
 
     def getLOMTechnical(self, response):
         technical = LomBase.getLOMTechnical(self, response)
-        source    = response.meta["item"]
+        metadata  = response.meta["item"]
 
-        for i in range(len(source['metadata'])):
-            technical.add_value("format"    , source['metadata'][i]['media']['dataType'])
-            technical.add_value("location"  , source['metadata'][i]['media']['originalUrl'])
-            technical.add_value("size"      , source['metadata'][i]['media']['size'])
+        technical.add_value("format"    , metadata['media']['dataType'])
+        technical.add_value("location"  , metadata['media']['originalUrl'])
+        technical.add_value("size"      , metadata['media']['size'])
+
         return technical
 
     def getValuespaces(self, response):
         valuespaces = LomBase.getValuespaces(self, response)
-        source      = response.meta["item"]
+        metadata    = response.meta["item"]
 
-        for i in range(len(source['metadata'])):
-            valuespaces.add_value("learningResourceType", source['metadata'][i]['learnResourceType'])
+        valuespaces.add_value("learningResourceType", metadata['learnResourceType'])
 
         return valuespaces
     
     # TODO
     def getPermissions(self, response):
         permissions = LomBase.getPermissions(self, response)
+
         return permissions    
 
     # TODO 
     def getLOMAnnotation(self, response=None) -> LomAnnotationItemLoader:
         annotation = LomBase.getLOMAnnotation(self, response)
+        #metadata  = response.meta["item"]
+
+        annotation.add_value("description" , "searchable==0")
+
         return annotation
 
     # TODO
     def getLOMRelation(self, response=None) -> LomRelationItemLoader:
         relation = LomBase.getLOMRelation(self, response)
+
         return relation
