6b20d11802b2525c95c7ac62553e59a2019cf2d3
Andre Blome <andre-david.blome@capgemini.com>
2022-04-27 10:27:36 +0200

    reformatting

--- converter/spiders/sodix_spider.py
+++ converter/spiders/sodix_spider.py
@@ -8,7 +8,6 @@ from converter.items import *
 from converter.spiders.base_classes.lom_base import LomBase
 from scrapy.spiders import CrawlSpider
 
-
 query_string = '''
 {
     sources {
@@ -46,21 +45,21 @@ class SodixSpider(CrawlSpider, LomBase):
     Author: BRB team
     """
 
-    name            = "sodix_spider"
-    url             = "https://www.sodix.de/"  # the url which will be linked as the primary link to your source (should be the main url of your site)
-    friendlyName    = "Sodix"  # name as shown in the search ui
-    version         = "0.1"  # the version of your crawler, used to identify if a reimport is necessary
-    urlLogin          = "https://api.sodix.de/gql/auth/login"  # * regular expression, to represent all possible values.
-    urlRequest         = "https://api.sodix.de/gql/graphql"
-    user            = env.get("SODIX_USER")
-    password        = env.get("SODIX_PASSWORD")
+    name = "sodix_spider"
+    url = "https://www.sodix.de/"  # the url which will be linked as the primary link to your source (should be the main url of your site)
+    friendlyName = "Sodix"  # name as shown in the search ui
+    version = "0.1"  # the version of your crawler, used to identify if a reimport is necessary
+    urlLogin = "https://api.sodix.de/gql/auth/login"  # * regular expression, to represent all possible values.
+    urlRequest = "https://api.sodix.de/gql/graphql"
+    user = env.get("SODIX_USER")
+    password = env.get("SODIX_PASSWORD")
 
     def __init__(self, **kwargs):
         LomBase.__init__(self, **kwargs)
         self.access_token = ''
 
     def start_requests(self):
-        #self.login()
+        # self.login()
         self.make_requests()
 
     def login(self):
@@ -93,20 +92,21 @@ class SodixSpider(CrawlSpider, LomBase):
     # to access sodix with access_token
     def parse_sodix(self, response: requests.Response):
         # TODO: make independent from scrapy (response)
-        elements       = json.loads(response.body.decode('utf-8'))
-        requestCount   = len(elements['data']['sources'])
+        elements = json.loads(response.body.decode('utf-8'))
+        requestCount = len(elements['data']['sources'])
 
         for i in range(requestCount):
-            #for debugging
+            # for debugging
             if i == 1:
-                    print('dev-mode : 1 requests done, exiting...')
-                    break
+                print('dev-mode : 1 requests done, exiting...')
+                break
             for j in range(len(elements['data']['sources'][i]['metadata'])):
 
-                copyResponse              = response.copy()
+                copyResponse = response.copy()
                 copyResponse.meta["item"] = elements['data']['sources'][i]['metadata'][j]
 
-                json_str = json.dumps(elements['data']['sources'][i]['metadata'][j], indent=4, sort_keys=True, ensure_ascii=False)
+                json_str = json.dumps(elements['data']['sources'][i]['metadata'][j], indent=4, sort_keys=True,
+                                      ensure_ascii=False)
 
                 copyResponse._set_body(json_str)
 
@@ -116,58 +116,57 @@ class SodixSpider(CrawlSpider, LomBase):
 
                 # to call LomBase functions 
                 LomBase.parse(self, copyResponse)
-            print('Finish parsing: ' + str(i+1) + '/' + str(requestCount))
+            print('Finish parsing: ' + str(i + 1) + '/' + str(requestCount))
 
     def getBase(self, response):
-        base         = LomBase.getBase(self, response)
-        metadata     = response.meta["item"]
+        base = LomBase.getBase(self, response)
+        metadata = response.meta["item"]
 
-        base.add_value("thumbnail"  , metadata['media']['thumbPreview'])
+        base.add_value("thumbnail", metadata['media']['thumbPreview'])
 
         return base
 
-    def getId(self, response) :
+    def getId(self, response):
         metadata = response.meta["item"]
 
         return metadata['id']
 
     def getHash(self, response):
 
-        return hash(str(self.version)+str(time.time()))
+        return hash(str(self.version) + str(time.time()))
 
     def mapResponse(self, response):
         r = ResponseItemLoader(response=response)
-        r.add_value("status"    , response.status)
-        r.add_value("headers"   , response.headers)
-        r.add_value("url"       , response.url)
+        r.add_value("status", response.status)
+        r.add_value("headers", response.headers)
+        r.add_value("url", response.url)
 
         return r
 
     def getLOMEducational(self, response=None):
         educational = LomBase.getLOMEducational(self, response)
-        metadata      = response.meta["item"]
+        metadata = response.meta["item"]
 
-        educational.add_value("language"    , metadata['language'])
+        educational.add_value("language", metadata['language'])
 
         return educational
 
     def getLOMGeneral(self, response):
         general = LomBase.getLOMGeneral(self, response)
-        metadata  = response.meta["item"]
+        metadata = response.meta["item"]
 
         general.add_value("aggregationLevel", "1")
-        general.add_value("identifier"  , metadata['id'])
-        general.add_value("title"       , metadata['title'])
-        general.add_value("keyword"     , metadata['keywords'])
-        general.add_value("language"    , metadata['language'])
-        general.add_value("description" , metadata['description'])
+        general.add_value("identifier", metadata['id'])
+        general.add_value("title", metadata['title'])
+        general.add_value("keyword", metadata['keywords'])
+        general.add_value("language", metadata['language'])
+        general.add_value("description", metadata['description'])
 
         return general
 
-
     def getLicense(self, response=None):
-        license     = LomBase.getLicense(self, response)
-        metadata    = response.meta["item"]
+        license = LomBase.getLicense(self, response)
+        metadata = response.meta["item"]
 
         for i in range(len(metadata['publishers'])):
             license.add_value("description", metadata['publishers'][i]['linkToGeneralUseRights'])
@@ -176,17 +175,17 @@ class SodixSpider(CrawlSpider, LomBase):
 
     def getLOMTechnical(self, response):
         technical = LomBase.getLOMTechnical(self, response)
-        metadata  = response.meta["item"]
+        metadata = response.meta["item"]
 
-        technical.add_value("format"    , metadata['media']['dataType'])
-        technical.add_value("location"  , metadata['media']['originalUrl'])
-        technical.add_value("size"      , metadata['media']['size'])
+        technical.add_value("format", metadata['media']['dataType'])
+        technical.add_value("location", metadata['media']['originalUrl'])
+        technical.add_value("size", metadata['media']['size'])
 
         return technical
 
     def getValuespaces(self, response):
         valuespaces = LomBase.getValuespaces(self, response)
-        metadata    = response.meta["item"]
+        metadata = response.meta["item"]
 
         valuespaces.add_value("learningResourceType", metadata['learnResourceType'])
 
@@ -201,11 +200,11 @@ class SodixSpider(CrawlSpider, LomBase):
     # TODO
     def getLOMAnnotation(self, response=None) -> LomAnnotationItemLoader:
         annotation = LomBase.getLOMAnnotation(self, response)
-        #metadata  = response.meta["item"]
+        # metadata  = response.meta["item"]
 
         # Adding a default searchable value to constitute this element (node) as a valid-to-be-returned object.
         annotation.add_value("entity", "crawler")
-        annotation.add_value("description" , "searchable==0")
+        annotation.add_value("description", "searchable==0")
 
         return annotation
 
