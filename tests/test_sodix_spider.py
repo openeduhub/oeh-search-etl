4bcd339612b57dd83556d88895605199e6a8f9a7
Krause <stephan.krause.ext@capgemini.com>
2022-05-03 14:30:40 +0200

    test sodix_spider UnexpectedResponseError login

--- tests/test_sodix_spider.py
+++ tests/test_sodix_spider.py
@@ -1,11 +1,12 @@
-from doctest import UnexpectedException
 import unittest
 from converter.spiders.sodix_spider import SodixSpider, UnexpectedResponseError
+import converter.env as env
 
 class TestSodixSpider(unittest.TestCase):
 
     def setUp(self):
         self.sodixSpider = SodixSpider()
+        self.unexpectedResponseError = UnexpectedResponseError()
 
     def test_placeholder(self):
         self.assertEqual("test1","test1")
@@ -14,15 +15,14 @@ class TestSodixSpider(unittest.TestCase):
         self.sodixSpider.login()
         self.assertEqual(200, self.sodixSpider.getLoginRepsonseStatusCode())
 
-    @unittest.expectedFailure
     def test_login_unexpected_response(self):
         self.sodixSpider.password='1'
-        self.assertRaises(UnexpectedResponseError, self.sodixSpider.login())
+        with self.assertRaises(UnexpectedResponseError):
+            self.sodixSpider.login()
 
     def test_login_accessToken(self):
         self.sodixSpider.login()
         self.assertTrue(self.sodixSpider.access_token)
-        print(f'response: {self.sodixSpider.access_token}')
 
 if __name__ == '__main__':
     unittest.main()
