d22fc5ab8f4b5c01b66f0dba314b430d8733e7aa
Andre Blome <andre-david.blome@capgemini.com>
2022-06-24 12:52:56 +0200

    settings cleaned up

--- converter/settings.py
+++ converter/settings.py
@@ -87,10 +87,9 @@ DOWNLOAD_DELAY = 0
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#   'converter.middlewares.OerScrapyDownloaderMiddleware': 543,
-#  'converter.middlewares.CustomRetryMiddleware' : 340,
-#}
+# DOWNLOADER_MIDDLEWARES = {
+#    'converter.middlewares.OerScrapyDownloaderMiddleware': 543,
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
