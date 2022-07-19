f278dda2c6c228d6916504831d62f7d1b25ffc9f
DDEITERS <dirk.deiters.ext@capgemini.com>
2022-07-19 13:52:36 +0200

    create the 'extract_metadata_from_json'-method

--- schulcloud/adhoc_tasks/h5p/h5p_extract_metadata.py
+++ schulcloud/adhoc_tasks/h5p/h5p_extract_metadata.py
@@ -2,6 +2,7 @@ import os
 import zipfile
 import json
 import shutil
+from typing import Dict
 
 
 class UnzipLocalFile:
@@ -25,6 +26,29 @@ class UnzipLocalFile:
         file.close()
         return data
 
+    def extract_metadata_from_json(metadata: [Dict]):
+        metadata_h5p = {
+            "title": "",
+            "download_url": "",
+            "copyright_license": "",
+            "author": "",
+            "ext_source": ""
+        }
+
+        # ToDO: Check if this schema is the same for all h5p-files (content.json).
+        #  If not, create JSON-attribute-search-method.
+
+        try:
+            metadata_h5p["title"] = metadata["interactiveVideo"]["video"]["files"][0]["copyright"]["title"]
+            metadata_h5p["download_url"] = metadata["interactiveVideo"]["video"]["files"][0]["path"]
+            metadata_h5p["copyright_license"] = metadata["interactiveVideo"]["video"]["files"][0]["copyright"]["license"]
+            metadata_h5p["author"] = metadata["interactiveVideo"]["video"]["files"][0]["copyright"]["author"]
+            metadata_h5p["ext_source"] = metadata["interactiveVideo"]["video"]["files"][0]["copyright"]["source"]
+        except KeyError:
+            print(f'KeyError: The JSON schema of the file doesn\'t match with the method schema.')
+
+        return metadata_h5p
+
 
 def main():
     # create local folder 'unzipped'
@@ -32,25 +56,19 @@ def main():
     print("Create folder \'unzipped\'.")
 
     # unzip the h5p-file
-    UnzipLocalFile.unzip_local_file(file_path="h5p_files/bob.h5p", unzipped_dir="unzipped")
+    h5p_file = "alice.h5p"
+    UnzipLocalFile.unzip_local_file(file_path=f"h5p_files/{h5p_file}", unzipped_dir="unzipped")
 
     # read out the lines, e.g. title
     metadata = json.loads(UnzipLocalFile.read_file(file_path="unzipped/content/content.json"))
-
-    # ToDO: Check if this schema is the same for all h5p-files (content.json).
-    #  If not, create JSON-attribute-search-method.
-    title = metadata["interactiveVideo"]["video"]["files"][0]["copyright"]["title"]
-    download_url = metadata["interactiveVideo"]["video"]["files"][0]["path"]
-    copyright_licence = metadata["interactiveVideo"]["video"]["files"][0]["copyright"]["license"]
-    author = metadata["interactiveVideo"]["video"]["files"][0]["copyright"]["author"]
-    ext_source = metadata["interactiveVideo"]["video"]["files"][0]["copyright"]["source"]
+    metadata_h5p = UnzipLocalFile.extract_metadata_from_json(metadata=metadata)
 
     print(metadata)
-    print(f'title: {title}')
-    print(f'download_url: {download_url}')
-    print(f'copyright_license: {copyright_licence}')
-    print(f'author: {author}')
-    print(f'external_source: {ext_source}')
+    print(f'title: {metadata_h5p["title"]}')
+    print(f'download_url: {metadata_h5p["download_url"]}')
+    print(f'copyright_license: {metadata_h5p["copyright_license"]}')
+    print(f'author: {metadata_h5p["author"]}')
+    print(f'external_source: {metadata_h5p["ext_source"]}')
 
     # delete the 'unzipped'-folder including all files
     UnzipLocalFile.delete_local_folder(folder_name="unzipped")
