718e87580901a18ec3ceec0149414183b70c6d68
DDEITERS <dirk.deiters.ext@capgemini.com>
2022-07-21 16:35:05 +0200

    add 'extract_metadata_from_excel'-method
    it returns the metadata of the excelSheet which matches with the filename

--- schulcloud/adhoc_tasks/h5p/h5p_extract_metadata.py
+++ schulcloud/adhoc_tasks/h5p/h5p_extract_metadata.py
@@ -3,6 +3,7 @@ import zipfile
 import json
 import shutil
 from typing import Dict
+import openpyxl
 
 
 class UnzipLocalFile:
@@ -40,6 +41,28 @@ class UnzipLocalFile:
 
         return metadata_h5p
 
+    def extract_metadata_from_excel(excel_file_path: str, file_name: str):
+        metadata_h5p = {
+            "package": "",
+            "order": "",
+            "task": "",
+            "keywords": ""
+        }
+
+        wb = openpyxl.load_workbook(filename=excel_file_path, data_only=True)
+        o_sheet = wb["Tabelle1"]
+
+        for i in range(1, o_sheet.max_row + 1, 1):
+            file_name_sheet = o_sheet.cell(row=i, column=4)
+
+            if file_name_sheet.value == file_name:
+                metadata_h5p["package"] = o_sheet.cell(row=i, column=1).value
+                metadata_h5p["order"] = o_sheet.cell(row=i, column=2).value
+                metadata_h5p["task"] = o_sheet.cell(row=i, column=3).value
+                metadata_h5p["keywords"] = o_sheet.cell(row=i, column=5).value
+
+        return metadata_h5p
+
 
 def main():
     # create local folder 'unzipped'
@@ -61,6 +84,13 @@ def main():
     # delete the 'unzipped'-folder including all files
     UnzipLocalFile.delete_local_folder(folder_name="unzipped")
     print("Delete folder \'unzipped\'")
+
+    # extract metadata from excel_file
+    file_name = "bio7v2_17_koerpermerkmale-von-schnecken-interaktive-wissensvermittlung.h5p"
+    excel_file_path = 'h5p_files/Biologie 7 Vol 2 - Wirbellose.xlsx'
+
+    metadata_excel = UnzipLocalFile.extract_metadata_from_excel(excel_file_path=excel_file_path, file_name=file_name)
+    print(metadata_excel)
 
 
 if __name__ == '__main__':
