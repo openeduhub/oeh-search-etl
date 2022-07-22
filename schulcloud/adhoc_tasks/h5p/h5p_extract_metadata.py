a55d8aa44ab92e6ae340cd98be7da224cca60c11
DDEITERS <dirk.deiters.ext@capgemini.com>
2022-07-22 10:26:11 +0200

    extend 'extract_metadata_from_excel'-method to new schema

--- schulcloud/adhoc_tasks/h5p/h5p_extract_metadata.py
+++ schulcloud/adhoc_tasks/h5p/h5p_extract_metadata.py
@@ -43,23 +43,25 @@ class UnzipLocalFile:
 
     def extract_metadata_from_excel(excel_file_path: str, file_name: str):
         metadata_h5p = {
-            "package": "",
+            "collection": "",
             "order": "",
-            "task": "",
-            "keywords": ""
+            "title": "",
+            "keywords": "",
+            "publisher": ""
         }
 
         wb = openpyxl.load_workbook(filename=excel_file_path, data_only=True)
         o_sheet = wb["Tabelle1"]
 
         for i in range(1, o_sheet.max_row + 1, 1):
-            file_name_sheet = o_sheet.cell(row=i, column=4)
+            file_name_sheet = o_sheet.cell(row=i, column=5)
 
             if file_name_sheet.value == file_name:
-                metadata_h5p["package"] = o_sheet.cell(row=i, column=1).value
+                metadata_h5p["collection"] = o_sheet.cell(row=i, column=1).value
                 metadata_h5p["order"] = o_sheet.cell(row=i, column=2).value
-                metadata_h5p["task"] = o_sheet.cell(row=i, column=3).value
-                metadata_h5p["keywords"] = o_sheet.cell(row=i, column=5).value
+                metadata_h5p["title"] = o_sheet.cell(row=i, column=4).value
+                metadata_h5p["keywords"] = o_sheet.cell(row=i, column=6).value
+                metadata_h5p["publisher"] = o_sheet.cell(row=i, column=7).value
 
         return metadata_h5p
 
@@ -87,7 +89,7 @@ def main():
 
     # extract metadata from excel_file
     file_name = "bio7v2_17_koerpermerkmale-von-schnecken-interaktive-wissensvermittlung.h5p"
-    excel_file_path = 'h5p_files/Biologie 7 Vol 2 - Wirbellose.xlsx'
+    excel_file_path = 'h5p_files/Biologie 7 Vol 2 - Wirbellose - Version 3 - MIT Keywords.xlsx'
 
     metadata_excel = UnzipLocalFile.extract_metadata_from_excel(excel_file_path=excel_file_path, file_name=file_name)
     print(metadata_excel)
