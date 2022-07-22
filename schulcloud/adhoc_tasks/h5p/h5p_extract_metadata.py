e7931a1d8bcfa7ad82a6dfaff59872493b3a6b60
DDEITERS <dirk.deiters.ext@capgemini.com>
2022-07-22 16:14:10 +0200

    clean-up code
    delete outdated method

--- schulcloud/adhoc_tasks/h5p/h5p_extract_metadata.py
+++ schulcloud/adhoc_tasks/h5p/h5p_extract_metadata.py
@@ -1,8 +1,6 @@
 import os
 import zipfile
-import json
 import shutil
-from typing import Dict
 import openpyxl
 
 
@@ -26,20 +24,6 @@ class UnzipLocalFile:
         data = file.read()
         file.close()
         return data
-
-    def extract_metadata_from_json(metadata: [Dict]):
-        metadata_h5p = {
-            "title": "",
-            "copyright_license": ""
-        }
-
-        try:
-            metadata_h5p["title"] = metadata["title"]
-            metadata_h5p["copyright_license"] = metadata["license"]
-        except KeyError:
-            print(f'KeyError: The JSON schema of the file doesn\'t match with the method schema.')
-
-        return metadata_h5p
 
     def extract_metadata_from_excel(excel_file_path: str, file_name: str):
         metadata_h5p = {
@@ -65,35 +49,3 @@ class UnzipLocalFile:
 
         return metadata_h5p
 
-
-def main():
-    # create local folder 'unzipped'
-    UnzipLocalFile.create_local_folder(folder_name='unzipped')
-    print("Create folder \'unzipped\'.")
-
-    # unzip the h5p-file
-    h5p_file = "alice.h5p"
-    UnzipLocalFile.unzip_local_file(file_path=f"h5p_files/{h5p_file}", unzipped_dir="unzipped")
-
-    # read out the lines, e.g. title
-    metadata = json.loads(UnzipLocalFile.read_file(file_path="unzipped/h5p.json"))
-    metadata_h5p = UnzipLocalFile.extract_metadata_from_json(metadata=metadata)
-
-    print(metadata)
-    print(f'title: {metadata_h5p["title"]}')
-    print(f'copyright_license: {metadata_h5p["copyright_license"]}')
-
-    # delete the 'unzipped'-folder including all files
-    UnzipLocalFile.delete_local_folder(folder_name="unzipped")
-    print("Delete folder \'unzipped\'")
-
-    # extract metadata from excel_file
-    file_name = "bio7v2_17_koerpermerkmale-von-schnecken-interaktive-wissensvermittlung.h5p"
-    excel_file_path = 'h5p_files/Biologie 7 Vol 2 - Wirbellose - Version 3 - MIT Keywords.xlsx'
-
-    metadata_excel = UnzipLocalFile.extract_metadata_from_excel(excel_file_path=excel_file_path, file_name=file_name)
-    print(metadata_excel)
-
-
-if __name__ == '__main__':
-    main()
