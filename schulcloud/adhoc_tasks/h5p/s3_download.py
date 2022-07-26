523b942ffb5f13054b7cb2a191c8acba5ab13b0f
Andre Blome <andre-david.blome@capgemini.com>
2022-07-26 15:14:54 +0200

    H5P: s3 download script poc

---
+++ schulcloud/adhoc_tasks/h5p/s3_download.py
@@ -0,0 +1,40 @@
+
+import tqdm
+import boto3
+
+import util
+
+
+def main():
+    env = util.Environment()
+
+    client = boto3.client(
+        's3',
+        endpoint_url=env['S3_ENDPOINT_URL'],
+        aws_access_key_id=env['S3_ACCESS_KEY'],
+        aws_secret_access_key=env['S3_SECRET_KEY'],
+    )
+    bucket_name = env['S3_BUCKET_NAME']
+
+    response = client.list_buckets()
+    for bucket in response['Buckets']:
+        if bucket['Name'] == bucket_name:
+            break
+    else:
+        raise RuntimeError(f'Bucket {bucket_name} does not exist')
+
+    response = client.list_objects_v2(Bucket=bucket_name)
+    objects = response['Contents']
+
+    print(f'Download all objects from bucket {bucket_name}')
+    total_size = sum([obj['Size'] for obj in objects])
+    progress_bar = tqdm.tqdm(total=total_size, unit='B', unit_scale=True, unit_divisor=1024)
+    for obj in objects:
+        progress_bar.set_description(obj['Key'])
+        client.download_file(Bucket=bucket_name, Key=obj['Key'], Filename=obj['Key'])
+        progress_bar.update(obj['Size'])
+    progress_bar.close()
+
+
+if __name__ == '__main__':
+    main()
