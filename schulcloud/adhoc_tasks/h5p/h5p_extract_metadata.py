df7b061b8c1490387829ea6a1e00becb67049386
Andre Blome <andre-david.blome@capgemini.com>
2022-07-27 11:08:09 +0200

    H5P: excel file validation, get_collection added

--- schulcloud/adhoc_tasks/h5p/h5p_extract_metadata.py
+++ schulcloud/adhoc_tasks/h5p/h5p_extract_metadata.py
@@ -1,11 +1,12 @@
 
 import os
 import re
+import sys
 import zipfile
 from typing import List, Union, IO
 
 import openpyxl
-
+from openpyxl.utils import get_column_letter
 
 class Metadata:
     def __init__(self, title: str, publisher: str, keywords: List[str], order: str, rating: str, collection: str):
@@ -18,23 +19,64 @@ class Metadata:
 
 
 class MetadataFile:
+    class COLUMN:
+        COLLECTION = 1
+        ORDER = 2
+        RATING = 3
+        TITLE = 4
+        FILENAME = 5
+        KEYWORDS = 6
+        PUBLISHER = 7
+        COUNT = 7
+
     def __init__(self, file: Union[str, IO]):
         self.workbook = openpyxl.load_workbook(filename=file, data_only=True)
         self.o_sheet = self.workbook["Tabelle1"]
+        self.validate_sheet()
+
+    def validate_sheet(self):
+        collection = self.get_collection()
+        for row in range(1, self.o_sheet.max_row + 1):
+            for column in range(1, self.COLUMN.COUNT + 1):
+                if column == self.COLUMN.KEYWORDS:
+                    continue
+                if not self.o_sheet.cell(row=row, column=column).value:
+                    raise ParsingError(f'Empty cell: {get_column_letter(column)}{row}')
+        if not collection:
+            raise ParsingError('No collection')
+        for row in range(1, self.o_sheet.max_row + 1):
+            other_collection = self.o_sheet.cell(row=row, column=self.COLUMN.COLLECTION).value
+            if not other_collection == collection:
+                raise ParsingError('Multiple collection in one excel file')
+
+    def check_for_files(self, filenames: List[str]):
+        existing_filenames = []
+        missing_filenames = []
+        for row in range(1, self.o_sheet.max_row + 1):
+            existing_filenames.append(self.o_sheet.cell(row=row, column=self.COLUMN.FILENAME).value)
+        for filename in filenames:
+            if filename not in existing_filenames:
+                missing_filenames.append(filename)
+        if missing_filenames:
+            print('Missing in excel file:', file=sys.stderr)
+            for filename in missing_filenames:
+                print(f'  {filename}', file=sys.stderr)
+            raise ParsingError('The excel file is missing metadata')
+
+    def get_collection(self):
+        return self.o_sheet.cell(row=1, column=self.COLUMN.COLLECTION).value
 
     def get_metadata(self, h5p_file: str):
-        for i in range(1, self.o_sheet.max_row + 1):
-            file_name_sheet = self.o_sheet.cell(row=i, column=5)
+        for row in range(1, self.o_sheet.max_row + 1):
+            file_name_sheet = self.o_sheet.cell(row=row, column=self.COLUMN.FILENAME)
 
             if file_name_sheet.value == h5p_file:
-                collection = self.o_sheet.cell(row=i, column=1).value
-                order = self.o_sheet.cell(row=i, column=2).value
-                rating = self.o_sheet.cell(row=i, column=3).value
-                title = self.o_sheet.cell(row=i, column=4).value
-                keywords = self.o_sheet.cell(row=i, column=6).value
-                publisher = self.o_sheet.cell(row=i, column=7).value
-
-                # TODO: validate all data coming from excel file
+                collection = self.o_sheet.cell(row=row, column=self.COLUMN.COLLECTION).value
+                order = self.o_sheet.cell(row=row, column=self.COLUMN.ORDER).value
+                rating = self.o_sheet.cell(row=row, column=self.COLUMN.RATING).value
+                title = self.o_sheet.cell(row=row, column=self.COLUMN.TITLE).value
+                keywords = self.o_sheet.cell(row=row, column=self.COLUMN.KEYWORDS).value
+                publisher = self.o_sheet.cell(row=row, column=self.COLUMN.PUBLISHER).value
 
                 keywords = re.findall(r'[^,; ]+', keywords)
                 break
@@ -42,3 +84,8 @@ class MetadataFile:
             raise RuntimeError(f'No metadata found for {h5p_file}')
 
         return Metadata(title, publisher, keywords, order, rating, collection)
+
+
+class ParsingError(Exception):
+    def __init__(self, msg: str):
+        super(ParsingError, self).__init__(msg)
