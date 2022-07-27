7689af1e3f9cb9c4171e2d3d20fc3f512a7b947a
Andre Blome <andre-david.blome@capgemini.com>
2022-07-27 11:09:38 +0200

    H5P: upload is now distinct between thuringia and general h5p upload; refactorings

--- schulcloud/adhoc_tasks/h5p/h5p_upload.py
+++ schulcloud/adhoc_tasks/h5p/h5p_upload.py
@@ -3,7 +3,7 @@ import uuid
 import zipfile
 from datetime import datetime
 import hashlib
-from typing import Optional, BinaryIO, List
+from typing import Optional, List, Dict, IO
 
 import edusharing
 import h5p_extract_metadata
@@ -12,8 +12,8 @@ import util
 
 ENV_VARS = ['EDU_SHARING_BASE_URL', 'EDU_SHARING_USERNAME', 'EDU_SHARING_PASSWORD']
 H5P_LOCAL_PATH = 'h5p_files'
-REPLICATION_SOURCE = 'h5p'
-FOLDER_NAME = REPLICATION_SOURCE
+FOLDER_NAME_GENERAL = 'h5p'
+FOLDER_NAME_THURINGIA = 'h5p-thuringia'
 
 
 def generate_node_properties(
@@ -21,6 +21,7 @@ def generate_node_properties(
         name: str,
         publisher: str,
         keywords: List[str],
+        folder_name: str,
         replication_source_id: Optional[str] = None,
         replication_source_uuid: Optional[str] = None,
         url: str = '',
@@ -52,7 +53,7 @@ def generate_node_properties(
         "ccm:ph_invited": ["GROUP_public"],
         "ccm:ph_action": ["PERMISSION_ADD"],
         "ccm:objecttype": ["MATERIAL"],
-        "ccm:replicationsource": [REPLICATION_SOURCE],
+        "ccm:replicationsource": [folder_name],
         "ccm:replicationsourceid": [hashlib.sha1(replication_source_id.encode()).hexdigest()],
         "ccm:replicationsourcehash": [date],
         "ccm:replicationsourceuuid": [replication_source_uuid],
@@ -82,21 +83,30 @@ class Uploader:
             self.env['EDU_SHARING_USERNAME'],
             self.env['EDU_SHARING_PASSWORD'])
 
-        self.permitted_groups = ["Brandenburg-public", "Thuringia-public"]
+    def setup_destination_folder(self, folder_name: str, permitted_groups: Optional[List[str]]):
+        if not permitted_groups:
+            permitted_groups = ["Brandenburg-public", "Thuringia-public", "LowerSaxony-public"]
 
         sync_obj = self.api.get_sync_obj_folder()
-        self.destination_folder = self.api.get_or_create_folder(sync_obj.id, FOLDER_NAME)
+        destination_folder = self.api.find_node_by_name(sync_obj.id, folder_name)
+        if not destination_folder:
+            destination_folder = self.api.get_or_create_folder(sync_obj.id, folder_name)
 
-    def upload(self, filename: str, metadata: h5p_extract_metadata.Metadata, file: Optional[BinaryIO] = None):
+            # set permissions for the package
+            self.api.set_permissions(destination_folder.id, permitted_groups, False)
+            print(f"Created folder {folder_name} with permissions for: {permitted_groups}")
+
+        return destination_folder
+
+    def upload_h5p_file(self, folder_name: str, filename: str, metadata: h5p_extract_metadata.Metadata, file: Optional[IO[bytes]] = None, relation: Optional[Dict] = None):
         # get h5p file, add metadata, upload and after all add permissions
         name = os.path.splitext(os.path.basename(filename))[0]
         keywords = ['h5p', metadata.title, metadata.collection, metadata.order] + metadata.keywords
 
         # ToDo: Add the url of the frontend rendering page
-        properties = generate_node_properties(metadata.order, metadata.title, metadata.publisher, keywords, replication_source_id=name)
+        properties = generate_node_properties(metadata.order, metadata.title, metadata.publisher, keywords, folder_name, replication_source_id=name, relation=relation)
 
-        node = self.api.sync_node(REPLICATION_SOURCE, properties,
-                             ['ccm:replicationsource', 'ccm:replicationsourceid'])
+        node = self.api.sync_node(folder_name, properties, ['ccm:replicationsource', 'ccm:replicationsourceid'])
 
         if node.size is not None:
             print(f'Already exists: {filename}')
@@ -116,7 +126,14 @@ class Uploader:
         print(f'Upload complete for: {filename}')
         return node.id, properties["ccm:replicationsourceuuid"]
 
-    def main(self):
+    def upload_h5p_general(self, edusharing_folder_name: str, permitted_groups: Optional[List[str]] = None):
+        destination_folder = self.setup_destination_folder(edusharing_folder_name, permitted_groups)
+        # TODO
+        pass
+
+    def upload_h5p_thuringia(self, edusharing_folder_name: str):
+        permitted_groups = ['Thuringia-public']
+        destination_folder = self.setup_destination_folder(edusharing_folder_name, permitted_groups)
 
         # ToDo:
         #  1. Check the metadata especially name, title and keywords (for the search query in the frontend)
@@ -141,56 +158,46 @@ class Uploader:
                 raise RuntimeError('Could not find excel file with metadata')
 
             # save the replicationsourceuuid, nodeId and the collection of each h5p-file corresponding to this package
-            package_h5p_files = []
-            package_h5p_files_nodeId = []
-            collection = ''
+            package_h5p_files_rep_source_uuids = []
+            collection_name = metadata_file.get_collection()
+
+            # now update metadata from the new node (add children) and the h5p-files (add parent)
+            keywords = ["h5p", collection_name, "Arbeitspaket"]
+            properties = generate_node_properties(
+                collection_name, collection_name, "MedienLB", keywords, edusharing_folder_name, format="text/html", aggregation_level=2
+            )
+            collection_rep_source_uuid = properties['ccm:replicationsourceuuid']
+            collection_node = self.api.sync_node(edusharing_folder_name, properties, ['ccm:replicationsource', 'ccm:replicationsourceid'])
+            print(f'Created Collection {collection_name}.')
 
             # loop through the unzipped h5p-files
-            for root, dirs, files in os.walk("unzipped"):
-                for filename in files:
-                    if filename.endswith(".h5p"):
-                        metadata = metadata_file.get_metadata(filename)
-                        if collection and not metadata.collection == collection:
-                            raise RuntimeError('Collection suddenly changed')
-                        collection = metadata.collection
-                        # TODO: multiple collections in one excel file / one call?
+            for filename in zip.namelist():
+                if filename.endswith(".h5p"):
+                    metadata = metadata_file.get_metadata(filename)
+                    file = zip.open(filename)
 
-                        node_id, rep_source_uuid = self.upload(os.path.join(root, filename), metadata)
-                        package_h5p_files_nodeId.append(node_id)
-                        package_h5p_files.append(rep_source_uuid)
+                    relation = {
+                        'kind': 'ispartof',
+                        'resource': {
+                            'identifier': [collection_rep_source_uuid]
+                        }
+                    }
+
+                    node_id, rep_source_uuid = self.upload_h5p_file(edusharing_folder_name, filename, metadata, file=file, relation=relation)
+                    package_h5p_files_rep_source_uuids.append(rep_source_uuid)
 
             excel_file.close()
             zip.close()
 
-            # set permissions for the package
-            self.api.set_permissions(self.destination_folder.id, self.permitted_groups, False)
-            print("Set permissions for: " + str(self.permitted_groups))
-
-            # now update metadata from the new node (add children) and the h5p-files (add parent)
-
-            name = collection
-            # name = os.path.splitext(filename)[0]
-            relation = "{'kind': 'hasparts', 'resource': {'identifier': " + str(package_h5p_files) + "}}"
-            keywords = ["h5p", name, "Arbeitspaket"]
-            properties = generate_node_properties(
-                name, name, "MedienLB", keywords, relation=relation, format="text/html", aggregation_level=2
-            )
-
-            self.api.sync_node(REPLICATION_SOURCE, properties, ['ccm:replicationsource', 'ccm:replicationsourceid'])
-            print(f'Created Collection {name}.')
-
-            # update the h5p-files with parent-nodeId
-            parent_uuid = properties["ccm:replicationsourceuuid"]
-
-            for node in package_h5p_files_nodeId:
-                # TODO: check whether those calls are really necessary
-                # TODO: if they are necessary, use urllib.parse.urlencode to do the url encoding
-                url_update_lom = f'/node/v1/nodes/-home-/{node}/property?property=ccm%3Alom_relation&value=%7B\'kind\'%3A%20\'ispartof\'%2C%20\'resource\'%3A%20%7B\'identifier\'%3A%20{parent_uuid}%7D'
-                url_update_lom_hpi = f'/node/v1/nodes/-home-/{node}/property?property=ccm%3Ahpi_lom_relation&value=%7B\'kind\'%3A%20\'ispartof\'%2C%20\'resource\'%3A%20%7B\'identifier\'%3A%20{parent_uuid}%7D'
-                response_update = self.api.make_request('POST', url_update_lom)
-                self.api.make_request('POST', url_update_lom_hpi)
-                print(f'Response status {response_update.status_code} for node: {node}')
+            relation = {
+                'kind': 'hasparts',
+                'resource': {
+                    'identifier': package_h5p_files_rep_source_uuids
+                }
+            }
+            self.api.set_property(collection_node.id, 'ccm:lom_relation', relation)
+            self.api.set_property(collection_node.id, 'ccm:hpi_lom_relation', relation)
 
 
 if __name__ == '__main__':
-    Uploader().main()
+    Uploader().upload_h5p_thuringia(FOLDER_NAME_THURINGIA)
