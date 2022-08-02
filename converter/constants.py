3108988ce78a01df79a5accf0ab1db3a9667da49
criamos <981166+Criamos@users.noreply.github.com>
2022-08-02 19:57:49 +0200

    add: typehints to Constants class attributes
    - in accordance to PEP 591 and PEP 484
    - replace "http"-links with "https"
    - questions in regard to LICENSE_MAPPINGS remain

--- converter/constants.py
+++ converter/constants.py
@@ -1,20 +1,23 @@
-class Constants:
-    LICENSE_CC_BY_30 = "https://creativecommons.org/licenses/by/3.0/"
-    LICENSE_CC_BY_40 = "https://creativecommons.org/licenses/by/4.0/"
-    LICENSE_CC_BY_NC_30 = "https://creativecommons.org/licenses/by-nc/3.0/"
-    LICENSE_CC_BY_NC_40 = "https://creativecommons.org/licenses/by-nc/4.0/"
-    LICENSE_CC_BY_NC_ND_30 = "https://creativecommons.org/licenses/by-nc-nd/3.0/"
-    LICENSE_CC_BY_NC_ND_40 = "https://creativecommons.org/licenses/by-nc-nd/4.0/"
-    LICENSE_CC_BY_NC_SA_30 = "https://creativecommons.org/licenses/by-nc-sa/3.0/"
-    LICENSE_CC_BY_NC_SA_40 = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
-    LICENSE_CC_BY_ND_30 = "https://creativecommons.org/licenses/by-nd/3.0/"
-    LICENSE_CC_BY_ND_40 = "https://creativecommons.org/licenses/by-nd/4.0/"
-    LICENSE_CC_BY_SA_30 = "https://creativecommons.org/licenses/by-sa/3.0/"
-    LICENSE_CC_BY_SA_40 = "https://creativecommons.org/licenses/by-sa/4.0/"
-    LICENSE_CC_ZERO_10 = "https://creativecommons.org/publicdomain/zero/1.0/"
-    LICENSE_PDM = "https://creativecommons.org/publicdomain/mark/1.0/"
+from typing import Final, Any
 
-    VALID_LICENSE_URLS = [
+
+class Constants:
+    LICENSE_CC_BY_30: Final[str] = "https://creativecommons.org/licenses/by/3.0/"
+    LICENSE_CC_BY_40: Final[str] = "https://creativecommons.org/licenses/by/4.0/"
+    LICENSE_CC_BY_NC_30: Final[str] = "https://creativecommons.org/licenses/by-nc/3.0/"
+    LICENSE_CC_BY_NC_40: Final[str] = "https://creativecommons.org/licenses/by-nc/4.0/"
+    LICENSE_CC_BY_NC_ND_30: Final[str] = "https://creativecommons.org/licenses/by-nc-nd/3.0/"
+    LICENSE_CC_BY_NC_ND_40: Final[str] = "https://creativecommons.org/licenses/by-nc-nd/4.0/"
+    LICENSE_CC_BY_NC_SA_30: Final[str] = "https://creativecommons.org/licenses/by-nc-sa/3.0/"
+    LICENSE_CC_BY_NC_SA_40: Final[str] = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
+    LICENSE_CC_BY_ND_30: Final[str] = "https://creativecommons.org/licenses/by-nd/3.0/"
+    LICENSE_CC_BY_ND_40: Final[str] = "https://creativecommons.org/licenses/by-nd/4.0/"
+    LICENSE_CC_BY_SA_30: Final[str] = "https://creativecommons.org/licenses/by-sa/3.0/"
+    LICENSE_CC_BY_SA_40: Final[str] = "https://creativecommons.org/licenses/by-sa/4.0/"
+    LICENSE_CC_ZERO_10: Final[str] = "https://creativecommons.org/publicdomain/zero/1.0/"
+    LICENSE_PDM: Final[str] = "https://creativecommons.org/publicdomain/mark/1.0/"
+
+    VALID_LICENSE_URLS: list[str | Any] = [
         LICENSE_CC_BY_30,
         LICENSE_CC_BY_40,
         LICENSE_CC_BY_NC_30,
@@ -30,16 +33,21 @@ class Constants:
         LICENSE_CC_ZERO_10,
         LICENSE_PDM,
     ]
-    LICENSE_MAPPINGS = {
-        "https://creativecommons.org/publicdomain/zero/": LICENSE_CC_ZERO_10,
-        "https://creativecommons.org/licenses/by/": LICENSE_CC_BY_40,
-        "https://creativecommons.org/licenses/by-sa/": LICENSE_CC_BY_SA_40,
-        # wrong mapping (currently from edu-sharing)
-        "https://creativecommons.org/licenses/pdm/": LICENSE_PDM,
+    LICENSE_MAPPINGS: dict[str, str] = {
+        "https://creativecommons.org/licenses/by/": LICENSE_CC_BY_40,  # ToDo: outdated approximation?
+        # ToDo: - CC_BY_NC (3.0 + 4.0)
         "https://creativecommons.org/licenses/by-nc-nd/3.0/": LICENSE_CC_BY_NC_ND_30,
         "https://creativecommons.org/licenses/by-nc-nd/4.0/": LICENSE_CC_BY_NC_ND_40,
+        # ToDo:
+        #  - CC_BY_NC_SA (3.0 + 4.0)
+        #  - CC_BY_ND (3.0 + 4.0)
+        #  - CC_BY_SA (3.0)
+        "https://creativecommons.org/licenses/by-sa/": LICENSE_CC_BY_SA_40,  # Todo: outdated approximation?
+        # wrong mapping (currently from edu-sharing)
+        "https://creativecommons.org/publicdomain/zero/": LICENSE_CC_ZERO_10,
+        "https://creativecommons.org/licenses/pdm/": LICENSE_PDM,
     }
-    LICENSE_MAPPINGS_INTERNAL = {
+    LICENSE_MAPPINGS_INTERNAL: dict[str, list[str]] = {
         "CC_0": [LICENSE_CC_ZERO_10],
         "CC_BY": [LICENSE_CC_BY_40, LICENSE_CC_BY_30],
         "CC_BY_SA": [LICENSE_CC_BY_SA_40, LICENSE_CC_BY_SA_30],
@@ -47,15 +55,15 @@ class Constants:
         "PDM": [LICENSE_PDM],
     }
 
-    LICENSE_COPYRIGHT_LAW = "COPYRIGHT_LAW"
-    LICENSE_CUSTOM = "CUSTOM" # Custom License, use the license description field for arbitrary values
-    LICENSE_NONPUBLIC = "NONPUBLIC"
+    LICENSE_COPYRIGHT_LAW: Final[str] = "COPYRIGHT_LAW"
+    LICENSE_CUSTOM: Final[str] = "CUSTOM"  # Custom License, use the license description field for arbitrary values
+    LICENSE_NONPUBLIC: Final[str] = "NONPUBLIC"
 
-    NEW_LRT_MATERIAL = "http://w3id.org/openeduhub/vocabs/new_lrt/1846d876-d8fd-476a-b540-b8ffd713fedb"
-    NEW_LRT_TOOL = "http://w3id.org/openeduhub/vocabs/new_lrt/cefccf75-cba3-427d-9a0f-35b4fedcbba1"
+    NEW_LRT_MATERIAL: Final[str] = "https://w3id.org/openeduhub/vocabs/new_lrt/1846d876-d8fd-476a-b540-b8ffd713fedb"
+    NEW_LRT_TOOL: Final[str] = "https://w3id.org/openeduhub/vocabs/new_lrt/cefccf75-cba3-427d-9a0f-35b4fedcbba1"
 
-    SOURCE_TYPE_SPIDER = 1
-    SOURCE_TYPE_EDITORIAL = 2
+    SOURCE_TYPE_SPIDER: int = 1
+    SOURCE_TYPE_EDITORIAL: int = 2
 
 
 class OerType:
