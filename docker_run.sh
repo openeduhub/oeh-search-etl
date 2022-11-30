f05ae3e9b7e64234a38b66ebc0be21ba6a77e59b
Andre Blome <andre-david.blome@capgemini.com>
2022-11-30 12:47:50 +0100

    made docker run script interactive

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-crawler="sodix_spider"
-echo "EDU_SHARING_BASE_URL=$EDU_SHARING_BASE_URL"
-echo "EDU_SHARING_USERNAME=$EDU_SHARING_USERNAME"
-echo "SODIX_USER=$SODIX_USER"
-echo "CRAWLER=$crawler"
-docker run -e "EDU_SHARING_BASE_URL=$EDU_SHARING_BASE_URL" -e "EDU_SHARING_USERNAME=$EDU_SHARING_USERNAME" -e "EDU_SHARING_PASSWORD=$EDU_SHARING_PASSWORD" -e "SODIX_USER=$SODIX_USER" -e "SODIX_PASSWORD=$SODIX_PASSWORD" -e "CRAWLER=$crawler" oeh-search-etl
+crawler="hello_world"
+schedule="*-*-12:45"
+blacklist="/oeh-search-etl/schulcloud/sodix/blacklist.json"
+docker run -it -e "EDU_SHARING_BASE_URL=$EDU_SHARING_BASE_URL" -e "EDU_SHARING_USERNAME=$EDU_SHARING_USERNAME" -e "EDU_SHARING_PASSWORD=$EDU_SHARING_PASSWORD" -e "SODIX_USER=$SODIX_USER" -e "SODIX_PASSWORD=$SODIX_PASSWORD" -e "SODIX_BLACKLIST_PATH=$blacklist" -e "SCHEDULE=$schedule" -e "CRAWLER=$crawler" oeh-search-etl
