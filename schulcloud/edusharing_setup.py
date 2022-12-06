ccf819fcd116f7114eea3ec754f027172b11bada
Andre Blome <andre-david.blome@capgemini.com>
2022-12-06 15:36:48 +0100

    edusharing setup: h5p color picker added

--- schulcloud/edusharing_setup.py
+++ schulcloud/edusharing_setup.py
@@ -1,12 +1,13 @@
 import json
+import os.path
 from dataclasses import dataclass
 from typing import List, Set, Literal
 
-from schulcloud.edusharing import EdusharingAPI
+from schulcloud.edusharing import EdusharingAPI, NotFoundException
 from schulcloud.util import Environment
 
 
-ENV_VARS = ['EDU_SHARING_BASE_URL', 'EDU_SHARING_USERNAME', 'EDU_SHARING_PASSWORD']
+ENV_VARS = ['EDU_SHARING_BASE_URL', 'EDU_SHARING_USERNAME', 'EDU_SHARING_PASSWORD', 'COLORPICKER_PATH']
 
 
 @dataclass
@@ -19,11 +20,11 @@ class User:
 
 class EdusharingSetup:
     def __init__(self):
-        environment = Environment(ENV_VARS, ask_for_missing=True)
+        self.env = Environment(ENV_VARS, ask_for_missing=True)
         self.api = EdusharingAPI(
-            environment['EDU_SHARING_BASE_URL'],
-            environment['EDU_SHARING_USERNAME'],
-            environment['EDU_SHARING_PASSWORD'])
+            self.env['EDU_SHARING_BASE_URL'],
+            self.env['EDU_SHARING_USERNAME'],
+            self.env['EDU_SHARING_PASSWORD'])
 
     def _add_metadata_sets(self):
         xml_name = 'homeApplication.properties.xml'
@@ -35,6 +36,7 @@ class EdusharingSetup:
 
     def _add_users_and_groups(self, users: List[User], groups: Set[str]):
         # requirement: all groups within users must also be within groups
+        # TODO: change only if users and groups do not exist
         for user in users:
             self.api.create_user(user.name, user.password, user.type)
             print(f'Created user {user.name}')
@@ -46,6 +48,19 @@ class EdusharingSetup:
             for group in user.groups:
                 self.api.group_add_user(group, user.name)
 
+    def _upload_color_picker(self):
+        # the color picker h5p content contains the color picker library needed for other h5p items
+        # which will be installed after upload
+        colorpicker_path = self.env['COLORPICKER_PATH']
+        colorpicker_name = os.path.basename(colorpicker_path)
+        try:
+            self.api.find_node_by_name('-userhome-', colorpicker_name)
+        except NotFoundException:
+            node = self.api.create_node('-userhome-', colorpicker_name)
+            file = open(colorpicker_path, 'wb')
+            self.api.upload_content(node.id, colorpicker_name, file)
+            file.close()
+
     def run(self, users: List[User], groups: List[str]):
         groups = set(groups)
         for user in users:
@@ -54,6 +69,7 @@ class EdusharingSetup:
 
         self._add_metadata_sets()
         self._add_users_and_groups(users, groups)
+        self._upload_color_picker()
 
 
 def temporary_setup():
