809db250cfb65f200aa4ddef463de8a498776db6
criamos <981166+Criamos@users.noreply.github.com>
2023-03-07 15:31:09 +0100

    tutory_spider v0.1.3
    - change: 'license.internal' no longer defaults to 'copyright'
    -- this change was requested as a temporary workaround until mixed Tutory licenses can be systematically determined
    - fix: API pagination
    -- the previous method of paginating through Tutory's API didn't work anymore because the old 'pageSize'-URL-parameter nowadays returns an HTTP Error 502 (Bad Gateway)
    -- tutory_spider will attempt to crawl through the API pages in iterations of 5000. If the API returns similar HTTP Errors in the future, try lowering the "api_pagesize_limit"-variable.
    - style: code formatting (via black)
    
    Signed-off-by: criamos <981166+Criamos@users.noreply.github.com>

--- converter/spiders/tutory_spider.py
+++ converter/spiders/tutory_spider.py
@@ -1,8 +1,10 @@
+import logging
+import re
+
 import scrapy
 from scrapy.selector import Selector
 from scrapy.spiders import CrawlSpider
 
-from converter.constants import Constants
 from .base_classes import LomBase, JSONBase
 
 
@@ -12,27 +14,61 @@ class TutorySpider(CrawlSpider, LomBase, JSONBase):
     url = "https://www.tutory.de/"
     objectUrl = "https://www.tutory.de/bereitstellung/dokument/"
     baseUrl = "https://www.tutory.de/api/v1/share/"
-    version = "0.1.2"  # last update: 2022-05-23
-    custom_settings = {
-        "AUTOTHROTTLE_ENABLED": True,
-        "ROBOTSTXT_OBEY": False,
-        "AUTOTHROTTLE_DEBUG": True
-    }
+    version = "0.1.3"  # last update: 2022-03-07
+    custom_settings = {"AUTOTHROTTLE_ENABLED": True, "ROBOTSTXT_OBEY": False, "AUTOTHROTTLE_DEBUG": True}
+
+    api_pagesize_limit = 5000
+    # the old API pageSize of 999999 (which was used in 2021) doesn't work anymore and throws a 502 Error (Bad Gateway).
+    # Setting the pageSize to 5000 appears to be a reasonable value with an API response time of 12-15s
 
     def __init__(self, **kwargs):
         LomBase.__init__(self, **kwargs)
 
     def start_requests(self):
-        url = self.baseUrl + "worksheet?groupSlug=entdecken&pageSize=999999"
-        yield scrapy.Request(url=url, callback=self.parse_list)
+        first_url: str = self.assemble_tutory_api_url(api_page=0)
+        yield scrapy.Request(url=first_url, callback=self.parse_api_page)
 
-    def parse_list(self, response: scrapy.http.TextResponse):
-        data = response.json()
-        for j in data["worksheets"]:
-            response_copy = response.replace(url=self.objectUrl + j["id"])
-            response_copy.meta["item"] = j
-            if self.hasChanged(response_copy):
-                yield self.parse(response_copy)
+    def parse_api_page(self, response: scrapy.http.TextResponse):
+        """
+        This method tries to parse the current pagination parameter from response.url and yields two types of
+        scrapy.Requests:
+        1) if the "worksheets"-list isn't empty, try to crawl the next API page
+        2) if there are "worksheets" in the current JSON Response, try to crawl the individual items
+        """
+        json_data: dict = response.json()
+        page_regex = re.compile(r"&page=(?P<page>\d+)")
+        pagination_parameter = page_regex.search(response.url)
+        pagination_current_page: int = 0
+        if pagination_parameter:
+            pagination_current_page: int = pagination_parameter.groupdict().get("page")
+        if "total" in json_data:
+            total_items = json_data.get("total")
+            logging.info(
+                f"Currently crawling Tutory API page {pagination_current_page} -> {response.url} // "
+                f"Expected items (in total): {total_items}"
+            )
+        pagination_next_page: int = int(pagination_current_page) + 1
+        url_next_page = self.assemble_tutory_api_url(pagination_next_page)
+        if "worksheets" in json_data:
+            worksheets_data: list = json_data.get("worksheets")
+            if worksheets_data:
+                # only crawl the next page if the "worksheets"-dict isn't empty
+                yield scrapy.Request(url=url_next_page, callback=self.parse_api_page)
+                logging.info(
+                    f"Tutory API page {pagination_current_page} is expected to yield " f"{len(worksheets_data)} items."
+                )
+                for j in worksheets_data:
+                    response_copy = response.replace(url=self.objectUrl + j["id"])
+                    response_copy.meta["item"] = j
+                    if self.hasChanged(response_copy):
+                        yield self.parse(response_copy)
+
+    def assemble_tutory_api_url(self, api_page: int):
+        url_current_page = (
+            f"{self.baseUrl}worksheet?groupSlug=entdecken&pageSize={str(self.api_pagesize_limit)}"
+            f"&page={str(api_page)}"
+        )
+        return url_current_page
 
     def getId(self, response=None):
         return str(response.meta["item"]["id"])
@@ -64,27 +100,22 @@ class TutorySpider(CrawlSpider, LomBase, JSONBase):
             )
         )
         valuespaces.add_value("discipline", discipline)
-
-        # valuespaces.add_value("learningResourceType", "worksheet")  # remove this value when reaching crawler v0.1.3
         valuespaces.add_value("new_lrt", "36e68792-6159-481d-a97b-2c00901f4f78")  # Arbeitsblatt
         return valuespaces
 
     def getLicense(self, response=None):
         license_loader = LomBase.getLicense(self, response)
-        license_loader.add_value("internal", Constants.LICENSE_COPYRIGHT_LAW)
         return license_loader
 
     def getLOMGeneral(self, response=None):
         general = LomBase.getLOMGeneral(self, response)
         general.add_value("title", response.meta["item"]["name"])
-        if 'description' in response.meta["item"]:
+        if "description" in response.meta["item"]:
             general.add_value("description", response.meta["item"]["description"])
         else:
             html = self.getUrlData(response.url)["html"]
             if html:
-                data = (
-                    Selector(text=html).xpath('//ul[contains(@class,"worksheet-pages")]//text()').getall()
-                )
+                data = Selector(text=html).xpath('//ul[contains(@class,"worksheet-pages")]//text()').getall()
                 cutoff = 4
                 if len(data) > cutoff:
                     for i in range(cutoff):
