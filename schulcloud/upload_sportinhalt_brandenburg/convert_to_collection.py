7124ddd6247ef074ca624fe8ab3237b1e31e1bee
Andre Blome <andre-david.blome@capgemini.com>
2023-03-29 16:49:57 +0200

    brb sportinhalte: updated for new edusharing version, uses edusharing.py now, refactorings

--- schulcloud/upload_sportinhalt_brandenburg/convert_to_collection.py
+++ schulcloud/upload_sportinhalt_brandenburg/convert_to_collection.py
@@ -1,38 +1,27 @@
-import json
-from http.client import HTTPResponse
-import requests
-from schulcloud.upload_sportinhalt_brandenburg.sportinhalt_utils import get_configuration, get_base_headers, \
-    edusharing_set_property, edusharing_change_metadata
 from tqdm import tqdm
 
-def identify_collections_in_directory(auth_response, directory_node_id):
+from schulcloud.upload_sportinhalt_brandenburg.sportinhalt_utils import get_api
+
+
+def identify_collections_in_directory(directory_node_id: str):
     """
     This is a method to identify and return any collections in a given directory node id.
 
     We *only* consider combinations of directories, which contain the children of a given collection, and files,
      which are the parents of the aforementioned collections. All remaining files and directories will be ignored.
     """
-
-    configuration = get_configuration()
-
-    at_uri = "/edu-sharing/rest/node/v1/nodes/-home-/" + directory_node_id + "/children?maxItems=500&skipCount=0"
-    at_url = configuration["parsed_edusharing_url"].scheme + "://" + configuration["hostname"] + at_uri
-
-    headers = get_base_headers(auth_response)
-    r = requests.request("GET", at_url, headers=headers, allow_redirects=False)
-
-    response_dict = json.loads(r.text)
+    nodes = get_api().get_children(directory_node_id)
 
     collections = {}
-    for response in response_dict["nodes"]:
+    for node in nodes:
         # Directory or filename (removing any extensions).
-        name = response["name"].rsplit(".", 1)[0]
+        name = node.name.rsplit(".", 1)[0]
         if name not in collections:
             collections[name] = {}
-        if response["isDirectory"]:
-            collections[name]["directory"] = response
+        if node.is_directory:
+            collections[name]["directory"] = node.obj
         else:
-            collections[name]["parent"] = response
+            collections[name]["parent"] = node.obj
 
     full_collections = {}
     for key in collections:
@@ -41,26 +30,19 @@ def identify_collections_in_directory(auth_response, directory_node_id):
 
     return full_collections
 
-def get_directory_elements(auth_response: HTTPResponse, directory_node_id: str):
-    at_uri = "/edu-sharing/rest/node/v1/nodes/-home-/" + directory_node_id + "/children?maxItems=500&skipCount=0"
 
-    configuration = get_configuration()
-    at_url = configuration["parsed_edusharing_url"].scheme + "://" + configuration["hostname"] + at_uri
+def get_directory_elements(directory_node_id: str):
+    nodes = get_api().get_children(directory_node_id)
+    nodes_by_id = {}
 
-    headers = get_base_headers(auth_response)
-    r = requests.request("GET", at_url, headers=headers, allow_redirects=False)
+    for node in nodes:
+        if not node.is_directory:
+            print(node.name)
+            nodes_by_id[node.id] = node.obj
+    return nodes_by_id
 
-    response_dict = json.loads(r.text)
 
-    node_id_to_reponse = {}
-
-    for response in response_dict["nodes"]:
-        if response["isDirectory"] is False:
-            print(response["name"])
-            node_id_to_reponse[response["ref"]["id"]] = response
-    return node_id_to_reponse
-
-def convert_to_collection(auth_response, parent_element, children_elements):
+def convert_to_collection(parent_element, children_elements):
     # Overwrite some attributes' default values.
 
     # Is the following needed?
@@ -105,5 +87,5 @@ def convert_to_collection(auth_response, parent_element, children_elements):
     for node_id in tqdm(sorted(elements), desc="Converting elements into collection"):
         for property_name in ["ccm:hpi_searchable", "ccm:hpi_lom_general_aggregationlevel", "ccm:hpi_lom_relation"]:
             # Apply changes.
-            edusharing_set_property(node_id, property_name, elements[node_id][property_name], auth_response)
+            get_api().set_property(node_id, property_name, [elements[node_id][property_name]])
         # edusharing_change_metadata(auth_response, node_id, "relation", elements[node_id]["relation"], is_array=True)
--- schulcloud/upload_sportinhalt_brandenburg/modify_sportinhalt.py
+++ schulcloud/upload_sportinhalt_brandenburg/modify_sportinhalt.py
@@ -1,162 +1,103 @@
 # This file assumes all Sportinhalt videos have been manually uploaded to an Edu-Sharing directory, the node-id of which is provided in config.yml
-import base64
 import copy
-import json
 import os
-from http.client import HTTPSConnection, HTTPResponse
-from urllib.parse import urlparse
-
-import requests
-import yaml
 from tqdm import tqdm
-
 
 from converter.es_connector import EduSharing
 from schulcloud.upload_sportinhalt_brandenburg.convert_to_collection import \
     identify_collections_in_directory, get_directory_elements, convert_to_collection
-from schulcloud.upload_sportinhalt_brandenburg.sportinhalt_permissions import \
-    edusharing_add_to_permission_groups, edusharing_get_permission_groups
 
-import pathlib
-
-# Load information required to connect to Edu-Sharing.
-from schulcloud.upload_sportinhalt_brandenburg.sportinhalt_utils import get_base_headers, get_configuration, \
-    authenticate, edusharing_change_metadata, edusharing_set_property
+from schulcloud.upload_sportinhalt_brandenburg.sportinhalt_utils import get_configuration, get_api
 
 
-def add_attributes_to_nodes(auth_response, node_id_to_response):
-    attribute_values = {
-        "ccm:hpi_searchable": "1",
-        "ccm:hpi_lom_general_aggregationlevel": "1",
-        "ccm:hpi_lom_relation": "{}",
-        "ccm:wwwurl": None,
-        "cclom:title": None,
-        "ccm:replicationsource": "Brandenburg (Sportinhalt)",
-        "ccm:replicationsourceuuid": None,
-        "cclom:general_keyword": None
-    }
+def add_attributes_to_nodes(node):
     configuration = get_configuration()
 
-    for node_id in tqdm(sorted(list(node_id_to_response.keys()), key=lambda x: node_id_to_response[x]["name"]),
-                        desc="Adding properties to elements"):
-        node_name = node_id_to_response[node_id]["name"]
-        for k, v in attribute_values.items():
-            if k == "ccm:wwwurl":
-                property_name = k
-                # property_value = node_id_to_reponse[node_id]["content"]["url"]
-                property_value = node_id_to_response[node_id]["downloadUrl"]
-                edusharing_change_metadata(auth_response, node_id, property_name, property_value)
+    node_id = node['ref']['id']
+    filename = get_clean_filename(node)
+    # 01.1 Curriculum Erwaermung.mp4 --> 01.1 --> 01 --> 1
+    # 0 Trailer.mp4 --> 0 --> 0 --> 0
+    chapter = filename.split(" ")[0].split(".")[-1]
+    keywords = list(configuration["general_keywords"])
+    if chapter in configuration["keywords_per_chapter"]:
+        keywords += configuration["keywords_per_chapter"][chapter]
+    metadata = {
+        'ccm:wwwurl': [node["downloadUrl"]],  # node["content"]["url"]
+        'cclom:title': [filename],
+        "ccm:replicationsource": ["Brandenburg (Sportinhalt)"],
+        'ccm:replicationsourceuuid': [EduSharing().buildUUID(filename + configuration["salt"])],
+        'cclom:general_keyword': keywords,
+        "ccm:hpi_searchable": ["1"],
+        "ccm:hpi_lom_general_aggregationlevel": ["1"],
+        "ccm:hpi_lom_relation": ["{}"],
+    }
 
-            elif k == "cclom:title":
-                property_name = k
-                property_value = get_filename(node_id, node_id_to_response)
-                edusharing_change_metadata(auth_response,node_id, property_name, property_value)
-
-            elif k == "ccm:replicationsourceuuid":
-                property_name = k
-                # property_name = k.replace(":", "%3A")
-
-                filename = get_filename(node_id, node_id_to_response)
-                property_value = EduSharing().buildUUID(filename + configuration["salt"])
-
-                edusharing_set_property(node_id, property_name, property_value, auth_response)
-
-            elif k == "cclom:general_keyword":
-                property_name = k
-
-                filename = get_filename(node_id, node_id_to_response)
-
-                # e.g.,
-                # 01.1 Curriculum Erwaermung.mp4 --> 01.1 --> 01 --> 1
-                # 0 Trailer.mp4 --> 0 --> 0 --> 0
-                chapter = filename.split(" ")[0].split(".")[-1]
-
-                property_value = copy.deepcopy(configuration["general_keywords"])
-                if chapter in configuration["keywords_per_chapter"]:
-                    property_value += configuration["keywords_per_chapter"][chapter]
-
-                edusharing_change_metadata(auth_response, node_id, property_name, property_value, True)
-
-            else:
-                property_name = k.replace(":", "%3A")
-                property_value = str(v)
-                edusharing_set_property(node_id, property_name, property_value, auth_response)
-            node_id_to_response[node_id][k] = property_value
+    get_api().set_property(node_id, 'cm:edu_forcemetadataset', ['true'])
+    for name in metadata:
+        get_api().set_property(node_id, name, metadata[name])
+    for property_name in metadata:
+        node[property_name] = metadata[property_name]
 
 
-def get_filename(node_id, node_id_to_response):
-    file = node_id_to_response[node_id]["name"]
-    # Remove file extension
-    filename = os.path.splitext(file)[0]
+def get_clean_filename(node):
+    filename = os.path.splitext(node["name"])[0]
 
     # XXX: Decide to remove chapter prefix?
     # filename = " ".join(filename.split(" ")[1:])
 
-    # Replace underscores with empty space
-    filename = filename.replace("_", " ")
+    replacements = {
+        '_': ' ',
+        'ae': 'ä',
+        'ue': 'ü',
+        'oe': 'ö'
+    }
 
-    # Replace non-umlaut form to umlaut
-    filename = filename.replace("ae", "ä").replace("ue", "ü").replace("ö", "oe")
-    property_value = filename
+    for old, repl in replacements.items():
+        filename = filename.replace(old, repl)
 
-    return property_value
-
-
-
-def add_permissions_to_nodes(auth_response, node_id_to_response):
-    """
-    Add the right permissions, which should be also reflected to ccm:ph_invited on individual elements.
-    """
-    configuration = get_configuration()
-
-    for node_id in tqdm(node_id_to_response.keys(), desc="Setting up permissions: "):
-        # Get current permissions of a node.
-        total_permissions, permissions_response_dict = edusharing_get_permission_groups(configuration["hostname"],
-                                                        configuration["parsed_edusharing_url"], auth_response, node_id)
-
-        # Add the remaining permissions that we require to the node.
-        edusharing_add_to_permission_groups(configuration["config"], configuration["hostname"], auth_response, node_id, total_permissions,
-                                            permissions_response_dict)
+    return filename
 
 
 def process_collections():
-    auth_response = authenticate()
-
     configuration = get_configuration()
     # Step 1
     print("Collecting elements.")
     # TODO: Identify directory structure
-    collections = identify_collections_in_directory(auth_response, configuration["directory_node_id"])
+    collections = identify_collections_in_directory(configuration["directory_node_id"])
     print("Collected " + str(len(collections.keys())) + " collections.")
 
     # Step 2
     for key, value in collections.items():
         parent = value["parent"]
         directory = value["directory"]
-        children = get_directory_elements(auth_response, directory["ref"]["id"])
+        children = get_directory_elements(directory["ref"]["id"])
 
         # parent_and_children = copy.deepcopy(children)
         parent_and_children = copy.copy(children)
         parent_and_children[parent["ref"]["id"]] = parent
 
-        add_attributes_to_nodes(auth_response, parent_and_children)
+        for node_id in tqdm(sorted(list(parent_and_children.keys()), key=lambda x: parent_and_children[x]["name"]),
+                            desc="Adding properties to elements"):
+            add_attributes_to_nodes(parent_and_children[node_id])
 
-        convert_to_collection(auth_response, parent, children)
+        convert_to_collection(parent, children)
 
-        add_permissions_to_nodes(auth_response, parent_and_children)
+        for node_id in tqdm(parent_and_children.keys(), desc="Setting up permissions: "):
+            get_api().set_permissions(node_id, [], True)
 
 
 def process_non_collection_files():
-    auth_response = authenticate()
-
     configuration = get_configuration()
 
     print("Collecting elements.")
-    node_id_to_response = get_directory_elements(auth_response, configuration["directory_node_id"])
-    print("Collected " + str(len(node_id_to_response.keys())) + " elements.")
+    nodes = get_directory_elements(configuration["directory_node_id"])
+    print("Collected " + str(len(nodes.keys())) + " elements.")
 
-    add_attributes_to_nodes(auth_response, node_id_to_response)
-    add_permissions_to_nodes(auth_response, node_id_to_response)
+    for node_id in tqdm(sorted(list(nodes.keys()), key=lambda x: nodes[x]["name"]),
+                        desc="Adding properties and permissions to elements"):
+        add_attributes_to_nodes(nodes[node_id])
+        get_api().set_permissions(node_id, [], True)
+
 
 if __name__ == '__main__':
     configuration = get_configuration()
