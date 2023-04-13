0ee86d2bc9f8f66bd1fc9f60ec39695fa1add88f
Andre Blome <andre-david.blome@capgemini.com>
2023-04-13 08:49:34 +0200

    edusharing api: improved error handling, debug option, new mark_for_deletion and get_permissions_groups, etc

--- schulcloud/edusharing.py
+++ schulcloud/edusharing.py
@@ -1,3 +1,4 @@
+import os
 import time
 from datetime import datetime
 from typing import Dict, List, Literal, Optional, IO, Any, Callable
@@ -54,6 +55,7 @@ class EdusharingAPI:
     def __init__(self, base_url: str, username: str = '', password: str = ''):
         if not base_url.endswith('/'):
             base_url += '/'
+        self.debug_enabled = os.environ.get('EDU_SHARING_API_DEBUG', 'false').lower() == 'true'
         self.base_url = base_url + 'rest'
         self.username = username
         self.password = password
@@ -70,54 +72,53 @@ class EdusharingAPI:
         i = -1
         while True:
             i += 1
-            #print(method, url, params, json_data)
+            if self.debug_enabled:
+                print(method, url, params, json_data)
             try:
                 response = self.session.request(method, url, params=params, headers=headers,
                                                 json=json_data, files=files, stream=stream, timeout=timeout)
-            except requests.exceptions.ReadTimeout:
+            except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as exc:
                 if i < retry:
                     time.sleep(i // 2)
                     continue
                 else:
-                    raise RequestTimeoutException()
+                    if isinstance(exc, requests.exceptions.Timeout):
+                        raise RequestTimeoutException()
+                    else:
+                        raise ConnectionErrorException()
             if 500 <= response.status_code < 600 and i < retry:
                 time.sleep(i // 2)
                 continue
+            if not (200 <= response.status_code < 300):
+                raise RequestErrorResponseException(response)
             return response
 
-    def get_all(self, url: str, params: Dict[str, str], callback: Callable, max_items: int, retry: int = 50, timeout: Optional[float] = None):
+    def get_all(self, method: Literal['GET', 'POST'], url: str, params: Dict[str, str], callback: Callable, max_items: int, json_data: Optional[dict] = None, retry: int = 50, timeout: Optional[float] = None):
         offset = 0
         items = []
         params['maxItems'] = str(max_items)
         while True:
             params['skipCount'] = str(offset)
-            response = self.make_request('GET', url, params, retry=retry, timeout=timeout)
-            if response.status_code == 200:
-                try:
-                    content = response.json()
-                    callback(items, content)
-                    offset += content['pagination']['count']
-                    total = content['pagination']['total']
-                except KeyError:
-                    raise RuntimeError(f'Could not parse response: {response.text}')
-                if offset >= total:
-                    break
-            else:
-                raise RequestErrorResponseException(response)
+            response = self.make_request(method, url, params, json_data=json_data, retry=retry, timeout=timeout)
+            try:
+                content = response.json()
+                callback(items, content)
+                offset += content['pagination']['count']
+                total = content['pagination']['total']
+            except KeyError:
+                raise RuntimeError(f'Could not parse response: {response.text}')
+            if offset >= total:
+                break
         return items
 
     def get_application_properties(self, xml_file_name: str):
         url = f'/admin/v1/applications/{xml_file_name}'
         response = self.make_request('GET', url)
-        if not 200 <= response.status_code < 300:
-            raise RequestErrorResponseException(response)
         return response.json()
 
     def set_application_properties(self, xml_file_name: str, values: Dict[str, str]):
         url = f'/admin/v1/applications/{xml_file_name}'
         response = self.make_request('PUT', url, json_data=values)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
 
     def upload_content(self, node_id: str, filename: str, file: IO[bytes], mimetype: str = ''):
         url = f'/node/v1/nodes/-home-/{node_id}/content'
@@ -126,8 +127,6 @@ class EdusharingAPI:
             'file': (filename, file, mimetype, {'Expires': '0'})
         }
         response = self.make_request('POST', url, params=params, files=files, stream=True)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
 
     def get_user(self, name: str = None):
         if name is None:
@@ -136,14 +135,12 @@ class EdusharingAPI:
         response = self.make_request('GET', url)
         if response.status_code == 404:
             raise NotFoundException(name)
-        if not 200 <= response.status_code < 300:
-            raise RequestErrorResponseException(response)
         return response.json()
 
     def get_users(self):
         url = f'/iam/v1/people/-home-'
         params = {'pattern': '*'}
-        users = self.get_all(url, params, lambda items, content: items.extend(content['users']), 20)
+        users = self.get_all('GET', url, params, lambda items, content: items.extend(content['users']), 20)
         return users
 
     def create_user(self, username: str, password: str, type: Literal['function', 'system'], quota: int = 1024 ** 2):
@@ -162,19 +159,22 @@ class EdusharingAPI:
             'about': None
         }
         response = self.make_request('POST', url, params=params, json_data=body)
-        if not 200 <= response.status_code < 300:
-            raise RequestErrorResponseException(response)
+
+    def mark_user_for_deletion(self, name: str):
+        url = f'/iam/v1/people/-home-/{name}/status/todelete?notify=false'
+        params = {
+            'notify': 'false'
+        }
+        response = self.make_request('PUT', url, params=params)
 
     def delete_user(self, name: str):
         url = f'/iam/v1/people/-home-/{name}'
         params = {'force': 'true'}
         response = self.make_request('DELETE', url, params=params)
-        if not 200 <= response.status_code < 300:
-            raise RequestErrorResponseException(response)
 
     def user_get_groups(self, username: str):
         url = f'/iam/v1/people/-home-/{username}/memberships'
-        groups = self.get_all(url, {}, lambda items, content: items.extend(content['groups']), 50)
+        groups = self.get_all('GET', url, {}, lambda items, content: items.extend(content['groups']), 50)
         return groups
 
     def create_group(self, group_name: str):
@@ -185,22 +185,18 @@ class EdusharingAPI:
             'scopeType': None
         }
         response = self.make_request('POST', url, json_data=body)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
 
     def get_groups(self):
         url = f'/iam/v1/groups/-home-/'
         params = {'pattern': '*'}
-        groups = self.get_all(url, params, lambda items, content: items.extend(content['groups']), 256)
+        groups = self.get_all('GET', url, params, lambda items, content: items.extend(content['groups']), 256)
         return groups
 
     def group_add_user(self, group_name: str, user_name: str):
         url = f'/iam/v1/groups/-home-/GROUP_{group_name}/members/{user_name}'
-        response = self.make_request('PUT', url)
-        if not 200 <= response.status_code < 300:
-            raise RequestErrorResponseException(response)
+        self.make_request('PUT', url)
 
-    def get_children(self, node_id: str, all_properties: bool = False, type: Literal['all', 'folders', 'files'] = 'all') -> List[Node]:
+    def get_children(self, node_id: str, all_properties: bool = False, type: Literal['all', 'folders', 'files'] = 'all', start: int = 0, count: int = 0) -> List[Node]:
         url = f'/node/v1/nodes/-home-/{node_id}/children'
         params = {}
         if all_properties:
@@ -209,18 +205,23 @@ class EdusharingAPI:
             if type not in ('files', 'folders'):
                 raise ValueError(f'Unknown node type: {type}')
             params['filter'] = type
-        children = self.get_all(url, params, lambda items, content: items.extend([Node(node) for node in content['nodes']]), 200)
-        return children
+        if count == 0:
+            return self.get_all('GET', url, params, lambda items, content: items.extend([Node(node) for node in content['nodes']]), 200)
+        else:
+            params['skipCount'] = start
+            params['maxItems'] = count
+            response = self.make_request('GET', url, params=params)
+            return [Node(node) for node in response.json()['nodes']]
 
-    def find_node_by_name(self, parent_id: str, child_name: str) -> Node:
-        nodes = self.get_children(parent_id)
+    def find_node_by_name(self, parent_id: str, child_name: str, type: Literal['all', 'files', 'folders'] = 'all') -> Node:
+        nodes = self.get_children(parent_id, type=type)
         for node in nodes:
             if node.name == child_name:
                 return node
         raise NotFoundException(child_name)
 
     def find_node_by_replication_source_id(self, replication_source_id: str, skip_exception: Optional[bool] = False) -> Node:
-        nodes = self.search_custom('ccm:replicationsourceid', replication_source_id, 2, 'FILES')
+        nodes = self.search_custom('ccm:replicationsourceid', replication_source_id, content_type='FILES')
         if len(nodes) == 1:
             return nodes[0]
         elif len(nodes) > 1:
@@ -245,8 +246,6 @@ class EdusharingAPI:
         if properties:
             data.update(properties)
         response = self.make_request('POST', url, params=params, json_data=data)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
         return Node(response.json()['node'])
 
     def sync_node(self, group: str, properties: Dict, match: List[str], type: str = 'ccm:io',
@@ -260,91 +259,96 @@ class EdusharingAPI:
         if group_by is not None:
             params['groupBy'] = group_by
         response = self.make_request('PUT', url, params=params, json_data=properties)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
         return Node(response.json()['node'])
 
     def delete_node(self, node_id: str):
         url = f'/node/v1/nodes/-home-/{node_id}'
-        response = self.make_request('DELETE', url)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
+        self.make_request('DELETE', url)
 
     def get_sync_obj_folder(self):
         return self.get_or_create_node('-userhome-', 'SYNC_OBJ', type='folder')
 
     def file_exists(self, parent_id: str, name: str):
         # TODO: should only search within specific parent node, not global search
-        return len(self.search_custom('cm:name', name, 2, 'FILES')) > 0
+        return len(self.search_custom('cm:name', name, content_type='FILES')) > 0
+
+    def search_ngsearch(self, criteria: list[dict[str, str]], content_type: Optional[Literal['FOLDERS', 'FILES']] = None, all_properties: bool = False):
+        url = f'/search/v1/queries/-home-/mds_oeh/ngsearch/'
+        params = {
+             'sortProperties': 'score'
+        }
+        if content_type:
+            params['contentType'] = content_type
+        if all_properties:
+            params['propertyFilter'] = '-all-'
+        body = {
+            'criteria': criteria,
+            'facets': ['cclom:general_keyword']
+        }
+        nodes = self.get_all('POST', url, params, lambda items, content: items.extend([Node(node) for node in content['nodes']]), 100, json_data=body)
+        return nodes
 
     def search_schulcloud(self, query: str):
         url = f'/search/v1/queries/-home-/mds_oeh/ngsearch/'
         params = {
             'contentType': 'FILES',
-            'skipCount': '0',
-            'maxItems': '20',
             'sortProperties': 'score',
             'sortAscending': 'false',
             'propertyFilter': '-all-',
         }
         body = {
             "criteria": [
-                {
-                    "property": "ccm:ph_invited",
-                    "values": [
-                        "GROUP_county-12051", "GROUP_public", "GROUP_LowerSaxony-public",
-                                      "GROUP_Brandenburg-public", "GROUP_Thuringia-public"
-                    ]
-                },
-                {
-                    "property": "ccm:hpi_searchable", "values": ["1"]},
-                {
-                    "property": "ngsearchword", "values": [query]
-                }
+                {"property": "ccm:hpi_searchable", "values": ["1"]},
+                {"property": "ngsearchword", "values": [query]}
             ],
             "facets": ["cclom:general_keyword"]
         }
-        response = self.make_request('POST', url, params=params, json_data=body)
-        return response
 
-    def search_custom(self, property: str, value: str, max_items: int, content_type: Literal['FOLDERS', 'FILES']):
-        url = f'/search/v1/custom/-home-?contentType={content_type}&combineMode=AND&property={property}&value={value}' \
-              f'&maxItems={max_items}&skipCount=0'
-        response = self.make_request('GET', url)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
-        json_obj = response.json()
-        return [Node(node) for node in json_obj['nodes']]
+        nodes = self.get_all('POST', url, params, lambda items, content: items.extend([Node(node) for node in content['nodes']]), 100, json_data=body)
+        return nodes
+
+    def search_custom(self, property: str, value: str, content_type: Optional[Literal['FOLDERS', 'FILES']] = None, all_properties: bool = False):
+        url = f'/search/v1/custom/-home-'
+        params = {
+            'combineMode': 'AND',
+            'property': property,
+            'value': value,
+        }
+        if content_type:
+            params['contentType'] = content_type
+        if all_properties:
+            params['propertyFilter'] = '-all-'
+        nodes = self.get_all('GET', url, params, lambda items, content: items.extend([Node(node) for node in content['nodes']]), 100)
+        return nodes
 
     def get_permissions(self, node_id: str):
         url = f'/node/v1/nodes/-home-/{node_id}/permissions'
         response = self.make_request('GET', url)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
-        try:
-            return response.json()['permissions']
-        except KeyError:
-            raise RequestErrorResponseException(response)
+        return response.json()['permissions']
+
+    def get_permissions_groups(self, node_id: str) -> (list[str], bool):
+        permissions = self.get_permissions(node_id)
+        groups = []
+        for permission in permissions['localPermissions']['permissions']:
+            auth_name: str = permission['authority']['authorityName']
+            if not auth_name.startswith('GROUP_'):
+                continue
+            groups.append(auth_name.replace('GROUP_', '', 1))
+        return groups, permissions['localPermissions']['inherited']
 
     def set_permissions(self, node_id: str, groups: List[str], inheritance: bool) -> None:
         url = f'/node/v1/nodes/-home-/{node_id}/permissions?sendMail=false&sendCopy=false'
         response = self.make_request('POST', url, json_data=self._craft_permission_body(groups, inheritance), timeout=8)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
 
     def get_metadata(self, node_id: str):
         url = f'/node/v1/nodes/-home-/{node_id}/metadata'
         response = self.make_request('GET', url)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
         return response.json()
 
     def change_metadata(self, node_id: str, properties: Dict[str, List[str]]):
         url = f'/node/v1/nodes/-home-/{node_id}/metadata'
         params = {'versionComment': 'METADATA_UPDATE'}
-        response = self.make_request('POST', url, params=params, json_data=properties)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response)
+        self.make_request('POST', url, params=params, json_data=properties)
 
     def get_node_timestamp(self, node):
         meta = self.get_metadata(node.id)
@@ -366,9 +370,7 @@ class EdusharingAPI:
         params = {'property': property}
         if value is not None:
             params['value'] = value
-        response = self.make_request('POST', url, params=params)
-        if not response.status_code == 200:
-            raise RequestErrorResponseException(response, node_id)
+        self.make_request('POST', url, params=params)
 
     def set_collection_children(self, node_id: str, children_uuids: List[str]):
         """
@@ -411,6 +413,10 @@ class RequestErrorResponseException(RequestFailedException):
 
 
 class RequestTimeoutException(RequestFailedException):
+    pass
+
+
+class ConnectionErrorException(RequestFailedException):
     pass
 
 
