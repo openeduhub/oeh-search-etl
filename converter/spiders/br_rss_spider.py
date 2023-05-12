4b86532fe265710af9e7684aeb82bf5e1875864b
criamos <981166+Criamos@users.noreply.github.com>
2023-05-12 15:17:25 +0200

    version bump to v0.1.1 to force metadata refresh
    - (since no version bump was made in February 2023)
    
    Signed-off-by: criamos <981166+Criamos@users.noreply.github.com>

--- converter/spiders/br_rss_spider.py
+++ converter/spiders/br_rss_spider.py
@@ -6,7 +6,7 @@ class BRRSSSpider(RSSListBase):
     name = "br_rss_spider"
     friendlyName = "Bayerischer Rundfunk"
     url = "https://www.br.de/"
-    version = "0.1.0"
+    version = "0.1.1"
 
     def __init__(self, **kwargs):
         RSSListBase.__init__(self, "../csv/br_rss.csv", **kwargs)
