d78c5195b12e713e173bbfa20f1a3da666e9b066
criamos <981166+Criamos@users.noreply.github.com>
2023-06-15 17:38:03 +0200

    change: z_api venv setup
    - docs: explain what's happening inside the generate-z-api.sh bash script
    -- mention additional (manual) steps that might be necessary to get it working on different systems
    - change: rename the virtual environment to follow venv naming schemes
    - gitignore: ignore future virtual environments that start with "venv_..."
    - requirements.txt: add beautifulSoup4 dependency from generic_spider.py (was missing)
    
    Signed-off-by: criamos <981166+Criamos@users.noreply.github.com>

--- generate-z-api.sh
+++ generate-z-api.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 # Creating a venv within the "venv_z_api"-directory (for easier distinction from our "normal" venv)
 python3 -m venv venv_z_api
-# activating the venv:
-source ./venv_z_api/bin/activate
-# confirm that you are within your venv, then install the requirements.txt:
-pip3 install -r requirements.txt
-
-# deleting the "tmp"-directory (if there was one from a previous execution of this script)
+# deleting the "tmp"-directory (if there is one)
 rm -rf tmp
 # create a temporary "tmp"-directory which will be used by the OpenAPI Generator (API Client)
 mkdir tmp
-# switch to directory (or: exit here if changing directories fails)
+# switch to it (or exit here if changing directories fails)
 cd tmp || exit
+#
+# follow these steps manually:
+# activating the venv:
+# source ./venv_z_api/bin/activate
+# confirm that you are within your venv, then install the requirements.txt:
+# pip3 install -r requirements.txt
 #
 # make sure that you have the OpenAPI Generator installed (and the PATH exposed within your ".profile")
 # before running this command, otherwise the following steps WILL NOT WORK:
-echo "Java version ---------------------- : " $(java -version)
-wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.2.0/openapi-generator-cli-7.2.0.jar -O openapi-generator-cli.jar
-java -jar openapi-generator-cli.jar generate -g python -o z_api --skip-validate-spec -i https://ai-prompt-service.staging.openeduhub.net/v3/api-docs --package-name z_api
-cd z_api
-python3 setup.py install --user
+openapi-generator generate -g python -o z_api -i https://ai-prompt-service.staging.openeduhub.net/v3/api-docs --package-name z_api
+cd z_api && python3 setup.py install --user
 # change directories to project root:
 cd ../../
 # move the only relevant sub-directory to the "z_api"-directory at project root:
