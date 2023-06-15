580bf606b8388d40e31ffbb9fb30c417b08ff7df
Torsten Simon <simon@edu-sharing.net>
2023-06-15 13:03:16 +0200

    fix z_api

--- generate-z-api.sh
+++ generate-z-api.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 python -m venv chatgpt-api
-rm -rf app/openapi_client
+rm -rf tmp
+mkdir tmp && cd tmp
 openapi-generator generate -g python -o z_api -i https://ai-prompt-service.staging.openeduhub.net/v3/api-docs --package-name z_api
 cd z_api && python setup.py install --user
-cd ..
-cp -r z_api/z_api/ converter/z_api
-rm -rf z_api
+cd ../../
+cp -r tmp/z_api/z_api/ z_api
+rm -rf tmp
