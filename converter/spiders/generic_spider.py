74dc769f5357f34f2c56153dda428e4a887aa5a6
criamos <981166+Criamos@users.noreply.github.com>
2023-06-16 17:26:39 +0200

    generic_spider (work-in-progress!)
    - add: URL-candidates for validation
    - feat: respect Robot Meta Tags
    - feat: trafilatura text extraction (work-in-progress!)
    - add: debug variables for comparing html2text and trafilatura results
    - code cleanup
    - add ToDos
    
    requirements.txt:
    - add: trafilatura to requirements.txt
    - update: requests package

--- converter/spiders/generic_spider.py
+++ converter/spiders/generic_spider.py
@@ -1,10 +1,13 @@
 import asyncio
 import logging
 import re
+from typing import Any
 
+import scrapy.http
+import trafilatura
 from bs4 import BeautifulSoup
 from scrapy import Request
-from scrapy.spiders import CrawlSpider, Rule, Spider
+from scrapy.spiders import Rule, Spider
 
 import z_api
 from valuespace_converter.app.valuespaces import Valuespaces
@@ -19,8 +22,44 @@ from ..web_tools import WebEngine, WebTools
 class GenericSpider(Spider, LrmiBase):
     name = "generic_spider"
     friendlyName = "generic_spider"  # name as shown in the search ui
-    start_urls = ["https://www.planet-schule.de/schwerpunkt/total-phaenomenal-energie/sonnenenergie-film-100.html"]
-    ROBOTSTXT_OBEY = False
+    version = "0.1.2"
+    start_urls = [
+        # "https://www.planet-schule.de/schwerpunkt/total-phaenomenal-energie/sonnenenergie-film-100.html",  # the original Hackathon example URL
+        # "https://de.serlo.org/informatik/158541/definitionen-von-%E2%80%9Ebig-data%E2%80%9C",
+        # "https://de.serlo.org/mathe/62630/aufgaben-zum-volumen-eines-quaders",
+        # "https://www.planet-schule.de/schwerpunkt/dichter-dran/fontane-film-100.html",
+        # "https://www.planet-schule.de/thema/fridays-for-future-was-steckt-hinter-den-klima-streiks-film-100.html",
+        # "https://www.dilertube.de/englisch/oer-video/algeria-in-a-nutshell.html",
+        # "https://www.dilertube.de/alltagskultur-ernaehrung-soziales-aes/oer-video/erklaerfilm-medikamente-richtig-entsorgen.html",
+        # "https://www.umwelt-im-unterricht.de/unterrichtsvorschlaege/der-mensch-hauptursache-fuer-den-rueckgang-der-biologischen-vielfalt",
+        # "https://www.umwelt-im-unterricht.de/hintergrund/die-endlagerung-hochradioaktiver-abfaelle",
+        # "https://editor.mnweg.org/mnw/sammlung/das-menschliche-skelett-m-78",
+        # "https://editor.mnweg.org/mnw/sammlung/bruchrechnen-m-10",
+        # "https://www.bpb.de/themen/migration-integration/laenderprofile/277555/afghanistan-geschichte-politik-gesellschaft/",
+        # "https://www.bpb.de/themen/kolonialismus-imperialismus/postkolonialismus-und-globalgeschichte/236617/kolonialismus-und-postkolonialismus-schluesselbegriffe-der-aktuellen-debatte/",
+        # "https://www.geschichtsquellen.de/werk/3402",
+        # "https://www.geschichtsquellen.de/werk/4799",
+        # "https://www.weltderphysik.de/gebiet/teilchen/quanteneffekte/",
+        # "https://www.weltderphysik.de/mediathek/podcast/geothermie/",
+        # "https://histomania.com/app/Saralee_Thungthongkam_W468573",
+        # "https://histomania.com/app/Anna_Maria_von_Anhalt_W527486",
+        # "https://medienportal.siemens-stiftung.org/de/design-thinking#examples",
+        # "https://medienportal.siemens-stiftung.org/portal/main.php?todo=metadata_search&searcharea=portal&crits%5Bmedialang%5D=de&options%5BsearchAndOr%5D=1&crits%5Bfree%5D=Bl%C3%A4tterwald&crits%5Bmedialang%5D=de&options%5Bsort%5D=mediatype%2Ctimepublish+DESC",
+        # "https://medienportal.siemens-stiftung.org/de/service-learning-in-den-mint-faechern-109145",
+        # "https://apps.zum.de/apps/25726",
+        # "https://apps.zum.de/apps/25532",
+        # "https://www.inf-schule.de/datenbanksysteme/terra/relationaledb/konzept_tabelle",
+        # "https://www.inf-schule.de/rechner/bonsai/murmelrechner/universellermurmelrechner",
+        # "https://weltverbessern-lernen.de/materialupload/dirk-ehnts-modern-monetary-theory-erklaert/",
+        # "https://weltverbessern-lernen.de/materialupload/mehr-oder-weniger-wachstumskritik-von-links/",
+        # "https://weltverbessern-lernen.de/materialupload/5707/",
+        # "https://lernen.schule.de/das-notensystem/",
+        # "https://lernen.schule.de/das-menschliche-skelett/",
+        # "https://www.schulebewegt.ch/de/aufgaben/Streckung",
+        # "https://www.schulebewegt.ch/de/aufgaben/Hampelmann",
+        # Debugging Examples - 'noindex' robot meta tag:
+        "https://de.serlo.org/geographie/169056/der-aufbau-der-atmosph%C3%A4re?contentOnly",
+    ]
     rules = [
         Rule(
             callback='parse'
@@ -30,6 +69,7 @@ class GenericSpider(Spider, LrmiBase):
         'WEB_TOOLS': WebEngine.Playwright,
         "ROBOTSTXT_OBEY": False
     }
+
     clean_tags = ['nav', 'header', 'footer']
     prompts = {
         'description': 'Fasse folgenden Text in 3 Sätzen zusammen: %(text)s',
@@ -39,7 +79,6 @@ class GenericSpider(Spider, LrmiBase):
         'new_lrt': 'Welche Materialart im schulischen Kontext ist folgender Text: %(text)s',
         'intendedEndUserRole': 'Für welche Zielgruppen eignet sich folgender Text: %(text)s',
     }
-    version = "0.1.2"
     valuespaces: Valuespaces
     z_api_text: z_api.AITextPromptsApi
 
@@ -49,20 +88,25 @@ class GenericSpider(Spider, LrmiBase):
         LrmiBase.__init__(self, **kwargs)
 
         self.valuespaces = Valuespaces()
+        # ToDo: optional Feature: "generic_spider" (AI=enabled) <-> "generic_minimal_spider" (AI=disabled)?
         z_api_config = z_api.Configuration.get_default_copy()
         z_api_config.api_key = {'ai-prompt-token': env.get("Z_API_KEY", False)}
         z_api_client = z_api.ApiClient(configuration=z_api_config)
         self.z_api_text = z_api.AITextPromptsApi(z_api_client)
 
     def start_requests(self):
+        # ToDo: Feature - control list of start_urls via '.env'-setting
         for url in self.start_urls:
             yield Request(url, callback=self.parse)
 
-    def parse(self, response):
+    def parse(self, response: scrapy.http.Response, **kwargs) -> Any | None:
         if not self.hasChanged(response):
             return
         data = asyncio.run(WebTools.fetchDataPlaywright(response.url))
         response = response.copy()
+        # ToDo: validate "trafilatura"-fulltext-extraction from playwright (compared to the html2text approach)
+        text_trafilatura = trafilatura.extract(data["content"])
+
         parsed_html = BeautifulSoup(data['content'], features='lxml')
         for tag in self.clean_tags:
             tags = parsed_html.find_all(tag) if parsed_html.find_all(tag) else []
@@ -70,11 +114,39 @@ class GenericSpider(Spider, LrmiBase):
                 t.clear()
         crawler_ignore = parsed_html.find_all(name=None, attrs={'data-crawler': 'ignore'})
         for t in crawler_ignore:
-            t.clear();
+            t.clear()
         html = parsed_html.prettify()
         data['parsed_html'] = parsed_html
         data['text'] = WebTools.html2Text(html)
         response.meta['data'] = data
+
+        text_html2text = data["text"]
+
+        selector_playwright = scrapy.Selector(text=data["content"])
+        # ToDo: optional-feature - respect Robot-Meta-Tags in the DOM Header (-> abort crawl on prohibited websites)
+        robot_meta_tags: list[str] = selector_playwright.xpath("//meta[@name='robots']/@content").getall()
+        # ToDo: use Playwright response instead of Scrapy's Response (-> Selector)
+        # ToDo: optional - parametrize setting if Robots Meta Tags should be respected?
+        if robot_meta_tags:
+            # There are 3 Robot Meta Tags that we need to respect:
+            # - "noindex"       (= don't index the current URL)
+            # - "nofollow"      (= don't follow any links on this site)
+            # - "none"          (= shortcut for combined value "noindex, nofollow")
+            if "noindex" in robot_meta_tags:
+                logging.info(f"Robot Meta Tag 'noindex' identified. Aborting further parsing of item: "
+                             f"{response.url} .")
+                return None
+            if "nofollow" in robot_meta_tags:
+                # ToDo: don't follow any links, but parse the current response
+                #  -> yield response with 'nofollow'-setting in cb_kwargs
+                logging.info(f"Robot Meta Tag 'nofollow' identified. Parsing item {response.url} , but WILL NOT "
+                             f"follow any links found within.")
+                pass
+            if "none" in robot_meta_tags:
+                logging.info(f"Robot Meta Tag 'none' identified (= 'noindex, nofollow'). "
+                             f"Aborting further parsing of item: {response.url} itself and any links within it.")
+                return None
+
         return LrmiBase.parse(self, response)
 
     # return a (stable) id of the source
@@ -85,6 +157,7 @@ class GenericSpider(Spider, LrmiBase):
     # if there is no hash available, may use the current time as "always changing" info
     # Please include your crawler version as well
     def getHash(self, response):
+        # ToDo: append the current date to the hash?
         return self.version
 
     def getBase(self, response):
@@ -100,6 +173,10 @@ class GenericSpider(Spider, LrmiBase):
         general = LrmiBase.getLOMGeneral(self, response)
         general.add_value("title", response.meta['data']['title'])
         # TODO: Map language based on z-api
+        html_language = response.xpath('//html/@lang').get()
+        # html language and locale properties haven proven to be pretty inconsistent, but they might be useful for
+        # fallback values
+        meta_locale = response.xpath('//meta[@property="og:locale"]/@content').get()
         general.add_value(
             "language", "de"
         )
@@ -135,6 +212,8 @@ class GenericSpider(Spider, LrmiBase):
         prompt = self.prompts[field] % {
             'text': response.meta['data']['text'][:4000]
         }
+        # ToDo: figure out a reasonable cutoff-length
+        # (prompts which are too long get thrown out by the AI services)
         result = self.z_api_text.prompt(body=prompt)
         logging.info(result)
         result = result.responses[0].strip()
