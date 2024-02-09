0a0bb714a639510fe0ff75191b4c3984b8714806
Andreas Schnäpp <981166+Criamos@users.noreply.github.com>
2024-02-09 15:55:58 +0100

    Fix warnings in 'getLRMI()'-method
    - fix: fixed hidden ValueError when trying to use 'LrmiBase.getLRMI()' on Response objects of type 'json'
    -- Scrapy's older version used the 'parsel'-package <1.8 which (somehow) was less strict when erroneously trying to navigate a 'json'-object with XPath-selectors
    -- as of Scrapy v2.9+ trying to use 'response.xpath()' on a response object other than of type "html" will throw an Error which needs to be handled
    -- a bare except previously hid this problem from us, causing digitallearninglab_spider.py to throw warnings which obfuscated the real problem
    -- see: https://github.com/scrapy/scrapy/issues/5923
    - fix: fixed weak warnings (ambiguous variable names)
    - fix: fixed weak warning regarding comparison with None (PEP8:E711)
    - optimized imports
    
    Signed-off-by: Andreas Schnäpp <981166+Criamos@users.noreply.github.com>

--- converter/spiders/base_classes/lrmi_base.py
+++ converter/spiders/base_classes/lrmi_base.py
@@ -1,15 +1,13 @@
-import re
-
-from .lom_base import LomBase
-from .json_base import JSONBase
-import json
-import time
 import html
+import json
 import logging
+import time
+
+from .json_base import JSONBase
+from .lom_base import LomBase
 
 # base spider mapping data via LRMI inside the html pages
 # Please override the lrmi_path if necessary and add your sitemap_urls
-from ...constants import Constants
 from ...items import LicenseItemLoader
 
 
@@ -21,22 +19,36 @@ class LrmiBase(LomBase, JSONBase):
         LomBase.__init__(self, **kwargs)
 
     def getLRMI(self, *params, response):
-        try:
-            lrmi = list(
-                map(
-                    lambda x: json.loads(x.replace("\r", "").replace("\n", " ")),
-                    response.xpath(self.lrmi_path).getall(),
+        lrmi: list[dict] = list()
+        if response:
+            if response.selector.type == "html":
+                # this check is necessary because querying a selector of type 'json' would result in an ValueError and
+                # throw warnings
+                lrmi_raw: list[str] = response.xpath(self.lrmi_path).getall()
+                for lrmi_item in lrmi_raw:
+                    lrmi_item = lrmi_item.replace("\r", "")
+                    lrmi_item = lrmi_item.replace("\n", " ")
+                    lrmi_item = lrmi_item.replace("\t", " ")
+                    # after these steps there might still be multiple whitespaces within a json-ld object
+                    lrmi_item = " ".join(lrmi_item.split())
+                    if lrmi_item:
+                        lrmi_object: dict = json.loads(lrmi_item)
+                        lrmi.append(lrmi_object)
+                    else:
+                        logging.warning(
+                            f"Failed parsing LRMI at {response.url} : After trying to sanitize the JSON string object, "
+                            f"the final string was invalid."
+                        )
+            else:
+                logging.warning(
+                    f"Failed parsing lrmi at {response.url} , please check source (if there was an JSON-LD available)"
                 )
-            )
-        except:
-            logging.warning(
-                "failed parsing lrmi at " + response.url + ", please check source"
-            )
-            return None
-        for l in lrmi:
-            value = JSONBase.get(self, *params, json=l)
-            if value != None:
-                return html.unescape(value)
+                return None
+        if lrmi and isinstance(lrmi, list):
+            for lrmi_dict in lrmi:
+                value = JSONBase.get(self, *params, json=lrmi_dict)
+                if value is not None:
+                    return html.unescape(value)
         return None
 
     async def parse(self, response):
@@ -46,7 +58,7 @@ class LrmiBase(LomBase, JSONBase):
         return self.getLRMI("identifier", "url", "name", response=response)
 
     def getHash(self, response):
-        if self.get("version") != None:
+        if self.get("version") is not None:
             return self.getLRMI("version", response=response)
         return time.time()
 
@@ -65,16 +77,12 @@ class LrmiBase(LomBase, JSONBase):
         general.add_value("title", self.getLRMI("name", "headline", response=response))
         general.add_value("keyword", self.getLRMI("keywords", response=response))
         general.add_value("language", self.getLRMI("inLanguage", response=response))
-        general.add_value(
-            "description", self.getLRMI("description", "about", response=response)
-        )
+        general.add_value("description", self.getLRMI("description", "about", response=response))
         return general
 
     def getLOMEducational(self, response):
         educational = LomBase.getLOMEducational(self, response)
-        educational.add_value(
-            "typicalLearningTime", self.getLRMI("timeRequired", response=response)
-        )
+        educational.add_value("typicalLearningTime", self.getLRMI("timeRequired", response=response))
         return educational
 
     def getValuespaces(self, response):
@@ -97,11 +105,15 @@ class LrmiBase(LomBase, JSONBase):
                 # the "license" field holds a valid URL -> use it directly as is
                 license_loader.add_value("url", license_raw)
             else:
-                logging.warning(f"Could not map the received 'license'-value {license_raw} within LrmiBase. "
-                                f"Please check Constants.py and LrmiBase for missing mappings/values.")
+                logging.warning(
+                    f"Could not map the received 'license'-value {license_raw} within LrmiBase. "
+                    f"Please check Constants.py and LrmiBase for missing mappings/values."
+                )
         else:
-            logging.warning("LrmiBase: The 'license'-field returned within the JSON_LD doesn't seem to be a URL.\n"
-                            "Please check if additional license-mapping is necessary within the spider itself.")
+            logging.warning(
+                "LrmiBase: The 'license'-field returned within the JSON_LD doesn't seem to be a URL.\n"
+                "Please check if additional license-mapping is necessary within the spider itself."
+            )
         return license_loader
 
     def getLOMTechnical(self, response):
