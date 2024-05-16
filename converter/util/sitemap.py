97e28bd014e96fe2adc599fae3f6d59afd783e1f
Andres Granados <afgranadosc@unal.edu.co>
2024-05-16 14:22:30 +0200

    Add functions for finding sitemap for any crawler. Removed from web service

--- converter/util/sitemap.py
+++ converter/util/sitemap.py
@@ -3,6 +3,11 @@ from typing import Optional
 
 import scrapy
 
+import os
+import subprocess
+from trafilatura import sitemaps, feeds
+from ..web_tools import ignored_file_extensions
+
 namespaces = {
     'sm': 'http://www.sitemaps.org/schemas/sitemap/0.9',
     'image': 'http://www.google.com/schemas/sitemap-image/1.1',
@@ -53,4 +58,52 @@ class SitemapEntry:
             payload['images'] = images
         return SitemapEntry(**payload)
 
+def find_generate_sitemap(url, max_entries = 5):
+    sitemap_list = sitemaps.sitemap_search(url, target_lang='de')
+    feeds_list = feeds.find_feed_urls(url)
 
+    sitemap_set = set(sitemap_list)
+    feeds_set = set(feeds_list)
+    all_urls_set = sitemap_set.union(feeds_set)
+    all_urls = list(all_urls_set)
+    if len(all_urls) == 0:
+        all_urls = generate_sitemap(url, max_entries)
+
+    ignore_urls = []
+    for url in all_urls:
+        for file_extension in ignored_file_extensions:
+            if url.endswith(file_extension):
+                ignore_urls.append(url)
+                break
+    ignore_urls_set = set(ignore_urls)
+    all_urls_set = set(all_urls)
+    valid_urls = list(all_urls_set.difference(ignore_urls_set))
+    return valid_urls[:max_entries]
+
+def generate_sitemap(url, max_entries):
+    DEVNULL = open(os.devnull, 'wb')
+    try:
+        bytes_result = subprocess.check_output([f'npx',
+                                                'sitemap-generator-cli',
+                                                '--max-depth',
+                                                '2',
+                                                '--max-entries',
+                                                str(max_entries),
+                                                '--verbose',
+                                                url,
+                                                '>>',
+                                                'sitemap.txt'],
+                                               stderr=DEVNULL)
+        print(bytes_result)
+        str_result = bytes_result.decode('utf-8')
+        str_result = str_result.replace('[ ADD ] ', '')
+        str_result = str_result.replace('\n', ',')
+        if str_result.index('Added ') == 0:
+            return []
+        else:
+            str_result = str_result[:str_result.index(',Added ')]
+        urls = str_result.split(",")
+        return urls
+    except (subprocess.CalledProcessError, Exception) as e:
+        print("Error generating sitemap:", e)
+        return []
