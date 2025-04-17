a8a0d5d9506f438d0529ecd3d30a5634cd200b4a
criamos <981166+Criamos@users.noreply.github.com>
2025-04-17 18:52:54 +0200

    fix: warnings and add missing TypeHints

--- valuespace_converter/app/valuespaces.py
+++ valuespace_converter/app/valuespaces.py
@@ -1,10 +1,9 @@
-import string
-
 import requests
+from requests import Response
 
 
 class Valuespaces:
-    idsVocabs = [
+    idsVocabs: list[str] = [
         "conditionsOfAccess",
         "discipline",
         "educationalContext",
@@ -18,8 +17,14 @@ class Valuespaces:
         "sourceContentType",
         "toolCategory",
     ]
-    idsW3ID = ["containsAdvertisement", "price", "accessibilitySummary", "dataProtectionConformity", "fskRating"]
-    data = {}
+    idsW3ID: list[str] = [
+        "containsAdvertisement",
+        "price",
+        "accessibilitySummary",
+        "dataProtectionConformity",
+        "fskRating",
+    ]
+    data: dict = {}
 
     def __init__(self):
         vocab_list: list[dict] = []
@@ -33,20 +38,20 @@ class Valuespaces:
             vocab_list.append({"key": v, "url": "http://w3id.org/openeduhub/vocabs/" + v + "/index.json"})
         for vocab_name in vocab_list:
             # try:
-            r = requests.get(vocab_name["url"])
+            r: Response = requests.get(vocab_name["url"])
             self.data[vocab_name["key"]] = self.flatten(r.json()["hasTopConcept"])
             # except:
             #    self.valuespaces[v] = {}
 
-    def flatten(self, tree: []):
-        result = tree
+    def flatten(self, tree: list[dict]) -> list[dict]:
+        result: list[dict] = tree
         for leaf in tree:
             if "narrower" in leaf:
                 result.extend(self.flatten(leaf["narrower"]))
         return result
 
     @staticmethod
-    def findKey(valuespaceId: string, id: string, valuespace=None):
+    def findKey(valuespaceId: str, id: str, valuespace=None):
         if not valuespace:
             valuespace = Valuespaces.data[valuespaceId]
         for key in valuespace:
@@ -59,6 +64,7 @@ class Valuespaces:
         return None
 
     def initTree(self, tree):
+        # ToDo: this method appears to be dead code that hasn't been used anywhere.
         for t in tree:
             names = self.getNames(t)
             # t['words'] = list(map(lambda x: nlp(x)[0], names))
@@ -67,6 +73,7 @@ class Valuespaces:
                 self.initTree(t["narrower"])
 
     def getNames(self, key):
+        # ToDo: this method appears to be dead code that hasn't been used anywhere.
         names = []
         if "prefLabel" in key:
             if "de" in key["prefLabel"]:
